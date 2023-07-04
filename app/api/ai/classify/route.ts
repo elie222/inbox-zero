1580b38729db878f1f2be38c46d6182c4fa61642
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-03 20:17:28 -0400

    classify email button

---
+++ app/api/ai/classify/route.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai-edge";
+
+const classifyThreadBody = z.object({ message: z.string() });
+export type ClassifyThreadBody = z.infer<typeof classifyThreadBody>;
+export type ClassifyThreadResponse = Awaited<ReturnType<typeof classify>>;
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+export const runtime = "edge";
+
+async function classify(body: ClassifyThreadBody) {
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo-16k",
+    messages: [{
+      role: 'system',
+      content: 'The following is a conversation with an AI assistant that helps classify emails into different categories. The user will send email messages and it is your job to return the category of the email. Categories to use are: "spam", "personal", "work", "promotions", "social", "requires_response"',
+    }, {
+      role: 'user',
+      content: `Please classify this email using a one-word response:\n\n###\n\n${body.message}`,
+    }],
+  });
+  const json = await response.json();
+  const message: string = json?.choices?.[0]?.message?.content;
+
+  return { message };
+}
+
+export async function POST(request: Request) {
+  const json = await request.json();
+  const body = classifyThreadBody.parse(json);
+  const res = await classify(body);
+
+  return NextResponse.json(res);
+}
--- components/List.tsx
+++ components/List.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import useSWR from "swr";
 import { LoadingContent } from "@/components/LoadingContent";
 import { Button } from "./Button";
 import { ThreadResponse } from "@/app/api/google/threads/[id]/route";
+import { postRequest } from "@/utils/api";
+import { ClassifyThreadResponse } from "@/app/api/ai/classify/route";
 
 type Item = { id: string; text: string };
 
@@ -26,25 +29,42 @@ function ListItem(props: { item: Item; onArchive: (id: string) => void }) {
   const { data, isLoading, error } = useSWR<ThreadResponse>(
     `/api/google/threads/${item.id}`
   );
+  const [category, setCategory] = useState<string>();
 
   return (
-    <li className="flex py-5">
+    <li className="flex py-5 text-white">
       <div className="max-w-full">
         <p className="text-sm font-semibold leading-6 text-white break-words whitespace-pre-wrap">
           {item.text}
         </p>
         <LoadingContent loading={isLoading} error={error}>
           {data && (
-            <p className="mt-1 truncate text-xs leading-5 text-gray-400 break-words whitespace-pre-wrap">
-              {data.thread.messages?.[0]?.text.substring(0, 280) || "No message text"}
-            </p>
+            <>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-400 break-words whitespace-pre-wrap">
+                {data.thread.messages?.[0]?.text.substring(0, 280) ||
+                  "No message text"}
+              </p>
+              <Button
+                onClick={async () => {
+                  const category = await postRequest<ClassifyThreadResponse>(
+                    "/api/ai/classify",
+                    {
+                      message: data.thread.messages?.[0]?.text || "",
+                    }
+                  );
+
+                  setCategory(category.message);
+                }}
+              >
+                Categorise
+              </Button>
+              {category && <div>Category: {category}</div>}
+            </>
           )}
         </LoadingContent>
       </div>
-      <div className="hidden sm:flex sm:flex-col sm:items-end">
-        <p className="text-sm leading-6 text-white">
-          <Button onClick={() => onArchive(item.id)}>Archive</Button>
-        </p>
+      <div className="">
+        <Button onClick={() => onArchive(item.id)}>Archive</Button>
       </div>
     </li>
   );
