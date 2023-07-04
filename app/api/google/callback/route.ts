76306202265e99d9de0e018038e362f9fec69f27
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-04 11:16:41 -0400

    redirect to mail after log in

--- app/api/google/callback/route.ts
+++ app/api/google/callback/route.ts
@@ -1,15 +1,15 @@
 import { NextResponse } from "next/server";
-import { client } from "../client";
+import { client } from "@/app/api/google/client";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
 
-  if (!code) 
+  if (!code)
     return NextResponse.error();
 
   const { tokens } = await client.getToken(code);
   client.setCredentials(tokens);
 
-  return NextResponse.json({ tokens });
+  return NextResponse.redirect("http://localhost:3000/mail");
 }
