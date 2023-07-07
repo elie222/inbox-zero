fa505951e3ded03894a43acb17cedc21e23e5151
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-07 19:23:30 -0400

    add more celebration images

--- utils/celebration.ts
+++ utils/celebration.ts
@@ -4,6 +4,8 @@ const urls = [
   'https://illustrations.popsy.co/amber/freelancer.svg',
   'https://illustrations.popsy.co/amber/working-vacation.svg',
   'https://illustrations.popsy.co/amber/remote-work.svg',
+  'https://illustrations.popsy.co/amber/man-riding-a-rocket.svg',
+  'https://illustrations.popsy.co/amber/backpacking.svg',
 ]
 
 export const getCelebrationImage = () => {
