cfd4e6dd1088b955e0f9f37b01b8a47ec6814cbd
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-14 14:31:41 -0400

    fix google log in and refresh

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,10 +1,21 @@
 // based on: https://github.com/vercel/platforms/blob/main/lib/auth.ts
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { getServerSession, type NextAuthOptions } from "next-auth";
+import {
+  getServerSession,
+  TokenSet,
+  type NextAuthOptions,
+  type DefaultSession,
+  Account,
+} from "next-auth";
+import { type JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/utils/prisma";
 
-const VERCEL_DEPLOYMENT = !!process.env.VERCEL_URL;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+if (!GOOGLE_CLIENT_ID) throw new Error("Missing env.GOOGLE_CLIENT_ID");
+if (!GOOGLE_CLIENT_SECRET) throw new Error("Missing env.GOOGLE_CLIENT_SECRET");
 
 const SCOPES = [
   "https://www.googleapis.com/auth/userinfo.profile",
@@ -14,74 +25,187 @@ const SCOPES = [
   "https://www.googleapis.com/auth/gmail.modify",
 ];
 
-const authorizationUrl = new URL(
-  "https://accounts.google.com/o/oauth2/v2/auth"
-);
-authorizationUrl.searchParams.set("prompt", "consent");
-authorizationUrl.searchParams.set("access_type", "offline");
-authorizationUrl.searchParams.set("response_type", "code");
-
 export const authOptions: NextAuthOptions = {
   secret: process.env.SECRET,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
       authorization: {
-        url: authorizationUrl.toString(),
-        params: { scope: SCOPES.join(" ") },
+        url: "https://accounts.google.com/o/oauth2/v2/auth",
+        params: {
+          scope: SCOPES.join(" "),
+          access_type: "offline",
+          response_type: "code",
+          // can be helpful for dev we don't have the refresh token: https://github.com/nextauthjs/next-auth/issues/269#issuecomment-644274504
+          prompt: "consent",
+        },
       },
     }),
   ],
-  // pages: {
-  //   signIn: `/login`,
-  //   verifyRequest: `/login`,
-  //   error: "/login", // Error code passed in query string as ?error=
-  // },
   adapter: PrismaAdapter(prisma) as any,
   session: { strategy: "jwt" },
-  cookies: {
-    sessionToken: {
-      name: `${VERCEL_DEPLOYMENT ? "__Secure-" : ""}next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT
-          ? `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
-          : undefined,
-        secure: VERCEL_DEPLOYMENT,
-      },
-    },
-  },
+  // based on: https://authjs.dev/guides/basics/refresh-token-rotation
+  // and: https://github.com/nextauthjs/next-auth-refresh-token-example/blob/main/pages/api/auth/%5B...nextauth%5D.js
   callbacks: {
     jwt: async ({ token, user, account }) => {
-      if (user) {
+      // Signing in
+      // on first sign in account and user are defined
+      // thereafter only token is defined
+      if (account && user) {
+        // Google sends us refresh_token only on first sign in so we need to save it to the database then
+        // On future log ins, we retrieve the refresh_token from the database
+        if (account.refresh_token) {
+          await saveRefreshToken(
+            {
+              access_token: account.access_token,
+              refresh_token: account.access_token,
+              expires_at: calculateExpiresAt(
+                account.expires_in as number | undefined
+              ),
+            },
+            {
+              providerAccountId: account.providerAccountId,
+              refresh_token: account.refresh_token,
+            }
+          );
+          token.refresh_token = account.refresh_token;
+        } else {
+          const dbAccount = await prisma.account.findUnique({
+            where: {
+              provider_providerAccountId: {
+                providerAccountId: account.providerAccountId,
+                provider: "google",
+              },
+            },
+            select: { refresh_token: true },
+          });
+          token.refresh_token = dbAccount?.refresh_token ?? undefined;
+        }
+
+        token.access_token = account.access_token;
+        token.expires_at = account.expires_at;
         token.user = user;
+
+        return token;
+      } else if (token.expires_at && Date.now() < token.expires_at) {
+        // If the access token has not expired yet, return it
+        return token;
+      } else {
+        // If the access token has expired, try to refresh it
+        return await refreshAccessToken(token);
       }
-      // based on: https://github.com/nextauthjs/next-auth/issues/1162#issuecomment-766331341
-      if (account) {
-        token.accessToken = account?.access_token;
-        token.refreshToken = account?.refresh_token;
-      }
-      return token;
     },
     session: async ({ session, token }) => {
       session.user = {
         ...session.user,
-        // @ts-expect-error
-        id: token.sub,
+        id: token.sub as string,
       };
 
       // based on: https://github.com/nextauthjs/next-auth/issues/1162#issuecomment-766331341
-      session.accessToken = token?.accessToken as string | undefined;
-      session.refreshToken = token?.refreshToken as string | undefined;
+      session.accessToken = token?.access_token;
+      session.refreshToken = token?.refresh_token;
+      session.error = token?.error;
 
       return session;
     },
   },
 };
+
+/**
+ * Takes a token, and returns a new token with updated
+ * `access_token` and `expires_at`. If an error occurs,
+ * returns the old token and an error property
+ */
+const refreshAccessToken = async (token: JWT): Promise<JWT> => {
+  const account = await prisma.account.findFirst({
+    where: { userId: token.sub as string, provider: "google" },
+  });
+
+  if (!account?.refresh_token) {
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+
+  try {
+    const url = new URL("https://oauth2.googleapis.com/token");
+    url.searchParams.set("client_id", GOOGLE_CLIENT_ID);
+    url.searchParams.set("client_secret", GOOGLE_CLIENT_SECRET);
+    url.searchParams.set("grant_type", "refresh_token");
+    url.searchParams.set("refresh_token", account.refresh_token);
+
+    const response = await fetch("https://oauth2.googleapis.com/token", {
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        client_id: GOOGLE_CLIENT_ID,
+        client_secret: GOOGLE_CLIENT_SECRET,
+        grant_type: "refresh_token",
+        refresh_token: account.refresh_token,
+      }),
+      method: "POST",
+    });
+
+    const tokens: TokenSet & { expires_in: number } = await response.json();
+
+    if (!response.ok) throw tokens;
+
+    const expires_at = calculateExpiresAt(tokens.expires_in);
+    await saveRefreshToken(
+      { ...tokens, expires_at },
+      {
+        providerAccountId: account.providerAccountId,
+        refresh_token: account.refresh_token,
+      }
+    );
+
+    return {
+      ...token, // Keep the previous token properties
+      access_token: tokens.access_token,
+      expires_at,
+      // Fall back to old refresh token, but note that
+      // many providers may only allow using a refresh token once.
+      refresh_token: tokens.refresh_token ?? token.refresh_token,
+    };
+  } catch (error) {
+    console.error("Error refreshing access token", error);
+
+    // The error property will be used client-side to handle the refresh token error
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+};
+
+function calculateExpiresAt(expiresIn?: number) {
+  if (!expiresIn) return undefined;
+  return Math.floor(Date.now() / 1000 + (expiresIn - 10)); // give 10 second buffer
+}
+
+async function saveRefreshToken(
+  tokens: {
+    access_token?: string;
+    refresh_token?: string;
+    expires_at?: number;
+  },
+  account: Pick<Account, "refresh_token" | "providerAccountId">
+) {
+  return await prisma.account.update({
+    data: {
+      access_token: tokens.access_token,
+      expires_at: tokens.expires_at,
+      refresh_token: tokens.refresh_token ?? account.refresh_token,
+    },
+    where: {
+      provider_providerAccountId: {
+        provider: "google",
+        providerAccountId: account.providerAccountId,
+      },
+    },
+  });
+}
 
 export function getSession() {
   return getServerSession(authOptions) as Promise<{
@@ -94,6 +218,27 @@ export function getSession() {
     accessToken?: string;
     refreshToken?: string;
   } | null>;
+}
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {} & DefaultSession["user"] & { id: string };
+    accessToken?: string;
+    refreshToken?: string;
+    error?: "RefreshAccessTokenError";
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    access_token?: string;
+    expires_at?: number;
+    refresh_token?: string;
+    error?: "RefreshAccessTokenError";
+  }
 }
 
 // export function withSiteAuth(action: any) {
