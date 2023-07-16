e407eb1790b172a1a712d3481727e0416e908e0e
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-16 12:47:00 -0400

    add link to join waitlist

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -59,7 +59,7 @@ export function Hero(props: {}) {
           </p>
           <div className="mt-10 flex items-center gap-x-6">
             <a
-              href="/mail"
+              href="https://airtable.com/shr7HNx6FXaIxR5q6"
               className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
             >
               Join waitlist
