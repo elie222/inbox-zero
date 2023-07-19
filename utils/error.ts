340e79ad1fc1df9e07c1054e97dca7e4648b3016
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-19 16:07:06 -0400

    fix is error

--- utils/error.ts
+++ utils/error.ts
@@ -3,7 +3,8 @@
 export type ErrorMessage = { message: string; data?: any };
 
 export function isErrorMessage(value: any): value is ErrorMessage {
-  return typeof value?.message === "string";
+  return !!value?.error;
+  // return typeof value?.message === "string";
 }
 
 export function captureException(error: unknown) {
