3143827fb79770e47a90644b136161e148c54ae2
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-19 16:10:35 -0400

    throw error

--- app/api/google/threads/archive/route.ts
+++ app/api/google/threads/archive/route.ts
@@ -9,7 +9,10 @@ export const POST = withError(async (request: Request) => {
   const json = await request.json();
   const body = archiveBody.parse(json);
 
-  const thread = await archiveEmail(body);
-
-  return NextResponse.json(thread);
+  try {
+    const thread = await archiveEmail(body);
+    return NextResponse.json(thread);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 });
