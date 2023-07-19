034a3689c73a32f573a7aed9527fe4d21d59ba61
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-19 16:07:17 -0400

    custom toast title

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,11 +1,15 @@
 import { Toaster as SonnerToaster, toast } from "sonner";
 
-export function toastSuccess(options: { description: string }) {
-  return toast.success("Success", { description: options.description });
+export function toastSuccess(options: { title?: string; description: string }) {
+  return toast.success(options.title || "Success", {
+    description: options.description,
+  });
 }
 
-export function toastError(options: { description: string }) {
-  return toast.error("Error", { description: options.description });
+export function toastError(options: { title?: string; description: string }) {
+  return toast.error(options.title || "Error", {
+    description: options.description,
+  });
 }
 
 export const Toaster = SonnerToaster;
