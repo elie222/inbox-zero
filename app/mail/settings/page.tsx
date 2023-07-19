a83f29684fb7d213b501deee2e63c35ae543608c
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-19 18:51:30 -0400

    fix refactor

--- app/mail/settings/page.tsx
+++ app/mail/settings/page.tsx
@@ -1,6 +1,6 @@
 import { FormWrapper } from "@/components/Form";
 import { AboutSection } from "@/app/mail/settings/AboutSection";
-import { LabelsSection } from "@/app/mail/settings/LabelsSectionForm";
+import { LabelsSection } from "@/app/mail/settings/LabelsSection";
 import { DeleteSection } from "@/app/mail/settings/DeleteSection";
 
 export default function Settings() {
