2f20419128977cb88044fa3a678deaac4d77559f
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-24 01:58:26 -0400

    hide unused prompt bar

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,11 +1,11 @@
 import { Fragment } from "react";
-import { Bars3Icon, BellIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Menu, Transition } from "@headlessui/react";
 import { Button } from "@/components/Button";
-import { PromptBar } from "@/components/PromptBar";
+// import { PromptBar } from "@/components/PromptBar";
 
 const userNavigation = [
   { name: "Usage", href: "/usage" },
@@ -28,10 +28,10 @@ export function TopNav(props: { setSidebarOpen: (open: boolean) => void }) {
       <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
 
       <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-        <PromptBar />
+        {/* <PromptBar /> */}
 
-        <div className="flex items-center gap-x-4 lg:gap-x-6">
-          <button
+        <div className="ml-auto flex items-center gap-x-4 lg:gap-x-6">
+          {/* <button
             type="button"
             className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
           >
@@ -39,13 +39,11 @@ export function TopNav(props: { setSidebarOpen: (open: boolean) => void }) {
             <BellIcon className="h-6 w-6" aria-hidden="true" />
           </button>
 
-          {/* Separator */}
           <div
             className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10"
             aria-hidden="true"
-          />
+          /> */}
 
-          {/* Profile dropdown */}
           <ProfileDropdown />
         </div>
       </div>
