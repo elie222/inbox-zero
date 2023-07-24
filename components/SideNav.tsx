df433184b86b490a4da1a28ce7e3657bb6e74989
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-24 01:53:22 -0400

    clean up side nav

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -61,12 +61,6 @@ export function SideNav(props: {
   sidebarOpen: boolean;
   setSidebarOpen: (open: boolean) => void;
 }) {
-  const { data, isLoading, error, mutate } = useSWR<PromptHistoryResponse>(
-    "/api/user/prompt-history"
-  );
-
-  const path = usePathname();
-
   return (
     <>
       <div className="h-full">
@@ -123,146 +117,15 @@ export function SideNav(props: {
                     </div>
                   </Transition.Child>
 
-                  {/* Sidebar component, swap this element with another sidebar if you like */}
-                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4 ring-1 ring-white/10">
-                    <div className="flex h-16 shrink-0 items-center text-white">
-                      <Logo className="h-4" />
-                    </div>
-                    <nav className="flex flex-1 flex-col">
-                      <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                        <li>
-                          <ul role="list" className="-mx-2 space-y-1">
-                            {navigation.map((item) => (
-                              <li key={item.name}>
-                                <a
-                                  href={item.href}
-                                  className={clsx(
-                                    item.href === path
-                                      ? "bg-gray-800 text-white"
-                                      : "text-gray-400 hover:bg-gray-800 hover:text-white",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                                  )}
-                                >
-                                  <item.icon
-                                    className="h-6 w-6 shrink-0"
-                                    aria-hidden="true"
-                                  />
-                                  {item.name}
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
-                        <LoadingContent loading={isLoading} error={error}>
-                          {data && (
-                            <PromptHistory
-                              history={data.history}
-                              refetch={mutate}
-                            />
-                          )}
-                        </LoadingContent>
-
-                        <li className="mt-auto">
-                          <ul role="list" className="-mx-2 space-y-1">
-                            {bottomLinks.map((link) => {
-                              return (
-                                <li key={link.name}>
-                                  <a
-                                    href={link.href}
-                                    className={clsx(
-                                      "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
-                                      link.href === path
-                                        ? "bg-gray-800 text-white"
-                                        : "text-gray-400 hover:bg-gray-800 hover:text-white"
-                                    )}
-                                  >
-                                    <link.icon
-                                      className="h-6 w-6 shrink-0"
-                                      aria-hidden="true"
-                                    />
-                                    {link.name}
-                                  </a>
-                                </li>
-                              );
-                            })}
-                          </ul>
-                        </li>
-                      </ul>
-                    </nav>
-                  </div>
+                  <Sidebar isMobile />
                 </Dialog.Panel>
               </Transition.Child>
             </div>
           </Dialog>
         </Transition.Root>
 
-        {/* Static sidebar for desktop */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-60 lg:flex-col 2xl:w-72">
-          {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4">
-            <Link href="/mail">
-              <div className="flex h-16 shrink-0 items-center text-white">
-                <Logo className="h-4" />
-              </div>
-            </Link>
-            <nav className="flex flex-1 flex-col">
-              <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                <li>
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className={clsx(
-                            item.href === path
-                              ? "bg-gray-800 text-white"
-                              : "text-gray-400 hover:bg-gray-800 hover:text-white",
-                            "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
-                          )}
-                        >
-                          <item.icon
-                            className="h-6 w-6 shrink-0"
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-                <LoadingContent loading={isLoading} error={error}>
-                  {data && (
-                    <PromptHistory history={data.history} refetch={mutate} />
-                  )}
-                </LoadingContent>
-                <li className="mt-auto">
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {bottomLinks.map((link) => {
-                      return (
-                        <li key={link.name}>
-                          <a
-                            href={link.href}
-                            className={clsx(
-                              "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
-                              link.href === path
-                                ? "bg-gray-800 text-white"
-                                : "text-gray-400 hover:bg-gray-800 hover:text-white"
-                            )}
-                          >
-                            <link.icon
-                              className="h-6 w-6 shrink-0"
-                              aria-hidden="true"
-                            />
-                            {link.name}
-                          </a>
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
+          <Sidebar isMobile={false} />
         </div>
 
         <main className="flex h-full flex-col lg:pl-60 2xl:pl-72">
@@ -272,5 +135,85 @@ export function SideNav(props: {
         </main>
       </div>
     </>
+  );
+}
+
+function Sidebar(props: { isMobile: boolean }) {
+  const { data, isLoading, error, mutate } = useSWR<PromptHistoryResponse>(
+    "/api/user/prompt-history"
+  );
+
+  const path = usePathname();
+
+  return (
+    <div
+      className={clsx(
+        "flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-4",
+        {
+          "ring-1 ring-white/10": props.isMobile,
+        }
+      )}
+    >
+      <Link href="/mail">
+        <div className="flex h-16 shrink-0 items-center text-white">
+          <Logo className="h-4" />
+        </div>
+      </Link>
+      <nav className="flex flex-1 flex-col">
+        <ul role="list" className="flex flex-1 flex-col gap-y-7">
+          <li>
+            <ul role="list" className="-mx-2 space-y-1">
+              {navigation.map((item) => (
+                <li key={item.name}>
+                  <a
+                    href={item.href}
+                    className={clsx(
+                      item.href === path
+                        ? "bg-gray-800 text-white"
+                        : "text-gray-400 hover:bg-gray-800 hover:text-white",
+                      "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6"
+                    )}
+                  >
+                    <item.icon
+                      className="h-6 w-6 shrink-0"
+                      aria-hidden="true"
+                    />
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </li>
+          <LoadingContent loading={isLoading} error={error}>
+            {data && <PromptHistory history={data.history} refetch={mutate} />}
+          </LoadingContent>
+          <li className="mt-auto">
+            <ul role="list" className="-mx-2 space-y-1">
+              {bottomLinks.map((link) => {
+                return (
+                  <li key={link.name}>
+                    <a
+                      href={link.href}
+                      className={clsx(
+                        "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
+                        link.href === path
+                          ? "bg-gray-800 text-white"
+                          : "text-gray-400 hover:bg-gray-800 hover:text-white"
+                      )}
+                    >
+                      <link.icon
+                        className="h-6 w-6 shrink-0"
+                        aria-hidden="true"
+                      />
+                      {link.name}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
