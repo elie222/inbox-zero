4155da18845626005e167e64144c7b03c27688e9
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-07-28 19:04:18 +0300

    redirect to login page if logged out

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,10 +1,12 @@
 import "../globals.css";
-// import { Inter } from "next/font/google";
+import React from "react";
+import { redirect } from "next/navigation";
 import { GmailProvider } from "@/providers/GmailProvider";
 import { SessionProvider } from "@/providers/SessionProvider";
 import { SideNavWithTopNav } from "@/components/SideNavWithTopNav";
 // import { PromptProvider } from "@/providers/PromptProvider";
 import { SWRProvider } from "@/providers/SWRProvider";
+import { getAuthSession } from "@/utils/auth";
 
 export const metadata = {
   title: "Inbox Zero AI",
@@ -12,21 +14,23 @@ export const metadata = {
     "Reach inbox zero in minutes. Inbox Zero uses AI to help you empty your inbox daily. What previously took hours, now takes minutes. Inbox Zero is your VA for emails.",
 };
 
-// NOTE: inherits from top level layout. use groups if we want to break this out
-export default function RootLayout({
+// NOTE: inherits from top level layout
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getAuthSession();
+
+  if (!session?.user.email) redirect("/api/auth/signin");
+
   return (
     <html lang="en">
       <body>
         <SWRProvider>
           <GmailProvider>
             <SessionProvider>
-              {/* <PromptProvider> */}
               <SideNavWithTopNav>{children}</SideNavWithTopNav>
-              {/* </PromptProvider> */}
             </SessionProvider>
           </GmailProvider>
         </SWRProvider>
