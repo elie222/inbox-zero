00884932f831b00cb3e14036da2058c9d130630d
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-08-10 16:57:06 +0300

    refactor providers

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,12 +1,10 @@
 import "../../styles/globals.css";
 import React from "react";
 import { redirect } from "next/navigation";
-import { GmailProvider } from "@/providers/GmailProvider";
-import { SessionProvider } from "@/providers/SessionProvider";
 import { SideNavWithTopNav } from "@/components/SideNavWithTopNav";
-import { SWRProvider } from "@/providers/SWRProvider";
 import { getAuthSession } from "@/utils/auth";
 import { TokenCheck } from "@/components/TokenCheck";
+import Providers from "@/app/(app)/providers";
 
 export const metadata = {
   title: "Inbox Zero AI",
@@ -27,14 +25,10 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <SWRProvider>
-          <GmailProvider>
-            <SessionProvider>
-              <TokenCheck />
-              <SideNavWithTopNav>{children}</SideNavWithTopNav>
-            </SessionProvider>
-          </GmailProvider>
-        </SWRProvider>
+        <Providers>
+          <TokenCheck />
+          <SideNavWithTopNav>{children}</SideNavWithTopNav>
+        </Providers>
       </body>
     </html>
   );
---
+++ app/(app)/providers.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from "react";
+import { GmailProvider } from "@/providers/GmailProvider";
+import { SessionProvider } from "@/providers/SessionProvider";
+import { SWRProvider } from "@/providers/SWRProvider";
+
+export default function Providers(props: { children: React.ReactNode }) {
+  return (
+    <SWRProvider>
+      <GmailProvider>
+        <SessionProvider>{props.children}</SessionProvider>
+      </GmailProvider>
+    </SWRProvider>
+  );
+}
