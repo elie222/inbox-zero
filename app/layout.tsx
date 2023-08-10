bf64896caf5173d0c1c1f89451b54f89141c7d63
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-08-10 14:31:29 +0300

    fix cal font

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className={`h-full ${inter.variable} ${calFont.variable}`}>
+      <body
+        className={`h-full ${inter.variable} ${calFont.variable} font-sans`}
+      >
         {children}
       </body>
     </html>
