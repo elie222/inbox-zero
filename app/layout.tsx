110de25114118dc35a80af0929840eaa57b645ab
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-08-10 14:09:45 +0300

    try fix font

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className={`h-full ${inter.className}`}>{children}</body>
+      <body className={`h-full ${inter.variable} ${calFont.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
