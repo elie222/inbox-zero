d7fdbe261bd2e1c4932931a89c4038188483eb54
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-08-15 18:17:48 +0300

    force push

--- apps/web/utils/gmail/thread.ts
+++ apps/web/utils/gmail/thread.ts
@@ -1,10 +1,11 @@
 import { gmail_v1 } from "googleapis";
 
 export async function getThread(threadId: string, gmail: gmail_v1.Gmail) {
-  const message = await gmail.users.threads.get({
+  const thread = await gmail.users.threads.get({
     userId: "me",
     id: threadId,
   });
+  console.log(thread);
 
-  return message.data;
+  return thread.data;
 }
