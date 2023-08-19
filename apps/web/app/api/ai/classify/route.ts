2a30e066187a6e16e7d07e091e1465d65cf63c2a
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-08-19 22:16:36 +0300

    update classify prompt

--- apps/web/app/api/ai/classify/route.ts
+++ apps/web/app/api/ai/classify/route.ts
@@ -9,7 +9,10 @@ import {
 } from "@/utils/types";
 import { withError } from "@/utils/middleware";
 
-const classifyThreadBody = z.object({ message: z.string() });
+const classifyThreadBody = z.object({
+  subject: z.string(),
+  content: z.string(),
+});
 export type ClassifyThreadBody = z.infer<typeof classifyThreadBody>;
 export type ClassifyThreadResponse = Awaited<ReturnType<typeof classify>>;
 
@@ -21,12 +24,37 @@ async function classify(body: ClassifyThreadBody) {
     messages: [
       {
         role: "system",
-        content:
-          'You are an AI assistant that helps classify emails into different categories. The user will send email messages and it is your job to return the category of the email. Categories to use are: "spam", "promotions", "social", "requires_response", "requires_action", "receipts", "newsletter", "app_update", "terms_and_conditions_update"',
+        content: "You are an assistant that helps categorise emails.",
       },
       {
         role: "user",
-        content: `Please classify this email using a one-word response:\n\n###\n\n${body.message}`,
+        content: `Please categorise this email.
+Return a JSON object with a "category" and "confidence" fields. Confidence is a number between 0 and 1.
+
+Categories to choose from with an explanation of each category:
+NEWSLETTER - newsletters
+MARKETING - marketing emails
+RECEIPT - a receipt or invoice for payments I've made
+EVENT - calendar or event invites
+TRAVEL - travel itineraries and confirmations
+QUESTION - someone asks me a question
+COLD_EMAIL - someone I don't know emails me to sell me something
+SOCIAL_MEDIA - notifications from social media
+LEGAL_UPDATE - updates about changes to terms of service or privacy policy
+OTHER - anything else
+
+An example response would be:
+{
+  "category": "NEWSLETTER",
+  "confidence": 0.8
+}
+
+##
+Email:
+
+Subject: ${body.subject}
+
+Body: ${body.content}`,
       },
     ],
   });
