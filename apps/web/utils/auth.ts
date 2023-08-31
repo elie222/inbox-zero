da79a3a817bad3ffa08677b4284f505928798e60
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-08-31 22:52:24 +0300

    remove unnecessary scope

--- apps/web/utils/auth.ts
+++ apps/web/utils/auth.ts
@@ -21,7 +21,6 @@ const SCOPES = [
   "https://www.googleapis.com/auth/userinfo.profile",
   "https://www.googleapis.com/auth/userinfo.email",
 
-  "https://www.googleapis.com/auth/gmail.readonly",
   "https://www.googleapis.com/auth/gmail.modify",
 ];
 
