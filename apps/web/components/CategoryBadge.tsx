fbe9a08b7d1e02236128fb8dbefa01812734d02c
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-09-10 12:11:44 +0300

    color categories

--- apps/web/components/CategoryBadge.tsx
+++ apps/web/components/CategoryBadge.tsx
@@ -1,12 +1,29 @@
-import { Badge } from "@/components/Badge";
+import { Badge, Color } from "@/components/Badge";
 import { capitalCase } from "capital-case";
+
+const categoryColors: Record<string, Color> = {
+  NEWSLETTER: "blue",
+  PROMOTIONAL: "yellow",
+  RECEIPT: "yellow",
+  ALERT: "yellow",
+  NOTIFICATION: "yellow",
+  FORUM: "yellow",
+  EVENT: "green",
+  TRAVEL: "green",
+  QUESTION: "red",
+  SUPPORT: "red",
+  COLD_EMAIL: "yellow",
+  SOCIAL_MEDIA: "yellow",
+  LEGAL_UPDATE: "yellow",
+  OTHER: "yellow",
+};
 
 export function CategoryBadge(props: { category?: string }) {
   const { category } = props;
 
   return (
     <Badge
-      color={category ? "blue" : "gray"}
+      color={category ? categoryColors[category] || "gray" : "gray"}
       className="max-w-[100px] overflow-hidden"
     >
       {capitalCase(category || "Uncategorized")}
