55fac0af8d7f793116805dd7ce7b2c13757897ad
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-09-13 16:18:31 +0300

    hide background pricing style

--- apps/web/app/(app)/premium/Pricing.tsx
+++ apps/web/app/(app)/premium/Pricing.tsx
@@ -42,7 +42,7 @@ const tiers = [
 export function Pricing() {
   return (
     <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div
+      {/* <div
         className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
         aria-hidden="true"
       >
@@ -53,7 +53,7 @@ export function Pricing() {
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
         />
-      </div>
+      </div> */}
       <div className="mx-auto max-w-2xl text-center lg:max-w-4xl">
         <h2 className="text-base font-semibold leading-7 text-blue-600">
           Pricing
