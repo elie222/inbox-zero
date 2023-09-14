90429793f0766e72cd1f00fc9fea5a8b8d52fed9
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-09-14 11:35:20 +0300

    add homepage screenshot

--- apps/web/app/(landing)/home/Hero.tsx
+++ apps/web/app/(landing)/home/Hero.tsx
@@ -42,17 +42,17 @@ export function Hero() {
               </Button>
             </div>
           </div>
-          {/* <div className="mt-16 flow-root sm:mt-24">
+          <div className="mt-16 flow-root sm:mt-24">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
               <img
-                src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
-                alt="App screenshot"
+                src="/images/mail.png"
+                alt="Inbox screenshot"
                 width={2432}
                 height={1442}
                 className="rounded-md shadow-2xl ring-1 ring-gray-900/10"
               />
             </div>
-          </div> */}
+          </div>
         </div>
       </div>
       <div
