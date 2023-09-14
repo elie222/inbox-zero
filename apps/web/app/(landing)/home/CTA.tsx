f6157938723261fd0cb60c1eb6a1666e8f184caf
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-09-14 12:34:25 +0300

    remove gradient splash homepage

--- apps/web/app/(landing)/home/CTA.tsx
+++ apps/web/app/(landing)/home/CTA.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { Button } from "@/components/Button";
+import { GithubIcon } from "lucide-react";
 
 export function CTA() {
   return (
     <div className="relative -z-10 mt-32 px-6 lg:px-8">
-      <div
+      {/* <div
         className="absolute inset-x-0 top-1/2 -z-10 flex -translate-y-1/2 transform-gpu justify-center overflow-hidden blur-3xl sm:bottom-0 sm:right-[calc(50%-6rem)] sm:top-auto sm:translate-y-0 sm:transform-gpu sm:justify-end"
         aria-hidden="true"
       >
@@ -15,7 +16,7 @@ export function CTA() {
               "polygon(73.6% 48.6%, 91.7% 88.5%, 100% 53.9%, 97.4% 18.1%, 92.5% 15.4%, 75.7% 36.3%, 55.3% 52.8%, 46.5% 50.9%, 45% 37.4%, 50.3% 13.1%, 21.3% 36.2%, 0.1% 0.1%, 5.4% 49.1%, 21.4% 36.4%, 58.9% 100%, 73.6% 48.6%)",
           }}
         />
-      </div>
+      </div> */}
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="font-cal text-3xl font-bold text-gray-900 sm:text-4xl">
           Boost your productivity.
@@ -31,9 +32,17 @@ export function CTA() {
           <Button size="2xl" link={{ href: "/waitlist" }}>
             Join Waitlist
           </Button>
+          <Button
+            size="2xl"
+            color="white"
+            link={{ href: "/github", target: "_blank" }}
+          >
+            <GithubIcon className="mr-2 h-4 w-4" />
+            Star on GitHub
+          </Button>
         </div>
       </div>
-      <div
+      {/* <div
         className="absolute left-1/2 right-0 top-full -z-10 hidden -translate-y-1/2 transform-gpu overflow-hidden blur-3xl sm:block"
         aria-hidden="true"
       >
@@ -44,7 +53,7 @@ export function CTA() {
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
         />
-      </div>
+      </div> */}
     </div>
   );
 }
--- apps/web/app/(landing)/home/Hero.tsx
+++ apps/web/app/(landing)/home/Hero.tsx
@@ -86,7 +86,7 @@ export function Hero() {
           </div>
         </div>
       </div>
-      <div
+      {/* <div
         className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
         aria-hidden="true"
       >
@@ -97,7 +97,7 @@ export function Hero() {
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
         />
-      </div>
+      </div> */}
     </div>
   );
 }
