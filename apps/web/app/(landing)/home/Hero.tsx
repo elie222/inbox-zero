b658e8577e5d3980b71a9319cdbab5a984b3fa9d
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-09-14 12:50:44 +0300

    use next image

--- apps/web/app/(landing)/home/Hero.tsx
+++ apps/web/app/(landing)/home/Hero.tsx
@@ -1,7 +1,7 @@
 import { GithubIcon } from "lucide-react";
+import Image from "next/image";
 import { Button } from "@/components/Button";
 
-/* eslint-disable @next/next/no-img-element */
 export function Hero() {
   return (
     <div className="relative pt-14">
@@ -75,7 +75,7 @@ export function Hero() {
           </div>
           <div className="mt-16 flow-root sm:mt-24">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <img
+              <Image
                 src="/images/mail.png"
                 alt="Inbox screenshot"
                 width={2432}
