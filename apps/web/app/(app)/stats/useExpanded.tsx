b5ad86c20ad3d13312273466db1671ac862d1973
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-10-27 10:54:45 -0400

    show less

--- apps/web/app/(app)/stats/useExpanded.tsx
+++ apps/web/app/(app)/stats/useExpanded.tsx
@@ -1,28 +1,38 @@
-import { ChevronsDownIcon } from "lucide-react";
+import { ChevronsDownIcon, ChevronsUpIcon } from "lucide-react";
 import { useCallback, useMemo, useState } from "react";
 import { Button } from "@/components/ui/button";
 
 export const useExpanded = () => {
   const [expanded, setExpanded] = useState(false);
-  const onExpand = useCallback(() => setExpanded(true), []);
+  const toggleExpand = useCallback(
+    () => setExpanded((expanded) => !expanded),
+    []
+  );
 
   const extra = useMemo(() => {
     return (
-      !expanded && (
-        <div className="mt-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={onExpand}
-            className="w-full"
-          >
-            <ChevronsDownIcon className="h-4 w-4" />
-            <span className="ml-2">Show more</span>
-          </Button>
-        </div>
-      )
+      <div className="mt-2">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={toggleExpand}
+          className="w-full"
+        >
+          {expanded ? (
+            <>
+              <ChevronsUpIcon className="h-4 w-4" />
+              <span className="ml-2">Show less</span>
+            </>
+          ) : (
+            <>
+              <ChevronsDownIcon className="h-4 w-4" />
+              <span className="ml-2">Show more</span>
+            </>
+          )}
+        </Button>
+      </div>
     );
-  }, [expanded, onExpand]);
+  }, [expanded, toggleExpand]);
 
   return { expanded, extra };
 };
