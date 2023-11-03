11855e3cce5b7efb4fde7eea274eea669acaeaaa
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-11-02 20:26:57 -0400

    fix delete

--- packages/tinybird/src/delete.ts
+++ packages/tinybird/src/delete.ts
@@ -1,16 +1,22 @@
+const TINYBIRD_BASE_URL = process.env.TINYBIRD_BASE_URL;
+const TINYBIRD_TOKEN = process.env.TINYBIRD_TOKEN;
+
+if (!TINYBIRD_BASE_URL || !TINYBIRD_TOKEN)
+  throw new Error("Missing TINYBIRD_TOKEN");
+
 async function deleteFromDatasource(
   datasource: string,
   deleteCondition: string // eg. "email='abc@example.com'"
 ): Promise<unknown> {
   const url = new URL(
     `/v0/datasources/${datasource}/delete`,
-    process.env.TINYBIRD_BASE_URL
+    TINYBIRD_BASE_URL
   );
   let res = await fetch(url, {
     method: "POST",
     body: `delete_condition=(${deleteCondition})`,
     headers: {
-      Authorization: `Bearer ${process.env.TINYBIRD_TOKEN}`,
+      Authorization: `Bearer ${TINYBIRD_TOKEN}`,
       "Content-Type": "application/x-www-form-urlencoded",
     },
   });
@@ -29,5 +35,5 @@ async function deleteFromDatasource(
 export async function deleteTinybirdEmails(options: {
   email: string;
 }): Promise<unknown> {
-  return await deleteFromDatasource("email", `email='${options.email}'`);
+  return await deleteFromDatasource("email", `ownerEmail='${options.email}'`);
 }
