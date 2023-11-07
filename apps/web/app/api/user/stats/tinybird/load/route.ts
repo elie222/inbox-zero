e69999aef32d24df1921286237d50578b53cb1e8
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-11-07 08:12:56 -0500

    find more unsubscribe links

--- apps/web/app/api/user/stats/tinybird/load/route.ts
+++ apps/web/app/api/user/stats/tinybird/load/route.ts
@@ -204,8 +204,36 @@ function findUnsubscribeLink(html: string) {
 
   $("a").each((_index, element) => {
     const text = $(element).text().toLowerCase();
-    if (text.includes("unsubscribe")) {
+    if (
+      text.includes("unsubscribe") ||
+      text.includes("email preferences") ||
+      text.includes("email settings") ||
+      text.includes("email options")
+    ) {
       unsubscribeLink = $(element).attr("href");
+      return false; // break the loop
+    }
+  });
+
+  if (unsubscribeLink) return unsubscribeLink;
+
+  // if we didn't find a link yet, try looking for lines that include the word unsubscribe
+  // with a link in the same line.
+
+  // nodeType of 3 represents a text node, which is the actual text inside an element or attribute.
+  const textNodes = $("*")
+    .contents()
+    .filter((_index, content) => {
+      return content.nodeType === 3 && content.data.includes("subscribe");
+    });
+
+  textNodes.each((_index, textNode) => {
+    // Find the closest parent that has an 'a' tag
+    const parent = $(textNode).parent();
+    const link = parent.find("a").attr("href");
+    if (link) {
+      console.log(`Found text including 'subscribe' and a link: ${link}`);
+      unsubscribeLink = link;
       return false; // break the loop
     }
   });
