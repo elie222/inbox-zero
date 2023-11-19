b106c62fed1e3503b1b847adc8d21039b197d015
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-11-18 20:50:39 -0500

    update video styling

--- apps/web/app/(landing)/home/VideoDemo.tsx
+++ apps/web/app/(landing)/home/VideoDemo.tsx
@@ -11,13 +11,13 @@ export function VideoDemo() {
   return (
     <>
       <div
-        className="group absolute inset-0 flex cursor-pointer items-center justify-center hover:bg-slate-700 hover:bg-opacity-30 lg:rounded-2xl"
+        className="group absolute inset-0 flex cursor-pointer items-center justify-center duration-500 hover:bg-white hover:bg-opacity-30 lg:rounded-2xl"
         onClick={() => {
           openModal();
           posthog.capture("Landing Page Video Clicked");
         }}
       >
-        <div className="inline-flex h-16 w-16 animate-pulse items-center justify-center rounded-full bg-blue-400 bg-opacity-80 text-slate-50 shadow-lg transition-transform focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 group-hover:scale-125 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90 dark:focus-visible:ring-slate-300">
+        <div className="inline-flex h-16 w-16 animate-pulse items-center justify-center rounded-full bg-blue-400 text-slate-50 shadow-lg transition-transform focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:pointer-events-none disabled:opacity-50 group-hover:scale-125 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90 dark:focus-visible:ring-slate-300">
           <PlayIcon className="h-7 w-7" />
         </div>
       </div>
