3d539f896e1fa0c9d4f1690f43ac46e8e89be919
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-11-20 15:11:21 -0500

    ability to minimise automation rules/header

--- apps/web/app/(app)/automation/page.tsx
+++ apps/web/app/(app)/automation/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import { useIsClient, useLocalStorage } from "usehooks-ts";
 import { useSearchParams } from "next/navigation";
 import { Tabs } from "@/components/Tabs";
 import { RulesSection } from "@/app/(app)/automation/RulesSection";
-import { SectionDescription, SectionHeader } from "@/components/Typography";
+import { SectionDescription } from "@/components/Typography";
 import { TopSection } from "@/components/TopSection";
 import { PremiumAlert, usePremium } from "@/components/PremiumAlert";
 import { Planned } from "@/app/(app)/automation/Planned";
@@ -18,33 +18,19 @@ export default function PlannedPage() {
 
   const { isPremium } = usePremium();
 
-  const [expandRules, setExpandRules] = useState(true);
+  const [expandRules, setExpandRules] = useLocalStorage(
+    "automationRulesExpanded",
+    true
+  );
   const toggleExpandRules = () => setExpandRules((prev) => !prev);
 
-  return (
-    <div>
-      <TopSection
-        title="AI Automation"
-        descriptionComponent={
-          <>
-            <SectionDescription>
-              Set rules for our AI to handle incoming emails automatically.
-            </SectionDescription>
-            <SectionDescription>
-              Run in planning mode to see what the AI would do without it
-              actually doing anything. Alternatively, activate automated mode to
-              enable the AI to automatically process your emails.
-            </SectionDescription>
-            {!isPremium && (
-              <div className="mt-4">
-                <PremiumAlert />
-              </div>
-            )}
-          </>
-        }
-      />
+  // prevent hydration error from localStorage
+  const isClient = useIsClient();
+  if (!isClient) return null;
 
-      <div className="relative border-b border-gray-200 bg-white shadow-sm">
+  return (
+    <div className="relative">
+      <>
         {expandRules ? (
           <>
             <Button
@@ -55,22 +41,44 @@ export default function PlannedPage() {
             >
               <Minimize2Icon className="h-4 w-4" />
             </Button>
-            <RulesSection />
+
+            <TopSection
+              title="AI Automation"
+              descriptionComponent={
+                <>
+                  <SectionDescription>
+                    Set rules for our AI to handle incoming emails
+                    automatically.
+                  </SectionDescription>
+                  <SectionDescription>
+                    Run in planning mode to see what the AI would do without it
+                    actually doing anything. Alternatively, activate automated
+                    mode to enable the AI to automatically process your emails.
+                  </SectionDescription>
+                  {!isPremium && (
+                    <div className="mt-4">
+                      <PremiumAlert />
+                    </div>
+                  )}
+                </>
+              }
+            />
+
+            <div className="border-b border-gray-200 bg-white shadow-sm">
+              <RulesSection />
+            </div>
           </>
         ) : (
-          <div className="flex h-[55px] items-center py-2 pl-6">
-            <SectionHeader>Set AI Rules</SectionHeader>
-            <Button
-              variant="outline"
-              size="icon"
-              onClick={toggleExpandRules}
-              className="absolute right-2 top-2"
-            >
-              <Maximize2Icon className="h-4 w-4" />
-            </Button>
-          </div>
+          <Button
+            variant="outline"
+            onClick={toggleExpandRules}
+            className="absolute right-2 top-2"
+          >
+            <Maximize2Icon className="mr-2 h-4 w-4" />
+            Set AI Rules
+          </Button>
         )}
-      </div>
+      </>
 
       <div className="mb-8 sm:px-4">
         <div className="py-2">
