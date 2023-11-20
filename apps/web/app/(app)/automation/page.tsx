914c64a8c791b4ab8020b2eea3d8c8a4c8041fe6
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-11-20 14:45:03 -0500

    make rules collapsible

--- apps/web/app/(app)/automation/page.tsx
+++ apps/web/app/(app)/automation/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
+import { useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { Tabs } from "@/components/Tabs";
 import { RulesSection } from "@/app/(app)/automation/RulesSection";
-import { SectionDescription } from "@/components/Typography";
+import { SectionDescription, SectionHeader } from "@/components/Typography";
 import { TopSection } from "@/components/TopSection";
 import { PremiumAlert, usePremium } from "@/components/PremiumAlert";
 import { Planned } from "@/app/(app)/automation/Planned";
 import { PlanHistory } from "@/app/(app)/automation/PlanHistory";
+import { Maximize2Icon, Minimize2Icon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function PlannedPage() {
   const params = useSearchParams();
   const selectedTab = params.get("tab") || "planned";
 
   const { isPremium } = usePremium();
+
+  const [expandRules, setExpandRules] = useState(true);
+  const toggleExpandRules = () => setExpandRules((prev) => !prev);
 
   return (
     <div>
@@ -38,12 +44,36 @@ export default function PlannedPage() {
         }
       />
 
-      <div className="border-b border-gray-200 bg-white shadow-sm">
-        <RulesSection />
+      <div className="relative border-b border-gray-200 bg-white shadow-sm">
+        {expandRules ? (
+          <>
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={toggleExpandRules}
+              className="absolute right-2 top-2"
+            >
+              <Minimize2Icon className="h-4 w-4" />
+            </Button>
+            <RulesSection />
+          </>
+        ) : (
+          <div className="flex h-[55px] items-center py-2 pl-6">
+            <SectionHeader>Set AI Rules</SectionHeader>
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={toggleExpandRules}
+              className="absolute right-2 top-2"
+            >
+              <Maximize2Icon className="h-4 w-4" />
+            </Button>
+          </div>
+        )}
       </div>
 
       <div className="mb-8 sm:px-4">
-        <div className="p-2">
+        <div className="py-2">
           <Tabs
             selected={selectedTab}
             tabs={[
