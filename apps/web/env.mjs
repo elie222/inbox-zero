4a5d513e1cbcedc09d34d094c3cd2c0781c82b4e
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-11-26 10:17:59 -0500

    better ignore

--- apps/web/env.mjs
+++ apps/web/env.mjs
@@ -30,7 +30,7 @@ export const env = createEnv({
     NEXT_PUBLIC_ENTERPRISE_PAYMENT_LINK: z.string().min(1),
     NEXT_PUBLIC_POSTHOG_KEY: z.string().optional(),
     NEXT_PUBLIC_POSTHOG_ONBOARDING_SURVEY_ID: z.string().optional(),
-    NEXT_PUBLIC_BASE_URL: z.string().default("https://getinboxzero.com"),
+    NEXT_PUBLIC_BASE_URL: z.string().default("https://www.getinboxzero.com"),
     NEXT_PUBLIC_CONTACTS_ENABLED: z.boolean().default(false),
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
