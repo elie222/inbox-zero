8ce21b3a2e7b8926cf4ef84a4218c8939184c24e
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-11-27 09:17:30 -0500

    add modify scope back

--- apps/web/utils/auth.ts
+++ apps/web/utils/auth.ts
@@ -10,6 +10,7 @@ const SCOPES = [
   "https://www.googleapis.com/auth/userinfo.profile",
   "https://www.googleapis.com/auth/userinfo.email",
 
+  "https://www.googleapis.com/auth/gmail.modify",
   "https://www.googleapis.com/auth/gmail.settings.basic",
   "https://www.googleapis.com/auth/contacts",
 ];
