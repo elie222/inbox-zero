36e98a1e391f64343ece65a638523c32e27527cf
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-12-07 12:59:08 -0800

    revert to old categorisation as it's more efficient and better

--- apps/web/app/api/ai/categorise/controller.ts
+++ apps/web/app/api/ai/categorise/controller.ts
@@ -1,128 +1,104 @@
 import { z } from "zod";
-import { zodToJsonSchema } from "zod-to-json-schema";
 import { parseJSON } from "@/utils/json";
 import { UserAIFields, getOpenAI } from "@/utils/openai";
 import { DEFAULT_AI_MODEL } from "@/utils/config";
 import { getCategory, saveCategory } from "@/utils/redis/category";
 import { CategoriseBody } from "@/app/api/ai/categorise/validation";
-import { RunnableFunctionWithParse } from "openai/lib/RunnableFunction";
 import { truncate } from "@/utils/mail";
-import { JSONSchema } from "openai/lib/jsonschema";
 
 export type CategoriseResponse = Awaited<ReturnType<typeof categorise>>;
 
-const aiResponseSchema = z.object({ category: z.string() });
-
-const categorizeFunctionSchema = z.object({
-  category: z.enum([
-    "NEWSLETTER",
-    "PROMOTIONAL",
-    "RECEIPT",
-    "ALERT",
-    "NOTIFICATION",
-    "FORUM",
-    "EVENT",
-    "TRAVEL",
-    "QUESTION",
-    "SUPPORT",
-    "COLD_EMAIL",
-    "SOCIAL_MEDIA",
-    "LEGAL_UPDATE",
-    "OTHER",
-  ]).describe(`The email category:
-
-* NEWSLETTER - emails that contain long-form articles, thought leadership, insights, or educational resources.
-* PROMOTIONAL - marketing or promotional emails to get users to engage with a product or service
-* RECEIPT - a receipt or invoice for payments I've made
-* ALERT - an alert or notification of error from a service I use
-* NOTIFICATION - updates regarding user activity, responses, form submissions, user interactions, or any other user-triggered events
-* FORUM - a message from someone on a forum, community, or discussion board
-* EVENT - calendar or event invites
-* TRAVEL - travel itineraries and confirmations
-* QUESTION - someone asks me a question
-* SUPPORT - support requests
-* COLD_EMAIL - someone I don't know emails me to sell me something
-* SOCIAL_MEDIA - social media notifications
-* LEGAL_UPDATE - updates about changes to terms of service or privacy policy
-* OTHER - anything else
-  `),
+const aiResponseSchema = z.object({
+  requiresMoreInformation: z.boolean(),
+  category: z
+    .enum([
+      "NEWSLETTER",
+      "PROMOTIONAL",
+      "RECEIPT",
+      "ALERT",
+      "NOTIFICATION",
+      "FORUM",
+      "EVENT",
+      "TRAVEL",
+      "QUESTION",
+      "SUPPORT",
+      "COLD_EMAIL",
+      "SOCIAL_MEDIA",
+      "LEGAL_UPDATE",
+      "OTHER",
+    ])
+    .optional(),
 });
 
-const categorizeFunctionJsonSchema = zodToJsonSchema(categorizeFunctionSchema);
+async function aiCategorise(
+  body: CategoriseBody & { content: string } & UserAIFields,
+  expanded: boolean,
+) {
+  console.log("snippet", body.snippet);
 
-export function getFunctions(options: { expandedEmail: string }) {
-  const functions: Record<string, RunnableFunctionWithParse<any>> = {
-    // We pass in a truncated snippet initially to try reduce tokens used
-    expandEmail: {
-      name: "expandEmail",
-      function: async (_args: {}) => {
-        return { email: options.expandedEmail };
-      },
-      description: "Expand the email if you do not have enough context.",
-      parse: (args: string) => z.object({}).parse(JSON.parse(args)),
-      parameters: { type: "object", properties: {} },
-    },
-    categorizeEmail: {
-      name: "categorizeEmail",
-      function: async (_args: z.infer<typeof categorizeFunctionSchema>) => {
-        // TODO return category as result?
+  const message = `Categorize this email.
+Return a JSON object with a "category" and "requiresMoreInformation" field.
 
-        // We grab this result from finalFunctionCall instead.
-        return { success: true };
-      },
-      description: "Categorize the email.",
-      parse: (args: string) => categorizeFunctionSchema.parse(JSON.parse(args)),
-      parameters: categorizeFunctionJsonSchema as JSONSchema,
-    },
-  };
+These are the categories to choose from, with an explanation of each one:
+NEWSLETTER - emails that contain long-form articles, thought leadership, insights, or educational resources.
+PROMOTIONAL - marketing or promotional emails to get users to engage with a product or service
+RECEIPT - a receipt or invoice for payments I've made
+ALERT - an alert or notification of error from a service I use
+NOTIFICATION - updates regarding user activity, responses, form submissions, user interactions, or any other user-triggered events
+FORUM - a message from someone on a forum, community, or discussion board
+EVENT - calendar or event invites
+TRAVEL - travel itineraries and confirmations
+QUESTION - someone asks me a question
+SUPPORT - support requests
+COLD_EMAIL - someone I don't know emails me to sell me something
+SOCIAL_MEDIA - social media notifications
+LEGAL_UPDATE - updates about changes to terms of service or privacy policy
+OTHER - anything else
 
-  return functions;
+We only provide you with a snippet of the email. If you need more information, set "requiresMoreInformation" to true and we will provide you with the full email to better categorize it.
+
+An example response would be:
+{
+  "category": "NEWSLETTER",
+  "requiresMoreInformation": false
 }
 
-async function aiCategorise(body: CategoriseBody & UserAIFields) {
-  const message = `Categorize this email.
+If the sender domain includes Beehiiv or Substack it probably is a newsletter.
 
-A mistake you often make is categorizing an email as NEWSLETTER when it is actually PROMOTIONAL or NOTIFICATION. Do not do this. If it is not a long-form article it is not a newsletter.
-
-###
 The email:
 
+###
 From: ${body.from}
 Subject: ${body.subject}
 Unsubscribe link: ${body.unsubscribeLink || "Not found"}
 Has emailed us before: ${body.hasPreviousEmail ? "yes" : "no"}
 
-Body:
-${body.snippet}
+Content:
+${expanded ? truncate(body.content, 2000) : body.snippet}
 ###
 `;
 
-  const runner = getOpenAI(
-    body.openAIApiKey,
-  ).beta.chat.completions.runFunctions({
+  const response = await getOpenAI(body.openAIApiKey).chat.completions.create({
     model: body.aiModel || DEFAULT_AI_MODEL,
+    response_format: { type: "json_object" },
     messages: [
       {
         role: "system",
-        content: `You are an assistant that categorizes emails.`,
+        content: "You are an assistant that helps categorize emails.",
       },
-      { role: "user", content: message },
+      {
+        role: "user",
+        content: message,
+      },
     ],
-    functions: Object.values(
-      getFunctions({ expandedEmail: truncate(body.content, 1000) }),
-    ),
-    temperature: 0,
-    frequency_penalty: 0,
   });
 
-  const finalCall = await runner.finalFunctionCall();
-  const content = finalCall?.arguments;
+  const content = response.choices[0].message.content;
 
   if (!content) return;
 
   try {
     const res = parseJSON(content);
-
     return aiResponseSchema.parse(res);
   } catch (error) {
     console.error("Error parsing json:", content);
@@ -131,7 +107,7 @@ ${body.snippet}
 }
 
 export async function categorise(
-  body: CategoriseBody & UserAIFields,
+  body: CategoriseBody & { content: string } & UserAIFields,
   options: { email: string },
 ) {
   // 1. check redis cache
@@ -139,21 +115,21 @@ export async function categorise(
     email: options.email,
     threadId: body.threadId,
   });
-
   if (existingCategory) return existingCategory;
-
   // 2. ai categorise
-  const category = await aiCategorise(body);
+  let category = await aiCategorise(body, false);
+  if (category?.requiresMoreInformation) {
+    console.log("Not enough information, expanding email and trying again");
+    category = await aiCategorise(body, true);
+  }
+
   console.log("category:", category);
-
-  if (!category) return;
-
+  if (!category?.category) return;
   // 3. save category
   await saveCategory({
     email: options.email,
     threadId: body.threadId,
-    category,
+    category: { category: category.category },
   });
-
   return category;
 }
