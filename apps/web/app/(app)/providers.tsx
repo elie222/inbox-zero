c5335ab7ed173c0f87d31c9a2066fba4059a758b
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-12-10 17:29:04 -0500

    disable unused gmail provider

--- apps/web/app/(app)/providers.tsx
+++ apps/web/app/(app)/providers.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { GmailProvider } from "@/providers/GmailProvider";
+// import { GmailProvider } from "@/providers/GmailProvider";
 import { SessionProvider } from "@/providers/SessionProvider";
 import { SWRProvider } from "@/providers/SWRProvider";
 
 export default function Providers(props: { children: React.ReactNode }) {
   return (
     <SWRProvider>
-      <GmailProvider>
-        <SessionProvider>{props.children}</SessionProvider>
-      </GmailProvider>
+      {/* <GmailProvider> */}
+      <SessionProvider>{props.children}</SessionProvider>
+      {/* </GmailProvider> */}
     </SWRProvider>
   );
 }
