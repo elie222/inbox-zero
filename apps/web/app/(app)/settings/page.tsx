ed7cee975bad473a4f029f9afcf5d4b4cf61f032
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-12-10 15:46:39 -0500

    disable unused labels settings for now

--- apps/web/app/(app)/settings/page.tsx
+++ apps/web/app/(app)/settings/page.tsx
@@ -1,6 +1,6 @@
 import { FormWrapper } from "@/components/Form";
 import { AboutSection } from "@/app/(app)/settings/AboutSection";
-import { LabelsSection } from "@/app/(app)/settings/LabelsSection";
+// import { LabelsSection } from "@/app/(app)/settings/LabelsSection";
 import { DeleteSection } from "@/app/(app)/settings/DeleteSection";
 import { ModelSection } from "@/app/(app)/settings/ModelSection";
 import { EmailUpdatesSection } from "@/app/(app)/settings/EmailUpdatesSection";
@@ -9,7 +9,7 @@ export default function Settings() {
   return (
     <FormWrapper>
       <AboutSection />
-      <LabelsSection />
+      {/* <LabelsSection /> */}
       <ModelSection />
       <EmailUpdatesSection />
       <DeleteSection />
