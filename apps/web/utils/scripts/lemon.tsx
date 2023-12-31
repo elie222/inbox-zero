e2645d14aa719dc33d452175157466dfe473aec6
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-12-31 15:15:25 -0500

    Fix lemon affiliate tracking

--- apps/web/utils/scripts/lemon.tsx
+++ apps/web/utils/scripts/lemon.tsx
@@ -1,23 +1,16 @@
-"use client";
-
-import Script from "next/script";
 import { env } from "@/env.mjs";
 
 export function LemonScript() {
   if (!env.NEXT_PUBLIC_LEMON_STORE_ID) return null;
 
   return (
-    <Script
-      src="https://lmsqueezy.com/affiliate.js"
-      defer
-      strategy="lazyOnload"
-      onLoad={() => {
-        if (window) {
-          (window as any).lemonSqueezyAffiliateConfig = {
-            store: env.NEXT_PUBLIC_LEMON_STORE_ID,
-          };
-        }
-      }}
-    />
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.lemonSqueezyAffiliateConfig = { store: "${env.NEXT_PUBLIC_LEMON_STORE_ID}", debug: true };`,
+        }}
+      />
+      <script src="https://lmsqueezy.com/affiliate.js" defer />
+    </>
   );
 }
