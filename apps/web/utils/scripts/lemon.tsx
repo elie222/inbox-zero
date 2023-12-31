c1ddd991d29778fdfe56e90df533ecb534651209
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2023-12-31 17:10:11 -0500

    try fix lemon affiliate click tracking

--- apps/web/utils/scripts/lemon.tsx
+++ apps/web/utils/scripts/lemon.tsx
@@ -3,11 +3,25 @@ import { env } from "@/env.mjs";
 export function LemonScript() {
   if (!env.NEXT_PUBLIC_LEMON_STORE_ID) return null;
 
+  console.log("Loading lemon script");
+
   return (
     <>
       <script
         dangerouslySetInnerHTML={{
-          __html: `window.lemonSqueezyAffiliateConfig = { store: "${env.NEXT_PUBLIC_LEMON_STORE_ID}", debug: true };`,
+          __html: `window.lemonSqueezyAffiliateConfig = {
+            store: "${env.NEXT_PUBLIC_LEMON_STORE_ID}",
+            debug: true,
+            trackOnLoad: false,
+            onReady: function (e) {
+              const url = new URL(window.location.href);
+              const affiliateCode = url.searchParams.get('aff');
+              e.Track(affiliateCode);
+            },
+            onTrack: function (e) {
+              console.log("onTrack", e);
+            }
+          };`,
         }}
       />
       <script src="https://lmsqueezy.com/affiliate.js" defer />
