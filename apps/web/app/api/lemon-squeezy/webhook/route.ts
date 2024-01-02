9457a5af838aece643b474dfaead2a800f80db4e
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-01-02 13:20:52 -0500

    Fix up pricing to give more control to admin

--- apps/web/app/api/lemon-squeezy/webhook/route.ts
+++ apps/web/app/api/lemon-squeezy/webhook/route.ts
@@ -4,6 +4,12 @@ import prisma from "@/utils/prisma";
 import { withError } from "@/utils/middleware";
 import { env } from "@/env.mjs";
 import { posthogCaptureEvent } from "@/utils/posthog";
+import {
+  cancelUserPremium,
+  extendUserPremium,
+  upgradeUserToPremium,
+} from "@/utils/premium/server";
+import { Payload } from "@/app/api/lemon-squeezy/webhook/types";
 
 // https://docs.lemonsqueezy.com/help/webhooks#signing-requests
 // https://gist.github.com/amosbastian/e403e1d8ccf4f7153f7840dd11a85a69
@@ -24,36 +30,46 @@ export const POST = withError(async (request: Request) => {
 
   const payload: Payload = JSON.parse(text);
 
-  if (
-    payload.meta.event_name === "subscription_created" ||
-    payload.meta.event_name === "order_created"
-  ) {
-    const userId = payload.meta.custom_data?.user_id;
+  const userId = payload.meta.custom_data?.user_id;
 
-    // Check if custom defined data i.e. the `userId` is there or not
+  // monthly/annual subscription
+  if (payload.meta.event_name === "subscription_created") {
+    if (!userId) throw new Error("No userId provided");
+    if (!payload.data.attributes.renews_at)
+      throw new Error("No renews_at provided");
+
+    const lemonSqueezyRenewsAt = new Date(payload.data.attributes.renews_at);
+
+    const updatedUser = await upgradeUserToPremium({
+      userId,
+      isLifetime: false,
+      lemonSqueezyRenewsAt,
+      lemonSqueezySubscriptionId: payload.data.id,
+      lemonSqueezyCustomerId: payload.data.attributes.customer_id,
+    });
+
+    if (updatedUser.email) {
+      await posthogCaptureEvent(
+        updatedUser.email,
+        "Upgraded to premium",
+        payload.data.attributes,
+      );
+    }
+
+    return NextResponse.json({ ok: true });
+  }
+
+  // lifetime plan
+  const productId = payload.data.attributes.first_order_item?.product_id;
+  const isLifetimePlan = productId === env.NEXT_PUBLIC_LIFETIME_PLAN_ID;
+  if (payload.meta.event_name === "order_created" && isLifetimePlan) {
     if (!userId) throw new Error("No userId provided");
 
-    const lemonSqueezySubscriptionId = payload.data.id;
-    const lemonSqueezyCustomerId = payload.data.attributes.customer_id;
-    const productId = payload.data.attributes.first_order_item?.product_id;
-
-    const TEN_YEARS = 10 * 365 * 24 * 60 * 60 * 1000;
-
-    const lemonSqueezyRenewsAt =
-      productId === env.NEXT_PUBLIC_LIFETIME_PLAN_ID
-        ? new Date(Date.now() + TEN_YEARS)
-        : payload.data.attributes.renews_at
-          ? new Date(payload.data.attributes.renews_at)
-          : undefined;
-
-    const updatedUser = await prisma.user.update({
-      where: { id: userId },
-      data: {
-        lemonSqueezyRenewsAt,
-        lemonSqueezySubscriptionId,
-        lemonSqueezyCustomerId,
-      },
-      select: { email: true },
+    const updatedUser = await upgradeUserToPremium({
+      userId,
+      isLifetime: true,
+      lemonSqueezySubscriptionId: payload.data.id,
+      lemonSqueezyCustomerId: payload.data.attributes.customer_id,
     });
 
     if (updatedUser.email) {
@@ -75,15 +91,14 @@ export const POST = withError(async (request: Request) => {
 
   if (!user) throw new Error("No user found for lemonSqueezyCustomerId");
 
+  // renewal
   if (payload.meta.event_name === "subscription_payment_success") {
-    const updatedUser = await prisma.user.update({
-      where: { id: user.id },
-      data: {
-        lemonSqueezyRenewsAt: payload.data.attributes.renews_at
-          ? new Date(payload.data.attributes.renews_at)
-          : undefined,
-      },
-      select: { email: true },
+    if (!payload.data.attributes.renews_at)
+      throw new Error("No renews_at provided");
+
+    const updatedUser = await extendUserPremium({
+      userId: user.id,
+      lemonSqueezyRenewsAt: new Date(payload.data.attributes.renews_at),
     });
 
     if (updatedUser.email) {
@@ -93,13 +108,15 @@ export const POST = withError(async (request: Request) => {
         payload.data.attributes,
       );
     }
-  } else if (payload.data.attributes.ends_at) {
-    const updatedUser = await prisma.user.update({
-      where: { id: user.id },
-      data: {
-        lemonSqueezyRenewsAt: payload.data.attributes.ends_at,
-      },
-      select: { email: true },
+
+    return NextResponse.json({ ok: true });
+  }
+
+  // cancellation
+  if (payload.data.attributes.ends_at) {
+    const updatedUser = await cancelUserPremium({
+      userId: user.id,
+      lemonSqueezyEndsAt: new Date(payload.data.attributes.ends_at),
     });
 
     if (updatedUser.email) {
@@ -113,180 +130,3 @@ export const POST = withError(async (request: Request) => {
 
   return NextResponse.json({ ok: true });
 });
-
-export interface Payload {
-  meta: Meta;
-  data: Data;
-}
-
-type EventName =
-  | "order_created"
-  | "order_refunded"
-  | "subscription_created"
-  | "subscription_cancelled"
-  | "subscription_resumed"
-  | "subscription_expired"
-  | "subscription_paused"
-  | "subscription_unpaused"
-  | "subscription_payment_failed"
-  | "subscription_payment_success"
-  | "subscription_payment_recovered";
-
-export interface Meta {
-  test_mode: boolean;
-  event_name: EventName;
-  custom_data?: { user_id: string };
-}
-
-export interface Data {
-  type: string;
-  id: string;
-  attributes: Attributes;
-  relationships: Relationships;
-  links: Links9;
-}
-
-export interface Attributes {
-  store_id: number;
-  customer_id: number;
-  order_id: number;
-  order_item_id: number;
-  product_id: number;
-  variant_id: number;
-  product_name: string;
-  variant_name: string;
-  user_name: string;
-  user_email: string;
-  status: string;
-  status_formatted: string;
-  card_brand: string;
-  card_last_four: string;
-  pause: any;
-  cancelled: boolean;
-  trial_ends_at: any;
-  billing_anchor: number;
-  first_subscription_item: FirstSubscriptionItem;
-  urls: Urls;
-  renews_at?: string;
-  ends_at?: string;
-  created_at: string;
-  updated_at: string;
-  test_mode: boolean;
-  first_order_item?: FirstOrderItem;
-}
-
-export interface FirstSubscriptionItem {
-  id: number;
-  subscription_id: number;
-  price_id: number;
-  quantity: number;
-  is_usage_based: boolean;
-  created_at: string;
-  updated_at: string;
-}
-
-export interface Urls {
-  update_payment_method: string;
-}
-
-export interface Relationships {
-  store: Store;
-  customer: Customer;
-  order: Order;
-  "order-item": OrderItem;
-  product: Product;
-  variant: Variant;
-  "subscription-items": SubscriptionItems;
-  "subscription-invoices": SubscriptionInvoices;
-}
-
-export interface Store {
-  links: Links;
-}
-
-export interface Links {
-  related: string;
-  self: string;
-}
-
-export interface Customer {
-  links: Links2;
-}
-
-export interface Links2 {
-  related: string;
-  self: string;
-}
-
-export interface Order {
-  links: Links3;
-}
-
-export interface Links3 {
-  related: string;
-  self: string;
-}
-
-export interface OrderItem {
-  links: Links4;
-}
-
-export interface Links4 {
-  related: string;
-  self: string;
-}
-
-export interface Product {
-  links: Links5;
-}
-
-export interface Links5 {
-  related: string;
-  self: string;
-}
-
-export interface Variant {
-  links: Links6;
-}
-
-export interface Links6 {
-  related: string;
-  self: string;
-}
-
-export interface SubscriptionItems {
-  links: Links7;
-}
-
-export interface Links7 {
-  related: string;
-  self: string;
-}
-
-export interface SubscriptionInvoices {
-  links: Links8;
-}
-
-export interface Links8 {
-  related: string;
-  self: string;
-}
-
-export interface Links9 {
-  self: string;
-}
-
-interface FirstOrderItem {
-  id: number;
-  order_id: number;
-  product_id: number;
-  variant_id: number;
-  price_id: number;
-  product_name: string;
-  variant_name: string;
-  price: number;
-  quantity: number;
-  created_at: string;
-  updated_at: string;
-  test_mode: boolean;
-}
