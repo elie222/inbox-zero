8df95d530e192766a590f338406808518dcab35b
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-01-02 18:10:42 -0500

    better error handling

--- apps/web/utils/middleware.ts
+++ apps/web/utils/middleware.ts
@@ -1,7 +1,8 @@
 import { ZodError } from "zod";
 import { NextRequest, NextResponse } from "next/server";
-import { captureException } from "@/utils/error";
+import { GaxiosError } from "gaxios";
 import { StreamingTextResponse } from "ai";
+import { captureException } from "@/utils/error";
 import { env } from "@/env.mjs";
 
 export type NextHandler = (
@@ -25,14 +26,30 @@ export function withError(handler: NextHandler): NextHandler {
         );
       }
 
-      if ((error as any)?.errors?.[0]?.reason === "insufficientPermissions") {
-        return NextResponse.json(
-          {
-            error:
-              "You must grant all Gmail permissions to use the app. Please log out and log in again to grant permissions.",
-          },
-          { status: 403 },
-        );
+      if (error instanceof GaxiosError) {
+        if ((error as any)?.errors?.[0]?.reason === "insufficientPermissions") {
+          return NextResponse.json(
+            {
+              error:
+                "You must grant all Gmail permissions to use the app. Please log out and log in again to grant permissions.",
+            },
+            { status: 403 },
+          );
+        }
+
+        if ((error as any)?.errors?.[0]?.reason === "rateLimitExceeded") {
+          return NextResponse.json(
+            {
+              error:
+                "Gmail rate limit exceeded. Please try refresh the page shortly.",
+            },
+            { status: 403 },
+          );
+        }
+      }
+
+      if (isErrorWithConfigAndHeaders(error)) {
+        delete error.config.headers;
       }
 
       captureException(error, { extra: { url: req.url, params } });
@@ -41,4 +58,15 @@ export function withError(handler: NextHandler): NextHandler {
       return NextResponse.json({ error: "Server error" }, { status: 500 });
     }
   };
+}
+
+function isErrorWithConfigAndHeaders(
+  error: unknown,
+): error is { config: { headers: unknown } } {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "config" in error &&
+    "headers" in (error as { config: any }).config
+  );
 }
