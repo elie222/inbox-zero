c32e7e60522b7ce0d7fc96e17e035170e5cbf763
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-01-02 18:13:28 -0500

    remove gaxios check that could cause issues

--- apps/web/utils/middleware.ts
+++ apps/web/utils/middleware.ts
@@ -1,6 +1,5 @@
 import { ZodError } from "zod";
 import { NextRequest, NextResponse } from "next/server";
-import { GaxiosError } from "gaxios";
 import { StreamingTextResponse } from "ai";
 import { captureException } from "@/utils/error";
 import { env } from "@/env.mjs";
@@ -26,26 +25,24 @@ export function withError(handler: NextHandler): NextHandler {
         );
       }
 
-      if (error instanceof GaxiosError) {
-        if ((error as any)?.errors?.[0]?.reason === "insufficientPermissions") {
-          return NextResponse.json(
-            {
-              error:
-                "You must grant all Gmail permissions to use the app. Please log out and log in again to grant permissions.",
-            },
-            { status: 403 },
-          );
-        }
+      if ((error as any)?.errors?.[0]?.reason === "insufficientPermissions") {
+        return NextResponse.json(
+          {
+            error:
+              "You must grant all Gmail permissions to use the app. Please log out and log in again to grant permissions.",
+          },
+          { status: 403 },
+        );
+      }
 
-        if ((error as any)?.errors?.[0]?.reason === "rateLimitExceeded") {
-          return NextResponse.json(
-            {
-              error:
-                "Gmail rate limit exceeded. Please try refresh the page shortly.",
-            },
-            { status: 403 },
-          );
-        }
+      if ((error as any)?.errors?.[0]?.reason === "rateLimitExceeded") {
+        return NextResponse.json(
+          {
+            error:
+              "Gmail rate limit exceeded. Please try refresh the page shortly.",
+          },
+          { status: 403 },
+        );
       }
 
       if (isErrorWithConfigAndHeaders(error)) {
