1e90b8161384cd8ff83da86eb9ee31fcaf5245f9
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-01-14 17:24:41 -0500

    disable purchase link if user has lifetime plan

--- apps/web/app/(app)/premium/Pricing.tsx
+++ apps/web/app/(app)/premium/Pricing.tsx
@@ -294,6 +294,8 @@ function LifetimePricing(props: {
   affiliateCode: string | null;
   premiumTier?: PremiumTier | null;
 }) {
+  const hasLifetime = props.premiumTier === PremiumTier.LIFETIME;
+
   return (
     <div className="bg-white py-4 sm:py-8">
       <div className="mx-auto max-w-2xl rounded-3xl ring-1 ring-gray-200 lg:mx-0 lg:flex lg:max-w-none">
@@ -345,16 +347,18 @@ function LifetimePricing(props: {
                 </span>
               </p>
               <a
-                href={buildLemonUrl(
-                  attachUserId(LIFETIME_LINK, props.userId),
-                  props.affiliateCode,
-                )}
+                href={
+                  hasLifetime
+                    ? "#"
+                    : buildLemonUrl(
+                        attachUserId(LIFETIME_LINK, props.userId),
+                        props.affiliateCode,
+                      )
+                }
                 target="_blank"
                 className="mt-10 block w-full rounded-md bg-blue-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
-                {props.premiumTier === PremiumTier.LIFETIME
-                  ? "Current plan"
-                  : "Get lifetime access"}
+                {hasLifetime ? "Current plan" : "Get lifetime access"}
               </a>
               <p className="mt-6 text-xs leading-5 text-gray-600">
                 Invoices and receipts available for easy company reimbursement
