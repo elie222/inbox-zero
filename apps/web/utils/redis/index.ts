0d7e4dcff5ad52f7b5bf363102c0d052a3addd13
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-01-18 14:10:06 -0500

    POC of Redis without using Upstash

--- apps/web/utils/redis/index.ts
+++ apps/web/utils/redis/index.ts
@@ -1,7 +1,25 @@
 import { env } from "@/env.mjs";
-import { Redis } from "@upstash/redis";
 
-export const redis = new Redis({
-  url: env.UPSTASH_REDIS_URL,
-  token: env.UPSTASH_REDIS_TOKEN,
-});
+// Function to dynamically import Redis based on user preference or environment variable
+async function getRedisClient(): Promise<{
+  get: (key: string) => Promise<any>;
+  set: (key: string, value: any) => Promise<any>;
+  del: (key: string) => Promise<any>;
+  // hgetall: (key: string) => Promise<any>;
+  // hset: (key: string, value: any) => Promise<any>;
+}> {
+  if (env.UPSTASH_REDIS_TOKEN) {
+    const { Redis } = await import("@upstash/redis");
+    return new Redis({
+      url: env.REDIS_URL,
+      token: env.UPSTASH_REDIS_TOKEN,
+    });
+  } else {
+    const { createClient } = await import("redis");
+    const RedisClient = createClient({ url: env.REDIS_URL });
+    await RedisClient.connect();
+    return RedisClient;
+  }
+}
+
+export const redis = await getRedisClient();
