1addc69fbb51b05dc184602ac68b0372bf6e98bb
Priyank Rajai <priyank73@hotmail.com>
2024-02-09 08:51:22 +0530

    refactor: env REDIS_URL  to UPSTASH_REDIS_URL

--- apps/web/utils/redis/index.ts
+++ apps/web/utils/redis/index.ts
@@ -2,6 +2,6 @@ import { env } from "@/env.mjs";
 import { Redis } from "@upstash/redis";
 
 export const redis = new Redis({
-  url: env.REDIS_URL,
-  token: env.REDIS_TOKEN,
+  url: env.UPSTASH_REDIS_URL,
+  token: env.UPSTASH_REDIS_TOKEN,
 });
