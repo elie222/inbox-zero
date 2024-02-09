9e17969f72db8b3eb8c0af7f9b87f869de56b23f
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-02-09 15:49:00 -0500

    update sitemap

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -32,5 +32,23 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://www.getinboxzero.com/blog/post/how-my-open-source-saas-hit-first-on-product-hunt",
     },
+    {
+      url: "https://www.getinboxzero.com/blog/post/why-build-an-open-source-saas",
+    },
+    {
+      url: "https://docs.getinboxzero.com/",
+    },
+    {
+      url: "https://docs.getinboxzero.com/introduction",
+    },
+    {
+      url: "https://docs.getinboxzero.com/essentials/email-ai-automation",
+    },
+    {
+      url: "https://docs.getinboxzero.com/essentials/bulk-email-unsubscriber",
+    },
+    {
+      url: "https://docs.getinboxzero.com/essentials/cold-email-blocker",
+    },
   ];
 }
