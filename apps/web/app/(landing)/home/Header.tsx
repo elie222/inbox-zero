215bf894a74c46b6ed28aad159e11ef46731a4c6
rohitGupta2022 <pta.rohit28@gmail.com>
2024-02-14 23:35:07 +0530

    Fix: header navigation in mobile
    In mobile view, after opening the menu dialog when clicking on any of the links the menu dialog does not close.
    this comit fixes that!

--- apps/web/app/(landing)/home/Header.tsx
+++ apps/web/app/(landing)/home/Header.tsx
@@ -93,6 +93,7 @@ export function Header() {
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </Link>
