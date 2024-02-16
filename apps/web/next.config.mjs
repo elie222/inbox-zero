b1023ce7f69d6977a664a2064df3b7ea857126c8
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-02-16 16:28:14 -0500

    Delete instagram shortlink

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -61,11 +61,6 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "/instagram",
-        destination: "https://www.instagram.com/inboxzero_ai/",
-        permanent: true,
-      },
-      {
         source: "/waitlist",
         destination: "https://airtable.com/shr7HNx6FXaIxR5q6",
         permanent: true,
