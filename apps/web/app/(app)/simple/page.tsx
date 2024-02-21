3d1708a85690055279951273b960e07efa72f89b
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-02-21 01:09:46 -0500

    load 5 emails in simple view

--- apps/web/app/(app)/simple/page.tsx
+++ apps/web/app/(app)/simple/page.tsx
@@ -1,64 +1,68 @@
+// import { BookmarkPlusIcon } from "lucide-react";
+import { auth } from "@/app/api/auth/[...nextauth]/auth";
 import { Button } from "@/components/Button";
 import { PageHeading } from "@/components/Typography";
-import { BookmarkPlusIcon } from "lucide-react";
+import { extractNameFromEmail } from "@/utils/email";
+import { getGmailAccessToken, getGmailClient } from "@/utils/gmail/client";
+import { decodeSnippet } from "@/utils/gmail/decode";
+import { parseMessage } from "@/utils/mail";
+import { MessageWithPayload } from "@/utils/types";
 
-const emails = [
-  {
-    id: 1,
-    from: "The Rundown AI",
-    subject: "🎥 OpenAI changes the AI video world!",
-    snippet:
-      "Google came out hot with another new Gemini upgrade — but OpenAI countered with a stealth drop that just elevated AI video to a whole new level. Let’s get into it.",
-  },
-  {
-    id: 2,
-    from: "The Rundown AI",
-    subject: "🎥 OpenAI changes the AI video world!",
-    snippet:
-      "Google came out hot with another new Gemini upgrade — but OpenAI countered with a stealth drop that just elevated AI video to a whole new level. Let’s get into it.",
-  },
-  {
-    id: 3,
-    from: "The Rundown AI",
-    subject: "🎥 OpenAI changes the AI video world!",
-    snippet:
-      "Google came out hot with another new Gemini upgrade — but OpenAI countered with a stealth drop that just elevated AI video to a whole new level. Let’s get into it.",
-  },
-  {
-    id: 4,
-    from: "The Rundown AI",
-    subject: "🎥 OpenAI changes the AI video world!",
-    snippet:
-      "Google came out hot with another new Gemini upgrade — but OpenAI countered with a stealth drop that just elevated AI video to a whole new level. Let’s get into it.",
-  },
-  {
-    id: 5,
-    from: "The Rundown AI",
-    subject: "🎥 OpenAI changes the AI video world!",
-    snippet:
-      "Google came out hot with another new Gemini upgrade — but OpenAI countered with a stealth drop that just elevated AI video to a whole new level. Let’s get into it.",
-  },
-];
+export default async function SimplePage() {
+  const session = await auth();
+  const email = session?.user.email;
+  if (!email) throw new Error("Not authenticated");
 
-export default function SimplePage() {
+  const gmail = getGmailClient(session);
+  const token = await getGmailAccessToken(session);
+  const accessToken = token?.token;
+
+  if (!accessToken) throw new Error("Missing access token");
+
+  const response = await gmail.users.messages.list({
+    userId: "me",
+    labelIds: ["CATEGORY_PROMOTIONS"],
+    maxResults: 5,
+    q: `newer_than:1d`,
+  });
+
+  const messages = await Promise.all(
+    response.data.messages?.map(async (message) => {
+      const fullMessage = await gmail.users.messages.get({
+        userId: "me",
+        id: message.id!,
+      });
+      const parsedMessage = parseMessage(
+        fullMessage.data as MessageWithPayload,
+      );
+      return parsedMessage;
+    }) || [],
+  );
+
   return (
     <div className="mx-auto max-w-2xl py-10">
       <PageHeading className="text-center">Today{`'`}s newsletters</PageHeading>
 
       <div className="mt-8 grid gap-4">
-        {emails.map((email) => (
-          <div key={email.id} className="bg-white p-4 shadow sm:rounded-lg">
-            <div className="flex items-center">
-              <div className="font-bold">{email.from}</div>
-              <div className="ml-4 mr-4">{email.subject}</div>
-              <Button className="ml-auto" color="white" size="sm">
-                <BookmarkPlusIcon className="mr-2 h-4 w-4" />
-                Read Later
-              </Button>
+        {messages.map((message) => {
+          return (
+            <div key={message.id} className="bg-white p-4 shadow sm:rounded-lg">
+              <div className="flex">
+                <div className="whitespace-nowrap font-bold">
+                  {extractNameFromEmail(message.headers.from)}
+                </div>
+                <div className="ml-4 mr-4">{message.headers.subject}</div>
+                {/* <Button className="ml-auto" color="white" size="sm">
+                  <BookmarkPlusIcon className="mr-2 h-4 w-4" />
+                  Read Later
+                </Button> */}
+              </div>
+              <div className="mt-2 text-sm text-gray-700">
+                {decodeSnippet(message.snippet).replace(/\u200C/g, "")}
+              </div>
             </div>
-            <div className="mt-2 text-sm text-gray-700">{email.snippet}</div>
-          </div>
-        ))}
+          );
+        })}
       </div>
 
       <div className="mt-8 flex justify-center">
