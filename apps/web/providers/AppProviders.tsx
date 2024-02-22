062a2d69ebe389fa2dbf6fdfad28dcbcde37023f
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-02-22 17:52:21 -0500

    use local storage to persist action queue

--- apps/web/providers/AppProviders.tsx
+++ apps/web/providers/AppProviders.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Provider } from "jotai";
 import { ComposeModalProvider } from "@/providers/ComposeModalProvider";
+import { QueueProvider } from "@/providers/QueueProvider";
 
 export function AppProviders(props: { children: React.ReactNode }) {
   return (
     <Provider>
-      <ComposeModalProvider>{props.children}</ComposeModalProvider>
+      <QueueProvider>
+        <ComposeModalProvider>{props.children}</ComposeModalProvider>
+      </QueueProvider>
     </Provider>
   );
 }
