ae4de013944094a6cafbbeefe46afb266d517356
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-02-27 08:21:00 -0500

    add more ctas

--- apps/web/utils/parse/cta.ts
+++ apps/web/utils/parse/cta.ts
@@ -1,11 +1,12 @@
 const ctaKeywords = [
   "see more", // eg. "See more details"
   "view it", // eg. "View it on GitHub"
+  "view reply",
+  "view comment",
+  "view in", // eg. "View in Airtable"
   "confirm", // eg. "Confirm subscription"
   "join the conversation", // eg. LinkedIn
   "go to console",
-  "view reply",
-  "view comment",
 ];
 
 export function containsCtaKeyword(text: string) {
