207d29a3746f0f2208bdbc5b2d6aca675defbb58
ketan mehta <meetketanmehta@gmail.com>
2024-03-03 19:00:14 +0530

    improved code for celebration

--- apps/web/components/Celebration.tsx
+++ apps/web/components/Celebration.tsx
@@ -11,77 +11,62 @@ export function Celebration(props: { type: string }) {
     setActive(true);
   }, []);
 
-  if (props.type == "draft") {
-    return (
-      <>
-        <div className="mt-20 flex items-center justify-center font-cal text-2xl text-gray-900">
-          You don't have any saved drafts.
-        </div>
-      </>
-    );
-  }
-  if (props.type == "sent") {
-    return (
-      <>
-        <div className="mt-20 flex items-center justify-center font-cal text-2xl text-gray-900">
-          You haven't sent any emails yet.
-        </div>
-      </>
-    );
-  }
-  if (props.type == "archive") {
-    return (
-      <>
-        <div className="mt-20 flex items-center justify-center font-cal text-2xl text-gray-900">
-          No emails have been archived.
-        </div>
-      </>
-    );
-  }
   return (
     <>
       <div className="mt-20 flex items-center justify-center font-cal text-2xl text-gray-900">
-        Congrats! You made it to Inbox Zero!
+        {props.type === "inbox"
+          ? "You made it to Inbox Zero!"
+          : props.type === "draft"
+            ? "You don't have any saved drafts."
+            : props.type === "sent"
+              ? "You haven't sent any emails yet."
+              : props.type === "archive"
+                ? "No emails have been archived."
+                : "All emails handled!"}
       </div>
-      <div className="flex items-center justify-center">
-        <Confetti
-          active={active}
-          config={{
-            duration: 5_000,
-            elementCount: 500,
-            spread: 200,
-          }}
-        />
-      </div>
+      {props.type === "inbox" && (
+        <>
+          <div className="flex items-center justify-center">
+            <Confetti
+              active={active}
+              config={{
+                duration: 5_000,
+                elementCount: 500,
+                spread: 200,
+              }}
+            />
+          </div>
 
-      <div className="mt-8 flex justify-center">
-        <Button
-          size="2xl"
-          onClick={() => {
-            const tweet = encodeURIComponent(
-              "I made it to Inbox Zero thanks to @inboxzero_ai!",
-            );
-            const twitterIntentURL = `https://x.com/intent/tweet?text=${tweet}`;
-            window.open(
-              twitterIntentURL,
-              "_blank",
-              "noopener,noreferrer,width=550,height=420",
-            );
-          }}
-        >
-          Share on Twitter
-        </Button>
-      </div>
+          <div className="mt-8 flex justify-center">
+            <Button
+              size="2xl"
+              onClick={() => {
+                const tweet = encodeURIComponent(
+                  "I made it to Inbox Zero thanks to @inboxzero_ai!",
+                );
+                const twitterIntentURL = `https://x.com/intent/tweet?text=${tweet}`;
+                window.open(
+                  twitterIntentURL,
+                  "_blank",
+                  "noopener,noreferrer,width=550,height=420",
+                );
+              }}
+            >
+              Share on Twitter
+            </Button>
+          </div>
 
-      <div className="mt-8 flex items-center justify-center">
-        <Image
-          src={getCelebrationImage()}
-          width={400}
-          height={400}
-          alt="Congrats!"
-          unoptimized
-        />
-      </div>
+          <div className="mt-8 flex items-center justify-center">
+            <Image
+              src={getCelebrationImage()}
+              width={400}
+              height={400}
+              alt="Congrats!"
+              unoptimized
+            />
+          </div>
+        </>
+      )}
     </>
   );
 }
