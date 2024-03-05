3acfa0b0da0f87aacc051a7b642870afb31dd3c1
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-03-05 14:17:45 -0500

    fix up bottom chat

--- apps/web/app/(app)/chat/page.tsx
+++ apps/web/app/(app)/chat/page.tsx
@@ -51,7 +51,7 @@ export default function Page() {
   }, [inputRef]);
 
   return (
-    <div>
+    <div className="relative h-screen">
       <div className="pb-[200px] pt-4 md:pt-10">
         {messages.length ? (
           <>
@@ -80,7 +80,7 @@ export default function Page() {
         )}
         <ChatScrollAnchor trackVisibility={true} />
       </div>
-      <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
+      <div className="absolute inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
         <div className="mx-auto sm:max-w-2xl sm:px-4">
           <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
             <form
