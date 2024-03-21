bef6cffb7567f34cf9595614fcf070dafe28562a
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-03-21 13:25:36 +0200

    Fix migration

--- apps/web/prisma/migrations/20240319151146_unique_executed_rule/migration.sql
+++ apps/web/prisma/migrations/20240319151146_unique_executed_rule/migration.sql
@@ -6,19 +6,19 @@
 */
 
 -- Delete duplicate entries
-SELECT
-  "userId",
-  "threadId",
-  "messageId",
-  COUNT(*)
-FROM
-  "ExecutedRule"
-GROUP BY
-  "userId",
-  "threadId",
-  "messageId"
-HAVING
-  COUNT(*) > 1;
+DELETE FROM "ExecutedRule"
+WHERE id IN (
+  SELECT id
+  FROM (
+    SELECT id, 
+      ROW_NUMBER() OVER (
+        PARTITION BY "userId", "threadId", "messageId"
+        ORDER BY id
+      ) AS row_num
+    FROM "ExecutedRule"
+  ) t
+  WHERE t.row_num > 1
+);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "ExecutedRule_userId_threadId_messageId_key" ON "ExecutedRule"("userId", "threadId", "messageId");
