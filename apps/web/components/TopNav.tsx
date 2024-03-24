58c60dca28a68f786f9d20a67dc028faf376010f
Deeptanshu Sankhwar <deeptanshu@walletsocket.com>
2024-03-24 08:18:01 +0530

    Make the navigation happening at top nav bar, to use next js <Link>

--- apps/web/components/TopNav.tsx
+++ apps/web/components/TopNav.tsx
@@ -5,6 +5,7 @@ import { Menu, Transition } from "@headlessui/react";
 import { Button } from "@/components/Button";
 import { ChevronDownIcon, MenuIcon } from "lucide-react";
 import { logOut } from "@/utils/user";
+import Link from "@/node_modules/next/link";
 
 const userNavigation = [
   { name: "Usage", href: "/usage" },
@@ -42,83 +43,78 @@ export function TopNav(props: { setSidebarOpen: (open: boolean) => void }) {
 function ProfileDropdown() {
   const { data: session, status } = useSession();
 
-  switch (status) {
-    case "authenticated":
-      if (session?.user) {
-        return (
-          <Menu as="div" className="relative">
-            <Menu.Button className="-m-1.5 flex items-center p-1.5">
-              <span className="sr-only">Open user menu</span>
-              {session.user.image ? (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img
-                  className="h-8 w-8 rounded-full bg-gray-50"
-                  src={session.user.image}
-                  alt="Profile"
-                />
-              ) : (
-                <div className="h-8 w-8 rounded-full bg-blue-500" />
-              )}
-              <span className="hidden lg:flex lg:items-center">
-                <span
-                  className="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                  aria-hidden="true"
-                >
-                  {session.user.name || "Account"}
-                </span>
-                <ChevronDownIcon
-                  className="ml-2 h-5 w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-              </span>
-            </Menu.Button>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
+  if (session?.user) {
+    return (
+      <Menu as="div" className="relative">
+        <Menu.Button className="-m-1.5 flex items-center p-1.5">
+          <span className="sr-only">Open user menu</span>
+          {session.user.image ? (
+            // eslint-disable-next-line @next/next/no-img-element
+            <img
+              className="h-8 w-8 rounded-full bg-gray-50"
+              src={session.user.image}
+              alt="Profile"
+            />
+          ) : (
+            <div className="h-8 w-8 rounded-full bg-blue-500" />
+          )}
+          <span className="hidden lg:flex lg:items-center">
+            <span
+              className="ml-4 text-sm font-semibold leading-6 text-gray-900"
+              aria-hidden="true"
             >
-              <Menu.Items className="absolute right-0 z-20 mt-2.5 w-52 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                <Menu.Item>
-                  <div className="truncate border-b border-gray-200 px-3 pb-2 text-sm">
-                    {session.user.email}
-                  </div>
-                </Menu.Item>
-                {userNavigation.map((item) => (
-                  <Menu.Item key={item.name}>
-                    {({ active }) => (
-                      <a
-                        href={item.href}
-                        className={clsx(
-                          active ? "bg-gray-50" : "",
-                          "block px-3 py-1 text-sm leading-6 text-gray-900",
-                        )}
-                        onClick={item.onClick}
-                      >
-                        {item.name}
-                      </a>
+              {session.user.name || "Account"}
+            </span>
+            <ChevronDownIcon
+              className="ml-2 h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </span>
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-20 mt-2.5 w-52 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+            <Menu.Item>
+              <div className="truncate border-b border-gray-200 px-3 pb-2 text-sm">
+                {session.user.email}
+              </div>
+            </Menu.Item>
+            {userNavigation.map((item) => (
+              <Menu.Item key={item.name}>
+                {({ active }) => (
+                  <Link
+                    href={item.href}
+                    className={clsx(
+                      active ? "bg-gray-50" : "",
+                      "block px-3 py-1 text-sm leading-6 text-gray-900",
                     )}
-                  </Menu.Item>
-                ))}
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        );
-      }
-
-    case "loading":
-      return (
-        <Button color="transparent" onClick={() => signIn()} loading={true}>
-          {}
-        </Button>
-      );
-
-    default:
-      <Button color="transparent" onClick={() => signIn()} loading={false}>
-        Sign in
-      </Button>;
+                    onClick={item.onClick}
+                  >
+                    {item.name}
+                  </Link>
+                )}
+              </Menu.Item>
+            ))}
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    );
   }
+
+  return (
+    <Button
+      color="transparent"
+      onClick={() => signIn()}
+      loading={status === "loading"}
+    >
+      Sign in
+    </Button>
+  );
 }
