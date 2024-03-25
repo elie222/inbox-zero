742ade2093fa093fba7a7b97e1072d3211483496
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-03-25 10:29:12 +0200

    Fix warning on client for mail beta banner

---
+++ apps/web/app/(app)/mail/BetaBanner.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useLocalStorage } from "usehooks-ts";
+import { Banner } from "@/components/Banner";
+
+export function BetaBanner() {
+  const [bannerVisible, setBannerVisible] = useLocalStorage<
+    boolean | undefined
+  >("mailBetaBannerVisibile", true);
+
+  if (bannerVisible && typeof window !== "undefined")
+    return (
+      <Banner
+        title="Beta"
+        description="Mail is currently in beta. It is not intended to be a full replacement for your email client yet."
+        onClose={() => setBannerVisible(false)}
+      />
+    );
+
+  return null;
+}
---
+++ apps/web/components/ClientOnly.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ReactNode, useEffect, useState } from "react";
+
+export const ClientOnly = ({ children }: { children: ReactNode }) => {
+  const [clientReady, setClientReady] = useState<boolean>(false);
+
+  useEffect(() => {
+    setClientReady(true);
+  }, []);
+
+  return clientReady ? <>{children}</> : null;
+};
