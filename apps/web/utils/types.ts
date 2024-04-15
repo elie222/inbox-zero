bc4ce54c26e0ba19d2277d791d0fa19c69452e4c
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-04-15 23:22:18 +0300

    Remove old code

--- apps/web/utils/types.ts
+++ apps/web/utils/types.ts
@@ -11,31 +11,6 @@ export function isDefined<T>(value: T | undefined | null): value is T {
 
 export type RuleWithActions = Rule & { actions: Action[] };
 
-// export type ChatCompletionResponse = {
-//   choices: { message: Message }[];
-//   usage: {
-//     prompt_tokens: number;
-//     completion_tokens: number;
-//     total_tokens: number;
-//   };
-// };
-
-// export type ChatCompletionError = {
-//   error: {
-//     message: string;
-//     type: "tokens" | "invalid_request_error"; // add more as needed
-//     param: string;
-//     code: "context_length_exceeded"; // add more as needed
-//   };
-// };
-
-// // typeguard to check if the response is an error
-// export function isChatCompletionError(
-//   response: ChatCompletionResponse | ChatCompletionError
-// ): response is ChatCompletionError {
-//   return !!(response as ChatCompletionError).error;
-// }
-
 export type MessageWithPayload = gmail_v1.Schema$Message & {
   payload: gmail_v1.Schema$MessagePart;
 };
