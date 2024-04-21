9b741709f2540e9d5d9189a0a4ed327e849fee21
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-04-21 23:40:03 +0300

    Improve receipts search

--- apps/web/utils/ai/group/find-receipts.ts
+++ apps/web/utils/ai/group/find-receipts.ts
@@ -2,10 +2,19 @@ import { gmail_v1 } from "googleapis";
 import uniq from "lodash/uniq";
 import { queryBatchMessagesPages } from "@/utils/gmail/message";
 
+const receiptSubjects = [
+  "invoice",
+  "receipt",
+  "payment",
+  '"purchase order"',
+  '"order confirmation"',
+  '"billing statement"',
+];
+
 // TODO this can be a mix of subject lines and from to find who actually sends receipts
 export async function findReceipts(gmail: gmail_v1.Gmail, accessToken: string) {
   const messages = await queryBatchMessagesPages(gmail, accessToken, {
-    query: "receipt",
+    query: `subject:(${receiptSubjects.join(" OR ")})`,
     maxResults: 100,
   });
 
