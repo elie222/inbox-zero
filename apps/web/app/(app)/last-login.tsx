0450d19fbe2282796fc055c93fa59cc7d998358c
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-04-24 14:12:43 +0300

    Prevent app crashing when no user

--- apps/web/app/(app)/last-login.tsx
+++ apps/web/app/(app)/last-login.tsx
@@ -1,10 +1,15 @@
+import { captureException } from "@/utils/error";
 import prisma from "@/utils/prisma";
 
 export async function LastLogin({ email }: { email: string }) {
-  await prisma.user.update({
-    where: { email },
-    data: { lastLogin: new Date() },
-  });
+  try {
+    await prisma.user.update({
+      where: { email },
+      data: { lastLogin: new Date() },
+    });
+  } catch (error) {
+    captureException(error);
+  }
 
   return null;
 }
