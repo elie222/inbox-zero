cad342459e2f6ff752846dbe59c5048438cdabe2
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-04-28 10:35:09 +0300

    Design clean up

--- apps/web/app/(app)/automation/rule/[ruleId]/page.tsx
+++ apps/web/app/(app)/automation/rule/[ruleId]/page.tsx
@@ -1,6 +1,5 @@
 import prisma from "@/utils/prisma";
 import { auth } from "@/app/api/auth/[...nextauth]/auth";
-import { TopSection } from "@/components/TopSection";
 import { UpdateRuleForm } from "@/app/(app)/automation/RuleModal";
 
 export default async function RulePage({
@@ -22,12 +21,8 @@ export default async function RulePage({
   if (!rule) throw new Error("Rule not found");
 
   return (
-    <div>
-      <TopSection title={rule.name} />
-
-      <div className="max-w-3xl px-4 py-6 sm:px-6 lg:px-8">
-        <UpdateRuleForm rule={rule} />
-      </div>
+    <div className="max-w-3xl px-4 sm:px-6 lg:px-8">
+      <UpdateRuleForm rule={rule} />
     </div>
   );
 }
