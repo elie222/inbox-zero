7f8b2a831ce01afe91a66dd3d88fbcd76e7fd114
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-04-30 22:54:34 +0300

    empty state

--- apps/web/app/(app)/automation/Rules.tsx
+++ apps/web/app/(app)/automation/Rules.tsx
@@ -6,7 +6,13 @@ import { MoreHorizontalIcon } from "lucide-react";
 import { RulesResponse } from "@/app/api/user/rules/controller";
 import { LoadingContent } from "@/components/LoadingContent";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -23,6 +29,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Tag } from "@/components/Tag";
+import Link from "next/link";
 
 export function Rules() {
   const { data, isLoading, error, mutate } = useSWR<
@@ -33,58 +40,75 @@ export function Rules() {
   return (
     <Card className="m-4">
       <LoadingContent loading={isLoading} error={error}>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Name</TableHead>
-              <TableHead>Instructions</TableHead>
-              <TableHead>Type</TableHead>
-              <TableHead>Actions</TableHead>
-              <TableHead className="text-center">Automated</TableHead>
-              <TableHead className="text-center">Run on Threads</TableHead>
-              {/* <TableHead className="text-right">Pending</TableHead>
+        {data?.length ? (
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>Instructions</TableHead>
+                <TableHead>Type</TableHead>
+                <TableHead>Actions</TableHead>
+                <TableHead className="text-center">Automated</TableHead>
+                <TableHead className="text-center">Run on Threads</TableHead>
+                {/* <TableHead className="text-right">Pending</TableHead>
               <TableHead className="text-right">Executed</TableHead> */}
-              <TableHead>
-                <span className="sr-only">User Actions</span>
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data?.map((rule) => (
-              <TableRow key={rule.id}>
-                <TableCell className="font-medium">{rule.name}</TableCell>
-                <TableCell>{rule.instructions}</TableCell>
-                <TableCell>{capitalCase(rule.type)}</TableCell>
-                <TableCell>
-                  <Actions actions={rule.actions} />
-                </TableCell>
-                <TableCell className="text-center">
-                  {rule.automate ? "Yes" : "No"}
-                </TableCell>
-                <TableCell className="text-center">
-                  {rule.runOnThreads ? "Yes" : "No"}
-                </TableCell>
-                {/* <TableCell className="text-right">33</TableCell>
-                <TableCell className="text-right">43</TableCell> */}
-                <TableCell>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Button aria-haspopup="true" size="icon" variant="ghost">
-                        <MoreHorizontalIcon className="h-4 w-4" />
-                        <span className="sr-only">Toggle menu</span>
-                      </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                      <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                      <DropdownMenuItem>Edit</DropdownMenuItem>
-                      <DropdownMenuItem>Delete</DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </TableCell>
+                <TableHead>
+                  <span className="sr-only">User Actions</span>
+                </TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {data?.map((rule) => (
+                <TableRow key={rule.id}>
+                  <TableCell className="font-medium">{rule.name}</TableCell>
+                  <TableCell>{rule.instructions}</TableCell>
+                  <TableCell>{capitalCase(rule.type)}</TableCell>
+                  <TableCell>
+                    <Actions actions={rule.actions} />
+                  </TableCell>
+                  <TableCell className="text-center">
+                    {rule.automate ? "Yes" : "No"}
+                  </TableCell>
+                  <TableCell className="text-center">
+                    {rule.runOnThreads ? "Yes" : "No"}
+                  </TableCell>
+                  {/* <TableCell className="text-right">33</TableCell>
+                <TableCell className="text-right">43</TableCell> */}
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          aria-haspopup="true"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontalIcon className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        ) : (
+          <>
+            <CardHeader>
+              <CardTitle>No automations</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Button asChild variant="outline">
+                <Link href="/automation/new">Create Automation</Link>
+              </Button>
+            </CardContent>
+          </>
+        )}
       </LoadingContent>
     </Card>
   );
