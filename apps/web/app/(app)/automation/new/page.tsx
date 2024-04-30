a3bfb85f345e8d777b2d1de15762656089600779
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-04-30 19:52:55 +0300

    Add new automation page

---
+++ apps/web/app/(app)/automation/new/page.tsx
@@ -0,0 +1,50 @@
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import { SparklesIcon } from "lucide-react";
+import { PlanHistory } from "@/app/(app)/automation/PlanHistory";
+import { Planned } from "@/app/(app)/automation/Planned";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { auth } from "@/app/api/auth/[...nextauth]/auth";
+import prisma from "@/utils/prisma";
+import { Button } from "@/components/ui/button";
+
+export default async function NewAutomationPage() {
+  const session = await auth();
+  if (!session?.user) throw new Error("Not logged in");
+  const [rule, executedRule] = await Promise.all([
+    prisma.rule.findFirst({
+      where: { userId: session.user.id },
+      select: { id: true },
+    }),
+    prisma.executedRule.findFirst({
+      where: { userId: session.user.id },
+      select: { id: true },
+    }),
+  ]);
+  if (!rule && !executedRule) redirect("/automation/create");
+
+  return (
+    <Tabs defaultValue="pending">
+      <div className="flex h-16 shrink-0 items-center justify-between gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6">
+        <TabsList className="m-2">
+          <TabsTrigger value="pending">Pending</TabsTrigger>
+          <TabsTrigger value="history">History</TabsTrigger>
+        </TabsList>
+
+        <Button asChild>
+          <Link href="/automation/create">
+            <SparklesIcon className="mr-2 h-4 w-4" />
+            Create Automation
+          </Link>
+        </Button>
+      </div>
+
+      <TabsContent value="pending">
+        <Planned />
+      </TabsContent>
+      <TabsContent value="history">
+        <PlanHistory />
+      </TabsContent>
+    </Tabs>
+  );
+}
--- apps/web/components/ui/tabs.tsx
+++ apps/web/components/ui/tabs.tsx
@@ -1,55 +1,128 @@
 "use client";
 
-import * as React from "react";
-import * as TabsPrimitive from "@radix-ui/react-tabs";
+// from: https://github.com/shadcn-ui/ui/issues/414#issuecomment-1772421366
 
+import * as React from "react";
+import Link, { LinkProps } from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 import { cn } from "@/utils";
 
-const Tabs = TabsPrimitive.Root;
+interface Context {
+  defaultValue: string;
+  hrefFor: (value: string) => LinkProps["href"];
+  searchParam: string;
+  selected: string;
+}
+const TabsContext = React.createContext<Context>(null as any);
 
-const TabsList = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.List>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.List
-    ref={ref}
-    className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-slate-100 p-1 text-slate-500 dark:bg-slate-800 dark:text-slate-400",
-      className
-    )}
-    {...props}
-  />
-));
-TabsList.displayName = TabsPrimitive.List.displayName;
+export function Tabs(props: {
+  children: React.ReactNode;
+  className?: string;
+  /**
+   * The default tab
+   */
+  defaultValue: string;
+  /**
+   * Which search param to use
+   * @default "tab"
+   */
+  searchParam?: string;
+}) {
+  const { children, className, searchParam = "tab", ...other } = props;
+  const searchParams = useSearchParams()!;
 
-const TabsTrigger = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Trigger
-    ref={ref}
-    className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-950 data-[state=active]:shadow-sm dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=active]:bg-slate-950 dark:data-[state=active]:text-slate-50",
-      className
-    )}
-    {...props}
-  />
-));
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+  const selected = searchParams.get(searchParam) || props.defaultValue;
 
-const TabsContent = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      "mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
-      className
-    )}
-    {...props}
-  />
-));
-TabsContent.displayName = TabsPrimitive.Content.displayName;
+  const pathname = usePathname();
+  const hrefFor: Context["hrefFor"] = React.useCallback(
+    (value) => {
+      const params = new URLSearchParams(searchParams);
+      if (value === props.defaultValue) {
+        params.delete(searchParam);
+      } else {
+        params.set(searchParam, value);
+      }
 
-export { Tabs, TabsList, TabsTrigger, TabsContent };
+      const asString = params.toString();
+
+      return pathname + (asString ? "?" + asString : "");
+    },
+    [searchParams, props.searchParam],
+  );
+
+  return (
+    <TabsContext.Provider value={{ ...other, hrefFor, searchParam, selected }}>
+      <div className={className}>{children}</div>
+    </TabsContext.Provider>
+  );
+}
+
+const useContext = () => {
+  const context = React.useContext(TabsContext);
+  if (!context) {
+    throw new Error(
+      "Tabs compound components cannot be rendered outside the Tabs component",
+    );
+  }
+
+  return context;
+};
+
+export function TabsList(props: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      {...props}
+      className={cn(
+        "inline-flex h-10 items-center justify-center rounded-md bg-slate-100 p-1 text-slate-500 dark:bg-slate-800 dark:text-slate-400",
+        props.className,
+      )}
+    />
+  );
+}
+
+export const TabsTrigger = (props: {
+  children: React.ReactNode;
+  className?: string;
+  value: string;
+}) => {
+  const context = useContext();
+
+  return (
+    <Link
+      {...props}
+      className={cn(
+        "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-950 data-[state=active]:shadow-sm dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=active]:bg-slate-950 dark:data-[state=active]:text-slate-50",
+        props.className,
+      )}
+      data-state={context.selected === props.value ? "active" : "inactive"}
+      href={context.hrefFor(props.value)}
+      scroll={false}
+      shallow={true}
+    />
+  );
+};
+
+export function TabsContent(props: {
+  children: React.ReactNode;
+  className?: string;
+  value: string;
+}) {
+  const context = useContext();
+
+  if (context.selected !== props.value) {
+    return null;
+  }
+
+  return (
+    <div
+      {...props}
+      className={cn(
+        "mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
+        props.className,
+      )}
+    />
+  );
+}
