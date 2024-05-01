3d30be9e5d716cf7cf64aa37976c7eda675dabdc
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-05-01 14:16:04 +0300

    Fix build

--- apps/web/app/(app)/automation/page.tsx
+++ apps/web/app/(app)/automation/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 import Link from "next/link";
+import { Suspense } from "react";
 import { SparklesIcon } from "lucide-react";
 import { PlanHistory } from "@/app/(app)/automation/PlanHistory";
 import { Planned } from "@/app/(app)/automation/Planned";
@@ -25,31 +26,33 @@ export default async function NewAutomationPage() {
   if (!rule && !executedRule) redirect("/automation/create");
 
   return (
-    <Tabs defaultValue="pending">
-      <div className="flex shrink-0 flex-col items-center justify-between gap-x-4 space-y-2 border-b border-gray-200 bg-white px-4 py-2 shadow-sm sm:flex-row sm:gap-x-6 sm:space-y-0 sm:px-6">
-        <TabsList>
-          <TabsTrigger value="pending">Pending</TabsTrigger>
-          <TabsTrigger value="history">History</TabsTrigger>
-          <TabsTrigger value="automations">Automations</TabsTrigger>
-        </TabsList>
+    <Suspense>
+      <Tabs defaultValue="pending">
+        <div className="flex shrink-0 flex-col items-center justify-between gap-x-4 space-y-2 border-b border-gray-200 bg-white px-4 py-2 shadow-sm sm:flex-row sm:gap-x-6 sm:space-y-0 sm:px-6">
+          <TabsList>
+            <TabsTrigger value="pending">Pending</TabsTrigger>
+            <TabsTrigger value="history">History</TabsTrigger>
+            <TabsTrigger value="automations">Automations</TabsTrigger>
+          </TabsList>
 
-        <Button asChild>
-          <Link href="/automation/create">
-            <SparklesIcon className="mr-2 h-4 w-4" />
-            Create Automation
-          </Link>
-        </Button>
-      </div>
+          <Button asChild>
+            <Link href="/automation/create">
+              <SparklesIcon className="mr-2 h-4 w-4" />
+              Create Automation
+            </Link>
+          </Button>
+        </div>
 
-      <TabsContent value="pending">
-        <Planned />
-      </TabsContent>
-      <TabsContent value="history">
-        <PlanHistory />
-      </TabsContent>
-      <TabsContent value="automations">
-        <Rules />
-      </TabsContent>
-    </Tabs>
+        <TabsContent value="pending">
+          <Planned />
+        </TabsContent>
+        <TabsContent value="history">
+          <PlanHistory />
+        </TabsContent>
+        <TabsContent value="automations">
+          <Rules />
+        </TabsContent>
+      </Tabs>
+    </Suspense>
   );
 }
