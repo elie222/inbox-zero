2783a0de9fddce08dba34930d73099ae36fdd6cb
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-05-12 13:24:44 +0300

    Adjust social proof copy

--- apps/web/app/(landing)/home/LogoCloud.tsx
+++ apps/web/app/(landing)/home/LogoCloud.tsx
@@ -4,7 +4,7 @@ export function LogoCloud() {
   return (
     <div className="mx-auto mt-16 max-w-7xl px-6 lg:px-8">
       <h2 className="text-center font-cal text-lg leading-8 text-gray-900">
-        You{`'`}re in good company
+        Trusted by 5000+ happy users
       </h2>
 
       <div className="mx-auto mt-8 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
