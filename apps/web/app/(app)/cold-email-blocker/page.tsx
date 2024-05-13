5d050e61ae3c30548d4b24b5709f6808dd113b74
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-05-13 13:50:34 +0300

    Put premium banner back for cold email blocker

--- apps/web/app/(app)/cold-email-blocker/page.tsx
+++ apps/web/app/(app)/cold-email-blocker/page.tsx
@@ -3,10 +3,15 @@ import { ColdEmailList } from "@/app/(app)/cold-email-blocker/ColdEmailList";
 import { ColdEmailSettings } from "@/app/(app)/cold-email-blocker/ColdEmailSettings";
 import { Card } from "@/components/ui/card";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { PremiumAlertWithData } from "@/components/PremiumAlert";
 
 export default function ColdEmailBlockerPage() {
   return (
     <Suspense>
+      <div className="content-container mt-2">
+        <PremiumAlertWithData />
+      </div>
+
       <Tabs defaultValue="cold-emails">
         <div className="content-container flex shrink-0 flex-col items-center justify-between gap-x-4 space-y-2 border-b border-gray-200 bg-white py-2 shadow-sm md:flex-row md:gap-x-6 md:space-y-0">
           <TabsList>
