03e83169b1425b3672b744510bae015bfa1cd5ce
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-05-16 14:07:47 +0300

    Add prisma optimise

--- apps/web/utils/prisma.ts
+++ apps/web/utils/prisma.ts
@@ -1,11 +1,34 @@
 import { env } from "@/env.mjs";
 import { Prisma, PrismaClient } from "@prisma/client";
+import {
+  DynamicClientExtensionThis,
+  InternalArgs,
+} from "@prisma/client/runtime/library";
+import { withOptimize } from "@prisma/extension-optimize";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma:
+    | DynamicClientExtensionThis<
+        Prisma.TypeMap<
+          InternalArgs & {
+            result: {};
+            model: {};
+            query: {};
+            client: {};
+          }
+        >,
+        Prisma.TypeMapCb,
+        {
+          result: {};
+          model: {};
+          query: {};
+          client: {};
+        }
+      >
+    | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = global.prisma || new PrismaClient().$extends(withOptimize());
 
 if (env.NODE_ENV === "development") global.prisma = prisma;
 
