39e6fdeb93cb964ac385df06e2e585e6be95493d
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-07-03 17:53:29 +0300

    service worker gtm

--- apps/web/app/sw.ts
+++ apps/web/app/sw.ts
@@ -1,6 +1,6 @@
 import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist } from "serwist";
+import { NetworkOnly, Serwist } from "serwist";
 
 // This declares the value of `injectionPoint` to TypeScript.
 // `injectionPoint` is the string that will be replaced by the
@@ -19,7 +19,25 @@ const serwist = new Serwist({
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
-  runtimeCaching: defaultCache,
+  runtimeCaching: [
+    {
+      // Google Tag Manager handling
+      matcher: ({ url }) =>
+        url.hostname === "www.googletagmanager.com" ||
+        url.hostname === "tagmanager.google.com",
+      handler: new NetworkOnly({
+        plugins: [
+          {
+            handlerDidError: async ({ request }) => {
+              console.error(`Failed to fetch: ${request.url}`);
+              return Response.error();
+            },
+          },
+        ],
+      }),
+    },
+    ...defaultCache,
+  ],
   disableDevLogs: true,
 });
 
