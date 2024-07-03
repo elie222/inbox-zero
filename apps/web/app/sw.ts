b78b91108f993aadfb73eaad76958a9850132157
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-07-03 15:53:39 +0300

    fix serwist logs in dev

--- apps/web/app/sw.ts
+++ apps/web/app/sw.ts
@@ -20,6 +20,7 @@ const serwist = new Serwist({
   clientsClaim: true,
   navigationPreload: true,
   runtimeCaching: defaultCache,
+  disableDevLogs: true,
 });
 
 serwist.addEventListeners();
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -174,8 +174,6 @@ const exportConfig =
     : mdxConfig;
 
 const withSerwist = withSerwistInit({
-  // Note: This is only an example. If you use Pages Router,
-  // use something else that works, such as "service-worker/index.ts".
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
 });
