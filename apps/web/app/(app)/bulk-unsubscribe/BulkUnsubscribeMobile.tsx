3d1e1dd626fcdb453e9a3846ef1d88922baf6521
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-07-10 17:52:59 +0300

    show unsubscribe stats on mobile

--- apps/web/app/(app)/bulk-unsubscribe/BulkUnsubscribeMobile.tsx
+++ apps/web/app/(app)/bulk-unsubscribe/BulkUnsubscribeMobile.tsx
@@ -19,6 +19,7 @@ import { Button, ButtonLoader } from "@/components/ui/button";
 import { NewsletterStatus } from "@prisma/client";
 import { BadgeCheckIcon, MailMinusIcon } from "lucide-react";
 import { cleanUnsubscribeLink } from "@/utils/parse/parseHtml.client";
+import { Badge } from "@/components/ui/badge";
 
 export function BulkUnsubscribeMobile({
   tableRows,
@@ -31,14 +32,14 @@ export function BulkUnsubscribeMobile({
 export function BulkUnsubscribeRowMobile(props: RowProps) {
   const { item, refetchPremium, mutate, hasUnsubscribeAccess } = props;
 
-  const posthog = usePostHog();
-
-  // const readPercentage = (item.readEmails / item.value) * 100;
-  // const archivedEmails = item.value - item.inboxEmails;
-  // const archivedPercentage = (archivedEmails / item.value) * 100;
+  const readPercentage = (item.readEmails / item.value) * 100;
+  const archivedEmails = item.value - item.inboxEmails;
+  const archivedPercentage = (archivedEmails / item.value) * 100;
 
   const name = extractNameFromEmail(item.name);
   const email = extractEmailAddress(item.name);
+
+  const posthog = usePostHog();
 
   const { approveLoading, onApprove } = useApproveButton({
     item,
@@ -59,7 +60,25 @@ export function BulkUnsubscribeRowMobile(props: RowProps) {
         <CardTitle className="truncate">{name}</CardTitle>
         <CardDescription className="truncate">{email}</CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-4">
+        <div className="grid grid-cols-3 gap-2 text-nowrap">
+          <Badge variant="outline" className="justify-center">
+            {item.value} emails
+          </Badge>
+          <Badge
+            variant={badgeVariant(readPercentage, 50, 75)}
+            className="justify-center"
+          >
+            {readPercentage.toFixed(0)}% read
+          </Badge>
+          <Badge
+            variant={badgeVariant(archivedPercentage, 50, 75)}
+            className="justify-center"
+          >
+            {archivedPercentage.toFixed(0)}% archived
+          </Badge>
+        </div>
+
         <div className="grid grid-cols-2 gap-2">
           <Button
             size="sm"
@@ -112,4 +131,14 @@ export function BulkUnsubscribeRowMobile(props: RowProps) {
       </CardContent>
     </Card>
   );
+}
+
+function badgeVariant(
+  value: number,
+  cutoffBad: number,
+  cutoffGood: number,
+): "green" | "red" | "outline" {
+  if (value < cutoffBad) return "red";
+  if (value > cutoffGood) return "green";
+  return "outline";
 }
