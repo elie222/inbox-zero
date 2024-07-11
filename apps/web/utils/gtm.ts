c32e22d91bf6909990126444f17f82ab1e0eacc7
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-07-11 12:57:46 +0300

    Remove unused event

--- apps/web/utils/gtm.ts
+++ apps/web/utils/gtm.ts
@@ -4,7 +4,3 @@ import { env } from "@/env";
 export const signUpEvent = () => {
   if (env.NEXT_PUBLIC_GTM_ID) sendGTMEvent({ event: "CompleteRegistration" });
 };
-
-export const beginCheckoutEvent = (value: number) => {
-  if (env.NEXT_PUBLIC_GTM_ID) sendGTMEvent({ event: "Begin checkout", value });
-};
