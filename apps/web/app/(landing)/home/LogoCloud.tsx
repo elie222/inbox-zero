d7062fcfcb934f638ff498d9662f611f5a448516
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-07-30 17:26:51 +0300

    Update logo cloud

--- apps/web/app/(landing)/home/LogoCloud.tsx
+++ apps/web/app/(landing)/home/LogoCloud.tsx
@@ -4,40 +4,47 @@ export function LogoCloud() {
   return (
     <div className="mx-auto mt-16 max-w-7xl px-6 lg:px-8">
       <h2 className="text-center font-cal text-lg leading-8 text-gray-900">
-        Trusted by 7,000+ happy users
+        Trusted by 7,000+ productive users
       </h2>
 
-      <div className="mx-auto mt-8 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+      <div className="mx-auto mt-8 grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-6">
         <Image
-          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          className="max-h-12 w-full object-contain"
           src="/images/logos/resend.svg"
           alt="Resend"
           width={158}
           height={48}
         />
         <Image
-          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          className="max-h-12 w-full object-contain"
           src="/images/logos/bytedance.svg"
           alt="ByteDance"
           width={158}
           height={48}
         />
         <Image
-          className="col-span-2 max-h-10 w-full object-contain lg:col-span-1"
+          className="max-h-12 w-full object-contain"
           src="/images/logos/zendesk.svg"
           alt="ZenDesk"
           width={158}
           height={48}
         />
         <Image
-          className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+          className="max-h-12 w-full object-contain"
           src="/images/logos/brilliant.svg"
           alt="Brilliant"
           width={158}
           height={48}
         />
         <Image
-          className="col-span-2 col-start-2 max-h-10 w-full object-contain sm:col-start-auto lg:col-span-1"
+          className="max-h-12 w-full object-contain"
+          src="/images/logos/doac.svg"
+          alt="DOAC"
+          width={158}
+          height={48}
+        />
+        <Image
+          className="max-h-12 w-full object-contain"
           src="/images/logos/joco.svg"
           alt="JOCO"
           width={158}
