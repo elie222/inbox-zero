a3dca3d93ab89f68f1db43166ab70053b3dd323f
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-08-21 16:43:49 +0100

    clean up loading

--- apps/web/components/Loading.tsx
+++ apps/web/components/Loading.tsx
@@ -1,28 +1,18 @@
-import { Loader2Icon } from "lucide-react";
 import type React from "react";
-import { VariantProps, cva } from "class-variance-authority";
-import { cn } from "@/utils";
+import { Loader2Icon } from "lucide-react";
 
-const loaderVariants = cva("mx-auto animate-spin", {
-  variants: {
-    size: {
-      xs: "size-4",
-      sm: "size-8",
-    },
-  },
-  defaultVariants: { size: "sm" },
-});
-
-export interface LoadingProps extends VariantProps<typeof loaderVariants> {}
-
-export function Loading(props: LoadingProps) {
-  return <Loader2Icon className={cn(loaderVariants(props))} />;
+export function Loading() {
+  return (
+    <div className="p-8">
+      <Loader2Icon className="size-8 animate-spin" />
+    </div>
+  );
 }
 
-export function LoadingMiniSpinner(props: LoadingProps) {
-  return <Loading {...props} size="xs" />;
+export function LoadingMiniSpinner() {
+  return <Loader2Icon className="size-4 animate-spin" />;
 }
 
 export function ButtonLoader() {
-  return <Loader2Icon className="mr-2 size-4" />;
+  return <Loader2Icon className="mr-2 size-4 animate-spin" />;
 }
