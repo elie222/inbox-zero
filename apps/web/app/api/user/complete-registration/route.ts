129f92207842e463ce8ff2f6de457214d06e650b
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-08-26 19:04:21 +0200

    track sign up event in posthog on server

--- apps/web/app/api/user/complete-registration/route.ts
+++ apps/web/app/api/user/complete-registration/route.ts
@@ -3,10 +3,12 @@ import { cookies, headers } from "next/headers";
 import { auth } from "@/app/api/auth/[...nextauth]/auth";
 import { withError } from "@/utils/middleware";
 import { sendCompleteRegistrationEvent } from "@/utils/fb";
+import { posthogCaptureEvent } from "@/utils/posthog";
+import prisma from "@/utils/prisma";
 
 export type CompleteRegistrationBody = {};
 
-export const POST = withError(async (request: NextRequest) => {
+export const POST = withError(async (_request: NextRequest) => {
   const session = await auth();
   if (!session?.user.email)
     return NextResponse.json({ error: "Not authenticated" });
@@ -20,7 +22,7 @@ export const POST = withError(async (request: NextRequest) => {
   const fbc = c.get("_fbc")?.value;
   const fbp = c.get("_fbp")?.value;
 
-  const result = await sendCompleteRegistrationEvent({
+  const fbPromise = sendCompleteRegistrationEvent({
     userId: session.user.id,
     email: session.user.email,
     eventSourceUrl: eventSourceUrl || "",
@@ -29,8 +31,14 @@ export const POST = withError(async (request: NextRequest) => {
     fbc: fbc || "",
     fbp: fbp || "",
   });
+  const posthogPromise = storePosthogSignupEvent(
+    session.user.id,
+    session.user.email,
+  );
 
-  return NextResponse.json(result);
+  await Promise.allSettled([fbPromise, posthogPromise]);
+
+  return NextResponse.json({ success: true });
 });
 
 function getIp() {
@@ -42,4 +50,29 @@ function getIp() {
   }
 
   return headers().get("x-real-ip") ?? FALLBACK_IP_ADDRESS;
+}
+
+async function storePosthogSignupEvent(userId: string, email: string) {
+  const userCreatedAt = await prisma.user.findUnique({
+    where: { id: userId },
+    select: { createdAt: true },
+  });
+  if (!userCreatedAt) {
+    console.error(`storePosthogSignupEvent: User not found: ${userId}`);
+    return;
+  }
+
+  const ONE_HOUR_MS = 60 * 60 * 1000;
+  const ONE_HOUR_AGO = new Date(Date.now() - ONE_HOUR_MS);
+
+  if (userCreatedAt.createdAt < ONE_HOUR_AGO) {
+    console.error(
+      `storePosthogSignupEvent: User created more than an hour ago: ${userId}`,
+    );
+    return;
+  }
+
+  return posthogCaptureEvent(email, "User signed up", {
+    $set_once: { createdAt: userCreatedAt.createdAt },
+  });
 }
