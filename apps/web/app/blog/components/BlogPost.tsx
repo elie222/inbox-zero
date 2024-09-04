ed6b94357c97433c24dc5310043eb91a23c760f9
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-05 02:38:36 +0300

    adjust blog post styling. add content sidebar

--- apps/web/app/blog/components/BlogPost.tsx
+++ apps/web/app/blog/components/BlogPost.tsx
@@ -1,6 +1,8 @@
 // import { format, parseISO } from "date-fns";
+import Image from "next/image";
 import { Prose } from "@/app/blog/components/Prose";
 import { BasicLayout } from "@/components/layouts/BasicLayout";
+import { Card, CardContent } from "@/components/ui/card";
 
 export function BlogPost(props: {
   date: string;
@@ -12,16 +14,18 @@ export function BlogPost(props: {
 
   return (
     <BasicLayout>
-      <article className="mx-auto max-w-xl px-6 py-20">
+      <article className="mx-auto max-w-2xl px-6 py-20">
         {/* <div className="text-center">
           <time dateTime={date} className="mb-1 text-xs text-gray-600">
             {format(parseISO(date), "LLLL d, yyyy")}
           </time>
           <p className="text-sm font-semibold">by {author}</p>
         </div> */}
-        <div className="mt-12">
-          <Prose>{content}</Prose>
-        </div>
+        <Card>
+          <CardContent className="pt-6">
+            <Prose>{content}</Prose>
+          </CardContent>
+        </Card>
       </article>
     </BasicLayout>
   );
--- apps/web/app/blog/post/[slug]/Post.tsx
+++ apps/web/app/blog/post/[slug]/Post.tsx
@@ -1,35 +1,87 @@
 "use client";
 
+import React from "react";
+import Link from "next/link";
 import Image from "next/image";
-import { PortableText } from "@portabletext/react";
+import {
+  PortableText,
+  PortableTextBlock,
+  PortableTextComponentProps,
+} from "@portabletext/react";
 import { client } from "@/sanity/lib/client";
 import imageUrlBuilder from "@sanity/image-url";
 import { BasicLayout } from "@/components/layouts/BasicLayout";
 import type { Post as PostType } from "@/app/blog/types";
 import { Prose } from "@/app/blog/components/Prose";
+import { TableOfContents } from "@/app/blog/components/TableOfContents";
+import { Card, CardContent } from "@/components/ui/card";
+import { extractTextFromPortableTextBlock, slugify } from "@/utils/text";
 
 const builder = imageUrlBuilder(client);
 
 export function Post({ post }: { post: PostType }) {
   return (
     <BasicLayout>
-      <article className="mx-auto max-w-xl px-6 py-20">
-        <main className="container mx-auto px-4 py-16">
-          <Prose>
-            <h1>{post.title}</h1>
-            <p>{post.description}</p>
-            {post.mainImage ? (
-              <Image
-                src={builder.image(post.mainImage).width(480).height(300).url()}
-                alt={post?.mainImage?.alt}
-                width={480}
-                height={300}
-              />
-            ) : null}
-            {post.body ? <PortableText value={post.body} /> : null}
-          </Prose>
+      <article className="mx-auto mt-20 grid w-full max-w-screen-xl gap-5 px-0 pt-10 md:grid-cols-4 lg:gap-10 lg:px-20">
+        <main className="md:col-span-3">
+          <Card>
+            <CardContent className="p-10">
+              <Prose>
+                <h1>{post.title}</h1>
+                <p>{post.description}</p>
+                {post.mainImage ? (
+                  <div className="-mx-10 my-8">
+                    <Image
+                      src={builder
+                        .image(post.mainImage)
+                        .width(1200)
+                        .height(675)
+                        .url()}
+                      alt={post?.mainImage?.alt || ""}
+                      width={1200}
+                      height={675}
+                      className="h-auto w-full"
+                    />
+                  </div>
+                ) : null}
+                {post.body ? (
+                  <PortableText
+                    value={post.body}
+                    components={{
+                      block: {
+                        h2: createHeadingComponent("h2"),
+                        h3: createHeadingComponent("h3"),
+                      },
+                    }}
+                  />
+                ) : null}
+              </Prose>
+            </CardContent>
+          </Card>
         </main>
+        <aside className="hidden md:block">
+          {post.body && (
+            <Card>
+              <CardContent className="pt-6">
+                <TableOfContents body={post.body} />
+              </CardContent>
+            </Card>
+          )}
+        </aside>
       </article>
     </BasicLayout>
   );
 }
+
+const createHeadingComponent =
+  (Tag: "h2" | "h3") =>
+  ({ children, value }: PortableTextComponentProps<PortableTextBlock>) => {
+    const text = extractTextFromPortableTextBlock(value);
+    const id = slugify(text);
+
+    return (
+      <Tag id={id}>
+        <Link href={`#${id}`}>{children}</Link>
+      </Tag>
+    );
+  };
