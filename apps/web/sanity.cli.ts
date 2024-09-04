ee2f6e07c441c30030bf1aa4fd67491115530e5a
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-04 15:56:49 +0300

    improve env for sanity deploy

--- apps/web/sanity.cli.ts
+++ apps/web/sanity.cli.ts
@@ -6,5 +6,9 @@ import { defineCliConfig } from "sanity/cli";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const studioHost = process.env.SANITY_STUDIO_HOST;
 
-export default defineCliConfig({ api: { projectId, dataset } });
+export default defineCliConfig({
+  api: { projectId, dataset },
+  studioHost,
+});
