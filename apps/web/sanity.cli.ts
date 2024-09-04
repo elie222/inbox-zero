023f20f602e262b92f6448e23f3af452d8f52f65
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-04 15:59:46 +0300

    fix sanity deploy

--- apps/web/sanity.cli.ts
+++ apps/web/sanity.cli.ts
@@ -4,8 +4,8 @@
  **/
 import { defineCliConfig } from "sanity/cli";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
+const dataset = process.env.SANITY_STUDIO_DATASET;
 const studioHost = process.env.SANITY_STUDIO_HOST;
 
 export default defineCliConfig({
