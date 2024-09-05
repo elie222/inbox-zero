fc9a60374eef8d087c30fe4a192f22e2093b9d9f
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-05 21:52:42 +0300

    adjust blog link style

--- apps/web/app/blog/components/Prose.tsx
+++ apps/web/app/blog/components/Prose.tsx
@@ -21,7 +21,7 @@ export function Prose<T extends React.ElementType = "div">({
         // links
         "prose-a:font-semibold dark:prose-a:text-blue-400",
         // link underline
-        "prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.blue.300))] hover:prose-a:[--tw-prose-underline-size:6px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.blue.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px]",
+        "prose-a:no-underline",
         // pre
         "prose-pre:rounded-xl prose-pre:bg-slate-900 prose-pre:shadow-lg dark:prose-pre:bg-slate-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-slate-300/10",
         // hr
