5b62859de24d24c241ea3c4108366f0eff1acbfb
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-10 00:56:06 +0300

    order blog by date

--- apps/web/sanity/lib/queries.ts
+++ apps/web/sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { groq } from "next-sanity";
 
 // Get all posts
-export const postsQuery = groq`*[_type == "post"] {
+export const postsQuery = groq`*[_type == "post"] | order(_createdAt desc) {
   _createdAt,
   title,
   description,
