150d7e2f4297a9410295490e6b8fff82a2bb4b26
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-13 19:37:31 -0400

    fix build

--- apps/web/app/(landing)/welcome-upgrade/page.tsx
+++ apps/web/app/(landing)/welcome-upgrade/page.tsx
@@ -1,32 +1,36 @@
+import { Suspense } from "react";
+import { CheckCircleIcon } from "lucide-react";
 import { Pricing } from "@/app/(app)/premium/Pricing";
 import { Footer } from "@/app/(landing)/home/Footer";
-import { CheckCircleIcon } from "lucide-react";
+import { Loading } from "@/components/Loading";
 
 export default function WelcomeUpgradePage() {
   return (
     <div className="mt-8">
-      <Pricing
-        header={
-          <div className="mb-8 flex flex-col items-start">
-            <div className="mx-auto text-center">
-              <h2 className="font-cal text-base leading-7 text-blue-600">
-                Spend 50% less time on email
-              </h2>
-              <p className="mt-2 font-cal text-2xl text-gray-900 sm:text-3xl">
-                Join 7,000+ users that use Inbox Zero
-                <br />
-                to be more productive!
-              </p>
-            </div>
+      <Suspense fallback={<Loading />}>
+        <Pricing
+          header={
+            <div className="mb-8 flex flex-col items-start">
+              <div className="mx-auto text-center">
+                <h2 className="font-cal text-base leading-7 text-blue-600">
+                  Spend 50% less time on email
+                </h2>
+                <p className="mt-2 font-cal text-2xl text-gray-900 sm:text-3xl">
+                  Join 7,000+ users that use Inbox Zero
+                  <br />
+                  to be more productive!
+                </p>
+              </div>
 
-            <div className="mx-auto mt-4 flex flex-col items-start gap-2">
-              <TrialFeature>100% no-risk trial</TrialFeature>
-              <TrialFeature>Free for the first 7 days</TrialFeature>
-              <TrialFeature>Cancel anytime, hassle-free</TrialFeature>
+              <div className="mx-auto mt-4 flex flex-col items-start gap-2">
+                <TrialFeature>100% no-risk trial</TrialFeature>
+                <TrialFeature>Free for the first 7 days</TrialFeature>
+                <TrialFeature>Cancel anytime, hassle-free</TrialFeature>
+              </div>
             </div>
-          </div>
-        }
-      />
+          }
+        />
+      </Suspense>
       <Footer />
     </div>
   );
