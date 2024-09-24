fe2c31bd0d570bacbd1cf64673055203a66bcf01
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-24 00:19:11 -0400

    add loading button

--- apps/web/components/ui/button.tsx
+++ apps/web/components/ui/button.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Loader2 } from "lucide-react";
 
 import { cn } from "@/utils";
 
@@ -32,10 +33,14 @@ const buttonVariants = cva(
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
       },
+      loading: {
+        true: "opacity-50 cursor-not-allowed",
+      },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      loading: false,
     },
   },
 );
@@ -44,19 +49,41 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      loading = false,
+      children,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button";
     const type = props.type ?? "button";
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, loading, className }))}
         ref={ref}
         type={type}
+        disabled={loading || props.disabled}
         {...props}
-      />
+      >
+        {loading ? (
+          <>
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            {children}
+          </>
+        ) : (
+          children
+        )}
+      </Comp>
     );
   },
 );
