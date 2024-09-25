96dd4a9f5f79dcaced76922875a645893d2b4c69
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-24 21:47:06 -0400

    cut down on eslint

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -9,13 +9,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "prettier",
-    require.resolve("@vercel/style-guide/eslint/node"),
-    require.resolve("@vercel/style-guide/eslint/typescript"),
-    require.resolve("@vercel/style-guide/eslint/browser"),
-    require.resolve("@vercel/style-guide/eslint/react"),
     require.resolve("@vercel/style-guide/eslint/next"),
-    // Turborepo custom eslint configuration configures the following rules:
-    //  - https://github.com/vercel/turborepo/blob/main/packages/eslint-plugin-turbo/docs/rules/no-undeclared-env-vars.md
     "eslint-config-turbo",
   ],
   parserOptions: {
