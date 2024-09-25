35482f69147b3c10ba6be1b8f849e18c5bebe616
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-24 21:45:24 -0400

    fix eslint error

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -17,7 +17,7 @@ module.exports = {
     // Turborepo custom eslint configuration configures the following rules:
     //  - https://github.com/vercel/turborepo/blob/main/packages/eslint-plugin-turbo/docs/rules/no-undeclared-env-vars.md
     "eslint-config-turbo",
-  ].map(require.resolve),
+  ],
   parserOptions: {
     project,
   },
