fe546c9a70c0ad9dac98354f1095f7387d22fb0c
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-09-29 20:40:42 -0400

    update demo video

--- apps/web/app/(landing)/home/Hero.tsx
+++ apps/web/app/(landing)/home/Hero.tsx
@@ -66,7 +66,7 @@ export function Hero(props: {
             <HeroVideoDialog
               className="block"
               animationStyle="top-in-bottom-out"
-              videoSrc="https://www.youtube.com/embed/kc_9WZ1ZWyg?autoplay=1"
+              videoSrc="https://www.youtube.com/embed/hfvKvTHBjG0?autoplay=1"
               thumbnailSrc={props.image || "/images/newsletters.png"}
               thumbnailAlt="Bulk Unsubscriber Screenshot"
             />
