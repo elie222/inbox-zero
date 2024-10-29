bf2a7e61dabef2f2561082e51edf6d98ff72c4a5
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-10-29 03:22:49 -0400

    add security headers

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -158,8 +158,16 @@ const nextConfig = {
         source: "/(.*)",
         headers: [
           {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
             key: "X-Frame-Options",
             value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
           },
         ],
       },
