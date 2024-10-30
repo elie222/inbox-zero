1a9a6a55f2706b782d3e7b99053a127f1c5a2779
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-10-30 18:33:40 -0400

    fix next/router error

--- apps/web/app/(app)/smart-categories/setup/SetUpCategories.tsx
+++ apps/web/app/(app)/smart-categories/setup/SetUpCategories.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { InfoIcon, TagsIcon } from "lucide-react";
 import {
   Card,
