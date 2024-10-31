d33b62e1905e1c2ac4ab75e60515fe2781ad3c46
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-10-31 02:09:37 -0400

    fix process env

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -1,12 +1,11 @@
 /* eslint-disable no-process-env */
 import * as Sentry from "@sentry/nextjs";
-import { env } from "node:process";
 
 export function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     // this is your Sentry.init call from `sentry.server.config.js|ts`
     Sentry.init({
-      dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
       // Adjust this value in production, or use tracesSampler for greater control
       tracesSampleRate: 1,
       // Setting this option to true will print useful information to the console while you're setting up Sentry.
@@ -19,7 +18,7 @@ export function register() {
   // This is your Sentry.init call from `sentry.edge.config.js|ts`
   if (process.env.NEXT_RUNTIME === "edge") {
     Sentry.init({
-      dsn: env.NEXT_PUBLIC_SENTRY_DSN,
+      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
       // Adjust this value in production, or use tracesSampler for greater control
       tracesSampleRate: 1,
       // Setting this option to true will print useful information to the console while you're setting up Sentry.
