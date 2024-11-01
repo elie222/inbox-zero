062a424687daf84d1f554d4dbfe6c5494f333465
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-11-01 16:34:55 -0400

    Remove rewrite for site_app

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -149,11 +149,7 @@ const nextConfig = {
       {
         source: "/ingest/:path*",
         destination: "https://app.posthog.com/:path*",
-      },
-      {
-        source: "/site_app/:path*",
-        destination: "https://app.posthog.com/site_app/:path*",
-      },
+      }
     ];
   },
   async headers() {
