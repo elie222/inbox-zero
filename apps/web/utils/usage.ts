c9d8f93820147f1f6a36304346b35561953b614b
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-11-02 20:58:12 -0400

    Add groq pricing

--- apps/web/utils/usage.ts
+++ apps/web/utils/usage.ts
@@ -37,6 +37,8 @@ export async function saveAiUsage({
   ]);
 }
 
+const MILLION = 1_000_000;
+
 const costs: Record<
   string,
   {
@@ -44,40 +46,61 @@ const costs: Record<
     output: number;
   }
 > = {
-  // https://openai.com/pricing
+  // https://openai.com/api/pricing/
   "gpt-3.5-turbo-0125": {
-    input: 0.5 / 1_000_000,
-    output: 1.5 / 1_000_000,
+    input: 0.5 / MILLION,
+    output: 1.5 / MILLION,
   },
   "gpt-4o-mini": {
-    input: 0.15 / 1_000_000,
-    output: 0.6 / 1_000_000,
+    input: 0.15 / MILLION,
+    output: 0.6 / MILLION,
   },
   "gpt-4-turbo": {
-    input: 10 / 1_000_000,
-    output: 30 / 1_000_000,
+    input: 10 / MILLION,
+    output: 30 / MILLION,
   },
   "gpt-4o": {
-    input: 5 / 1_000_000,
-    output: 15 / 1_000_000,
+    input: 5 / MILLION,
+    output: 15 / MILLION,
+  },
+  "o1-preview": {
+    input: 15 / MILLION,
+    output: 60 / MILLION,
+  },
+  "o1-preview-2024-09-12": {
+    input: 15 / MILLION,
+    output: 60 / MILLION,
+  },
+  "o1-mini": {
+    input: 3 / MILLION,
+    output: 12 / MILLION,
+  },
+  "o1-mini-2024-09-12": {
+    input: 3 / MILLION,
+    output: 12 / MILLION,
   },
   // https://www.anthropic.com/pricing#anthropic-api
   "claude-3-5-sonnet-20240620": {
-    input: 3 / 1_000_000,
-    output: 15 / 1_000_000,
+    input: 3 / MILLION,
+    output: 15 / MILLION,
   },
   "claude-3-5-sonnet-20241022": {
-    input: 3 / 1_000_000,
-    output: 15 / 1_000_000,
+    input: 3 / MILLION,
+    output: 15 / MILLION,
   },
   // https://aws.amazon.com/bedrock/pricing/
   "anthropic.claude-3-5-sonnet-20240620-v1:0": {
-    input: 3 / 1_000_000,
-    output: 15 / 1_000_000,
+    input: 3 / MILLION,
+    output: 15 / MILLION,
   },
   "anthropic.claude-3-5-sonnet-20241022-v2:0": {
-    input: 3 / 1_000_000,
-    output: 15 / 1_000_000,
+    input: 3 / MILLION,
+    output: 15 / MILLION,
+  },
+  // https://groq.com/pricing/
+  "llama3-groq-70b-8192-tool-use-preview": {
+    input: 0.89 / MILLION,
+    output: 0.89 / MILLION,
   },
 };
 
