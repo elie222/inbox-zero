dd53570b15975cfc9c0d7e8a690774db22162236
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-11-02 21:35:54 -0400

    Fix early access page

---
+++ apps/web/app/(app)/request-access/EarlyAccessFeatures.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { usePostHog, useActiveFeatureFlags } from "posthog-js/react";
+import type { EarlyAccessFeature } from "posthog-js";
+import { Toggle } from "@/components/Toggle";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export function EarlyAccessFeatures() {
+  const posthog = usePostHog();
+  const activeFlags = useActiveFeatureFlags();
+  const [features, setFeatures] = useState<EarlyAccessFeature[]>([]);
+
+  useEffect(() => {
+    posthog.getEarlyAccessFeatures((features) => {
+      setFeatures(features);
+    }, true);
+  }, [posthog]);
+
+  const toggleBeta = useCallback(
+    (betaKey: string) => {
+      const isActive = activeFlags?.includes(betaKey);
+      posthog.updateEarlyAccessFeatureEnrollment(betaKey, !isActive);
+    },
+    [posthog, activeFlags],
+  );
+
+  if (!features.length) {
+    return null;
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Early Access Features</CardTitle>
+        <CardDescription>
+          You can enable and disable early access features here.
+        </CardDescription>
+      </CardHeader>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Feature</TableHead>
+            <TableHead className="w-24">Enabled</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {features.map((feature) => (
+            <TableRow key={feature.name}>
+              <TableCell>{feature.name}</TableCell>
+              <TableCell>
+                <Toggle
+                  name={feature.name}
+                  enabled={!!activeFlags?.includes(feature.flagKey!)}
+                  onChange={() => toggleBeta(feature.flagKey!)}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
+  );
+}
--- apps/web/app/(app)/request-access/page.tsx
+++ apps/web/app/(app)/request-access/page.tsx
@@ -1,76 +1,74 @@
-import { Button } from "@/components/Button";
-import { Card } from "@/components/Card";
-import { PageHeading, SectionDescription } from "@/components/Typography";
+import Link from "next/link";
+import { EarlyAccessFeatures } from "@/app/(app)/request-access/EarlyAccessFeatures";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function RequestAccessPage() {
   return (
-    <div className="px-2 sm:px-4">
-      <Card className="mx-auto mt-2 w-full max-w-2xl sm:mt-8">
-        <PageHeading>Enable Early Access Features</PageHeading>
-        <div className="mt-2 max-w-prose">
-          <SectionDescription>
-            Enable early access features to test and provide feedback.
-          </SectionDescription>
-        </div>
-        <div className="mt-4">
-          <Button id="beta-button" size="xl">
-            Enable Early Access Features
-          </Button>
-        </div>
-      </Card>
+    <div className="container px-2 pt-2 sm:px-4 sm:pt-8">
+      <div className="mx-auto max-w-2xl space-y-4 sm:space-y-8">
+        <EarlyAccessFeatures />
 
-      <Card className="mx-auto mt-2 w-full max-w-2xl sm:mt-8">
-        <PageHeading>Early Access</PageHeading>
+        <Card>
+          <CardHeader>
+            <CardTitle>Early Access</CardTitle>
+            <CardDescription>
+              Give us feedback on what features you want to see.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Button asChild>
+              <Link href="/waitlist" target="_blank">
+                Feedback Form
+              </Link>
+            </Button>
+          </CardContent>
+        </Card>
 
-        <div className="mt-2 max-w-prose">
-          <SectionDescription>
-            Give us feedback on what features you want to see.
-          </SectionDescription>
-        </div>
-        <div className="mt-4">
-          <Button size="xl" link={{ href: "/waitlist", target: "_blank" }}>
-            Feedback Form
-          </Button>
-        </div>
-      </Card>
+        {/* <Card className="mx-auto mt-2 w-full max-w-2xl sm:mt-8">
+          <PageHeading>Email Client (Beta)</PageHeading>
+          <div className="mt-2 max-w-prose">
+            <SectionDescription>
+              Use the Inbox Zero email client to manage your email.
+            </SectionDescription>
+          </div>
+          <div className="mt-4">
+            <Button size="xl" link={{ href: "/mail" }}>
+              Open Mail
+            </Button>
+          </div>
+        </Card> */}
 
-      {/* <Card className="mx-auto mt-2 w-full max-w-2xl sm:mt-8">
-        <PageHeading>Email Client (Beta)</PageHeading>
-        <div className="mt-2 max-w-prose">
-          <SectionDescription>
-            Use the Inbox Zero email client to manage your email.
-          </SectionDescription>
-        </div>
-        <div className="mt-4">
-          <Button size="xl" link={{ href: "/mail" }}>
-            Open Mail
-          </Button>
-        </div>
-      </Card> */}
-
-      {/* <Card className="mx-auto mt-2 w-full max-w-2xl sm:mt-8">
-        <PageHeading>Inbox Zero Daily Challenge</PageHeading>
-        <div className="mt-2 max-w-prose">
-          <SectionDescription>
-            Getting to inbox zero is overwhelming. But doing it for a single day
-            is doable.
-          </SectionDescription>
-          <SectionDescription>
-            Daily challenge mode aims to make handling email simpler:
-            <ul className="mt-2 list-inside list-disc">
-              <li>Handle emails in batches of 5</li>
-              <li>Set aside what you want to handle later. Archive the rest</li>
-              <li>Long emails summarized</li>
-              <li>Timer to maintain focus</li>
-            </ul>
-          </SectionDescription>
-        </div>
-        <div className="mt-4">
-          <Button size="xl" link={{ href: "/simple" }}>
-            Try now
-          </Button>
-        </div>
-      </Card> */}
+        {/* <Card className="mx-auto mt-2 w-full max-w-2xl sm:mt-8">
+          <PageHeading>Inbox Zero Daily Challenge</PageHeading>
+          <div className="mt-2 max-w-prose">
+            <SectionDescription>
+              Getting to inbox zero is overwhelming. But doing it for a single day
+              is doable.
+            </SectionDescription>
+            <SectionDescription>
+              Daily challenge mode aims to make handling email simpler:
+              <ul className="mt-2 list-inside list-disc">
+                <li>Handle emails in batches of 5</li>
+                <li>Set aside what you want to handle later. Archive the rest</li>
+                <li>Long emails summarized</li>
+                <li>Timer to maintain focus</li>
+              </ul>
+            </SectionDescription>
+          </div>
+          <div className="mt-4">
+            <Button size="xl" link={{ href: "/simple" }}>
+              Try now
+            </Button>
+          </div>
+        </Card> */}
+      </div>
     </div>
   );
 }
