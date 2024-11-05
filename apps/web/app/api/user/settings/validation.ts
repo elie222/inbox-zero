a6fbffbdf8777cf1998e868cdc7f15c41d137bb3
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-11-05 13:42:36 -0500

    Require API key for openai

--- apps/web/app/api/user/settings/validation.ts
+++ apps/web/app/api/user/settings/validation.ts
@@ -1,18 +1,20 @@
-import { modelOptions } from "@/utils/llms/config";
+import { Provider } from "@/utils/llms/config";
 import { z } from "zod";
 
 export const saveSettingsBody = z
   .object({
-    aiProvider: z.string(),
+    aiProvider: z.enum([Provider.ANTHROPIC, Provider.OPEN_AI]),
     aiModel: z.string(),
     aiApiKey: z.string().optional(),
   })
-  .superRefine((val) => {
-    // if ai api key is not set, model must be a preset model
-    if (!val.aiApiKey) {
-      const validModels = modelOptions[val.aiProvider].map((m) => m.value);
-      z.enum(validModels as [string, ...string[]]).parse(val.aiModel);
+  .superRefine((val, ctx) => {
+    if (!val.aiApiKey && val.aiProvider !== Provider.ANTHROPIC) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "API key is required for non-default providers",
+        path: ["aiProvider"],
+      });
     }
-    return true;
   });
+
 export type SaveSettingsBody = z.infer<typeof saveSettingsBody>;
