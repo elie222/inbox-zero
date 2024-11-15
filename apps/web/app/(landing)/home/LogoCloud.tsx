be36a5e039d755548a5d76b52b13e27621a3cd6a
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2024-11-15 14:43:43 -0500

    Update user count

--- apps/web/app/(landing)/home/LogoCloud.tsx
+++ apps/web/app/(landing)/home/LogoCloud.tsx
@@ -4,7 +4,7 @@ export function LogoCloud() {
   return (
     <div className="mx-auto mt-16 max-w-7xl px-6 lg:px-8">
       <h2 className="text-center font-cal text-lg leading-8 text-gray-900">
-        Trusted by 8,000+ productive users
+        Trusted by 9,000+ productive users
       </h2>
 
       <div className="mx-auto mt-8 grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-6">
