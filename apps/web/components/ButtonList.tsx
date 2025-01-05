5ca4209c681dda98b25c734c20d4c07eb7c4f7ed
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-01-05 21:10:33 +0200

    Create button list component

---
+++ apps/web/components/ButtonList.tsx
@@ -0,0 +1,46 @@
+import { SectionDescription } from "@/components/Typography";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/Input";
+
+type ButtonListItem = {
+  id: string;
+  name: string;
+};
+
+interface ButtonListProps {
+  title?: string;
+  items: ButtonListItem[];
+  onSelect: (id: string) => void;
+  selectedId?: string;
+  emptyMessage: string;
+}
+
+export function ButtonList({
+  title,
+  items,
+  onSelect,
+  selectedId,
+  emptyMessage,
+}: ButtonListProps) {
+  return (
+    <div>
+      {title && <Label name={title} label={title} />}
+
+      {!items.length && (
+        <SectionDescription className="mt-2">{emptyMessage}</SectionDescription>
+      )}
+
+      <div className="flex flex-col gap-1">
+        {items.map((item) => (
+          <Button
+            key={item.id}
+            variant={selectedId === item.id ? "default" : "outline"}
+            onClick={() => onSelect(item.id)}
+          >
+            {item.name}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+}
