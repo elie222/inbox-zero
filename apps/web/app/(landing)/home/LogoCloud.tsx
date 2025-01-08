b3735add7863c96a8eb8353f3f59eedf2099bacb
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-01-08 15:33:25 +0200

    Adjust social proof

--- apps/web/app/(landing)/home/LogoCloud.tsx
+++ apps/web/app/(landing)/home/LogoCloud.tsx
@@ -7,44 +7,51 @@ export function LogoCloud() {
         Trusted by 10,000+ productive users
       </h2>
 
-      <div className="mx-auto mt-8 grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-6">
+      <div className="mx-auto mt-8 grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-7">
         <Image
-          className="max-h-12 w-full object-contain"
+          className="order-4 max-h-12 w-full object-contain lg:order-none"
           src="/images/logos/resend.svg"
           alt="Resend"
           width={158}
           height={48}
         />
         <Image
-          className="max-h-12 w-full object-contain"
+          className="order-3 max-h-12 w-full object-contain lg:order-none"
           src="/images/logos/bytedance.svg"
           alt="ByteDance"
           width={158}
           height={48}
         />
         <Image
-          className="max-h-12 w-full object-contain"
+          className="order-2 max-h-12 w-full object-contain lg:order-none"
           src="/images/logos/zendesk.svg"
           alt="ZenDesk"
           width={158}
           height={48}
         />
         <Image
-          className="max-h-12 w-full object-contain"
-          src="/images/logos/brilliant.svg"
-          alt="Brilliant"
-          width={158}
+          className="order-1 max-h-12 w-full object-contain lg:order-none"
+          src="/images/logos/netflix.svg"
+          alt="Netflix"
+          width={178}
           height={48}
         />
         <Image
-          className="max-h-12 w-full object-contain"
+          className="order-5 max-h-12 w-full object-contain lg:order-none"
           src="/images/logos/doac.svg"
           alt="DOAC"
           width={158}
           height={48}
         />
         <Image
-          className="max-h-12 w-full object-contain"
+          className="order-6 max-h-12 w-full object-contain lg:order-none"
+          src="/images/logos/brilliant.svg"
+          alt="Brilliant"
+          width={158}
+          height={48}
+        />
+        <Image
+          className="order-7 hidden max-h-12 w-full object-contain lg:order-none lg:block"
           src="/images/logos/joco.svg"
           alt="JOCO"
           width={158}
