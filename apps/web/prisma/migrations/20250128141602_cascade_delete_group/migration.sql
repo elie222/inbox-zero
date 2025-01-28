d5eb2c77f3df8585ec7b2da0013c07f2a2c969f4
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-01-28 16:18:06 +0200

    delete detached rules

--- apps/web/prisma/migrations/20250128141602_cascade_delete_group/migration.sql
+++ apps/web/prisma/migrations/20250128141602_cascade_delete_group/migration.sql
@@ -3,3 +3,11 @@ ALTER TABLE "Rule" DROP CONSTRAINT "Rule_groupId_fkey";
 
 -- AddForeignKey
 ALTER TABLE "Rule" ADD CONSTRAINT "Rule_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- Delete groups that are not used by any rule
+DELETE FROM "Group"
+WHERE "id" NOT IN (
+    SELECT "groupId"
+    FROM "Rule"
+    WHERE "groupId" IS NOT NULL
+);
