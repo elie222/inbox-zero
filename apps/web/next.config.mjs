68de94afce46cb9fd493dc19abd7fd44e03cdd94
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-02-02 14:30:29 +0200

    add headers for sw

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -173,6 +173,31 @@ const nextConfig = {
             key: "X-XSS-Protection",
             value: "1; mode=block",
           },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/javascript; charset=utf-8",
+          },
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'",
+          },
         ],
       },
     ];
