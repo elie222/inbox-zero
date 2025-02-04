8845e5e8e6edcde279bc02c869abd2af5ddae158
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-02-04 21:16:22 +0200

    add migration file

---
+++ apps/web/prisma/migrations/20250204191020_remove_email_token_action/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `action` on the `EmailToken` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "EmailToken" DROP COLUMN "action";
