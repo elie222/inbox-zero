fa52f9c9308a440d807eae0f94b7875537922768
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-02-23 14:50:00 +0200

    mail beta

--- apps/web/app/(app)/mail/BetaBanner.tsx
+++ apps/web/app/(app)/mail/BetaBanner.tsx
@@ -11,8 +11,8 @@ export function BetaBanner() {
   if (bannerVisible && typeof window !== "undefined")
     return (
       <Banner
-        title="Alpha"
-        description="Mail is currently in alpha. It is not intended to be a full replacement for your email client yet."
+        title="Beta"
+        description="Mail is currently in beta. It is not intended to be a full replacement for your email client yet."
         onClose={() => setBannerVisible(false)}
       />
     );
