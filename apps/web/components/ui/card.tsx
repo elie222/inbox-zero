802417e186c7dc4226533fc9ae7f2adbb361a216
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-02-25 22:27:42 +0200

    adjust card styles

--- apps/web/components/ui/card.tsx
+++ apps/web/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border border-border bg-background text-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-sm",
       className,
     )}
     {...props}
