74cf273fba1da40f9febc9ff4d3bf383560b5fc1
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-03-02 22:59:44 +0200

    assess refactor

--- apps/web/utils/actions/assess.ts
+++ apps/web/utils/actions/assess.ts
@@ -1,21 +1,11 @@
 "use server";
 
-import uniq from "lodash/uniq";
-import countBy from "lodash/countBy";
 import { auth } from "@/app/api/auth/[...nextauth]/auth";
 import { getGmailAccessToken, getGmailClient } from "@/utils/gmail/client";
-import type { gmail_v1 } from "@googleapis/gmail";
-import { queryBatchMessages } from "@/utils/gmail/message";
-import { getEmailClient } from "@/utils/mail";
 import prisma from "@/utils/prisma";
-import { isDefined } from "@/utils/types";
-import { getLabelById, getLabels, GmailLabel } from "@/utils/gmail/label";
 import { SafeError } from "@/utils/error";
 import { withActionInstrumentation } from "@/utils/actions/middleware";
-import { createScopedLogger } from "@/utils/logger";
-import { getFilters, getForwardingAddresses } from "@/utils/gmail/settings";
-
-const logger = createScopedLogger("actions/assess");
+import { assessUser } from "@/utils/assess";
 
 // to help with onboarding and provide the best flow to new users
 export const assessUserAction = withActionInstrumentation(
@@ -44,50 +34,6 @@ export const assessUserAction = withActionInstrumentation(
   },
 );
 
-async function assessUser({
-  gmail,
-  accessToken,
-}: {
-  gmail: gmail_v1.Gmail;
-  accessToken: string;
-}) {
-  // how many unread emails?
-  const unreadCount = await getUnreadEmailCount(gmail);
-  // how many unarchived emails?
-  const inboxCount = await getInboxCount(gmail);
-  // how many sent emails?
-  const sentCount = await getSentCount(gmail);
-
-  // does user make use of labels?
-  const labelCount = await getLabelCount(gmail);
-
-  // does user have any filters?
-  const filtersCount = await getFiltersCount(gmail);
-
-  // does user have any auto-forwarding rules?
-  // TODO
-
-  // does user forward emails to other accounts?
-  const forwardingAddressesCount = await getForwardingAddressesCount(gmail);
-
-  // does user use snippets?
-  // Gmail API doesn't provide a way to check this
-  // TODO We could check it with embeddings
-
-  // what email client does user use?
-  const emailClients = await getEmailClients(gmail, accessToken);
-
-  return {
-    unreadCount,
-    inboxCount,
-    sentCount,
-    labelCount,
-    filtersCount,
-    forwardingAddressesCount,
-    emailClients,
-  };
-}
-
 async function saveBehaviorProfile(
   email: string,
   assessment: Awaited<ReturnType<typeof assessUser>>,
@@ -96,69 +42,4 @@ async function saveBehaviorProfile(
     where: { email },
     data: { behaviorProfile: assessment },
   });
-}
-
-async function getUnreadEmailCount(gmail: gmail_v1.Gmail) {
-  const label = await getLabelById({ gmail, id: GmailLabel.UNREAD });
-  const unreadCount = label?.messagesUnread || 0;
-  return unreadCount;
-}
-
-async function getInboxCount(gmail: gmail_v1.Gmail) {
-  const label = await getLabelById({ gmail, id: GmailLabel.INBOX });
-  const inboxCount = label?.messagesTotal || 0;
-  return inboxCount;
-}
-
-async function getSentCount(gmail: gmail_v1.Gmail) {
-  const label = await getLabelById({ gmail, id: GmailLabel.SENT });
-  const sentCount = label?.messagesTotal || 0;
-  return sentCount;
-}
-
-async function getLabelCount(gmail: gmail_v1.Gmail) {
-  const labels = (await getLabels(gmail)) || [];
-  const DEFAULT_LABEL_COUNT = 13;
-  return labels.length - DEFAULT_LABEL_COUNT;
-}
-
-async function getFiltersCount(gmail: gmail_v1.Gmail) {
-  const filters = await getFilters(gmail);
-  return filters.length;
-}
-
-async function getForwardingAddressesCount(gmail: gmail_v1.Gmail) {
-  try {
-    const forwardingAddresses = await getForwardingAddresses(gmail);
-    return forwardingAddresses.length;
-  } catch (error) {
-    // Can happen due to "Forwarding features disabled by administrator"
-    logger.error("Error getting forwarding addresses", { error });
-    return 0;
-  }
-}
-
-async function getEmailClients(gmail: gmail_v1.Gmail, accessToken: string) {
-  try {
-    const { messages } = await queryBatchMessages(gmail, accessToken, {
-      query: "from:me",
-    });
-
-    // go through the messages, and check the headers for the email client
-    const clients = messages
-      .map((message) => {
-        return (
-          message.headers["message-id"] &&
-          getEmailClient(message.headers["message-id"])
-        );
-      })
-      .filter(isDefined);
-
-    const counts = countBy(clients);
-    const mostPopular = Object.entries(counts).sort((a, b) => b[1] - a[1]);
-
-    return { clients: uniq(clients), primary: mostPopular[0][0] };
-  } catch (error) {
-    logger.error("Error getting email clients", { error });
-  }
 }
