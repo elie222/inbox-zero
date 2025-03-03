f3560c1ad7b3beded2997eb284d80e2e50026eee
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-03-03 15:03:00 +0200

    Add set up page

--- apps/web/components/SideNav.tsx
+++ apps/web/components/SideNav.tsx
@@ -232,7 +232,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     <Sidebar collapsible="icon" {...props}>
       {state === "expanded" ? (
         <SidebarHeader>
-          <Link href={env.NEXT_PUBLIC_APP_HOME_PATH}>
+          <Link href="/setup">
             <div className="flex h-12 items-center p-4 text-white">
               <Logo className="h-4" />
             </div>
