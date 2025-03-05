9bd7d72219d1e7bf06f9485fe061bd62c584746c
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-03-05 14:54:01 +0200

    log publish and type

--- apps/web/utils/actions/clean.ts
+++ apps/web/utils/actions/clean.ts
@@ -11,6 +11,11 @@ import { getGmailClient } from "@/utils/gmail/client";
 import { bulkPublishToQstash } from "@/utils/upstash";
 import { env } from "@/env";
 import { GmailLabel } from "@/utils/gmail/label";
+import { createScopedLogger } from "@/utils/logger";
+import type { CleanThreadBody } from "@/app/api/clean/route";
+import { isDefined } from "@/utils/types";
+
+const logger = createScopedLogger("actions/clean");
 
 export const cleanInboxAction = withActionInstrumentation(
   "cleanInbox",
@@ -40,21 +45,31 @@ export const cleanInboxAction = withActionInstrumentation(
 
       if (!pageToken || threads.length === 0) break;
 
-      // push to queue
       const url = `${env.WEBHOOK_URL || env.NEXT_PUBLIC_BASE_URL}/api/email`;
 
+      logger.info("Pushing to Qstash", {
+        userId,
+        threadCount: threads.length,
+        nextPageToken,
+      });
+
       await bulkPublishToQstash({
-        items: threads.map((thread) => ({
-          url,
-          body: { userId, threadId: thread.id },
-          // give every user their own queue for ai processing. if we get too many parallel users we may need more
-          // api keys or a global queue
-          // problem with a global queue is that if there's a backlog users will have to wait for others to finish first
-          flowControl: {
-            key: `ai-clean-${userId}`,
-            parallelism: 1,
-          },
-        })),
+        items: threads
+          .map((thread) => {
+            if (!thread.id) return;
+            return {
+              url,
+              body: { userId, threadId: thread.id } satisfies CleanThreadBody,
+              // give every user their own queue for ai processing. if we get too many parallel users we may need more
+              // api keys or a global queue
+              // problem with a global queue is that if there's a backlog users will have to wait for others to finish first
+              flowControl: {
+                key: `ai-clean-${userId}`,
+                parallelism: 1,
+              },
+            };
+          })
+          .filter(isDefined),
       });
     }
   },
