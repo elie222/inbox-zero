94100127bab5b65af5e9fb54f7baba3e431893fa
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-03-22 23:20:08 +0200

    Delete assistant ui

--- apps/web/app/(app)/automation/chat/page.tsx
+++ apps/web/app/(app)/automation/chat/page.tsx
@@ -1,9 +1,7 @@
-// import { Assistant } from "@/app/assistant";
 import { Chat } from "@/components/assistant-chat/chat";
 import { DataStreamHandler } from "@/components/assistant-chat/data-stream-handler";
 
 export default function AssistantChatPage() {
-  // return <Assistant />;
   return (
     <>
       <Chat id="1" initialMessages={[]} selectedChatModel="gpt-4o" />
