f328ef04f54fdb154d0d20cd349326ef79cc7ca5
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-04-08 11:50:41 +0300

    validation

--- apps/web/utils/actions/unsubscriber.ts
+++ apps/web/utils/actions/unsubscriber.ts
@@ -2,31 +2,37 @@
 
 import { auth } from "@/app/api/auth/[...nextauth]/auth";
 import prisma from "@/utils/prisma";
-import type { NewsletterStatus } from "@prisma/client";
 import { withActionInstrumentation } from "@/utils/actions/middleware";
+import {
+  setNewsletterStatusBody,
+  type SetNewsletterStatusBody,
+} from "@/utils/actions/unsubscriber.validation";
 
 export const setNewsletterStatusAction = withActionInstrumentation(
   "setNewsletterStatus",
-  async (options: {
-    newsletterEmail: string;
-    status: NewsletterStatus | null;
-  }) => {
+  async (unsafeData: SetNewsletterStatusBody) => {
     const session = await auth();
     if (!session?.user.email) return { error: "Not logged in" };
+
+    const { data, success, error } =
+      setNewsletterStatusBody.safeParse(unsafeData);
+    if (!success) return { error: error.message };
+
+    const { newsletterEmail, status } = data;
 
     return await prisma.newsletter.upsert({
       where: {
         email_userId: {
-          email: options.newsletterEmail,
+          email: newsletterEmail,
           userId: session.user.id,
         },
       },
       create: {
-        status: options.status,
-        email: options.newsletterEmail,
+        status,
+        email: newsletterEmail,
         user: { connect: { id: session.user.id } },
       },
-      update: { status: options.status },
+      update: { status },
     });
   },
 );
