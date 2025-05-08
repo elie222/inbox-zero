44f0a4343b6359888d9de9af478a597789d28833
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-05-08 17:23:12 +0300

    delete old code

--- apps/web/utils/premium/index.ts
+++ apps/web/utils/premium/index.ts
@@ -10,42 +10,21 @@ export const isPremium = (
   return false;
 };
 
-// deprecated. we now store the plan in the database
-// but this is so that things don't break for older users
-const getUserPlan = (
-  lemonSqueezyRenewsAt?: Date | null,
-): PremiumTier | null => {
-  if (!lemonSqueezyRenewsAt) return null;
-
-  const renewsAt = new Date(lemonSqueezyRenewsAt);
-
-  // if renewsAt is 5 years in the future then it's a lifetime plan
-  if (renewsAt.getFullYear() - new Date().getFullYear() >= 5)
-    return PremiumTier.LIFETIME;
-
-  // if renewsAt is more than 6 months in the future then it's annual plan
-  if (renewsAt > new Date(new Date().setMonth(new Date().getMonth() + 6)))
-    return PremiumTier.BUSINESS_ANNUALLY;
-
-  // if renewsAt is less than 6 months in the future then it's a monthly plan
-  return PremiumTier.BUSINESS_MONTHLY;
-};
-
 export const getUserTier = (
   premium?: Pick<Premium, "tier" | "lemonSqueezyRenewsAt"> | null,
 ) => {
   if (isPremiumExpired(premium)) return null;
-  return premium?.tier || getUserPlan(premium?.lemonSqueezyRenewsAt);
+  return premium?.tier || null;
 };
 
-const isPremiumExpired = (
+function isPremiumExpired(
   premium?: Pick<Premium, "lemonSqueezyRenewsAt"> | null,
-) => {
+) {
   return (
     !!premium?.lemonSqueezyRenewsAt &&
     new Date(premium.lemonSqueezyRenewsAt) < new Date()
   );
-};
+}
 
 export const isAdminForPremium = (
   premiumAdmins: { id: string }[],
