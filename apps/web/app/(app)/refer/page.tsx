7acdf4823cb2c0db88f3e88e3c5b2f787b76aef0
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-06-19 21:34:56 +0200

    styling fix

--- apps/web/app/(app)/refer/page.tsx
+++ apps/web/app/(app)/refer/page.tsx
@@ -2,7 +2,7 @@ import { Referrals } from "@/components/ReferralDialog";
 
 export default function ReferPage() {
   return (
-    <div className="container flex items-center justify-center">
+    <div className="container flex h-full items-center justify-center">
       <Referrals />
     </div>
   );
