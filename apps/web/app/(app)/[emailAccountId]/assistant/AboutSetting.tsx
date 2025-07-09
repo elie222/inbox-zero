c7dcf8727e0827ac53d6ba4a4675c3601ff2bf28
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-07-09 19:47:18 +0200

    add about setting dialog

--- apps/web/app/(app)/[emailAccountId]/assistant/AboutSetting.tsx
+++ apps/web/app/(app)/[emailAccountId]/assistant/AboutSetting.tsx
@@ -1,5 +1,16 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { SettingCard } from "@/components/SettingCard";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { AboutSection } from "@/app/(app)/[emailAccountId]/settings/AboutSectionForm";
 
 export function AboutSetting() {
   return (
@@ -7,9 +18,24 @@ export function AboutSetting() {
       title="About you"
       description="Provide extra information that will help our AI better understand how to process your emails."
       right={
-        <Button variant="outline" size="sm">
-          Configure
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="sm">
+              Configure
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="max-w-2xl">
+            <DialogHeader>
+              <DialogTitle>About you</DialogTitle>
+              <DialogDescription>
+                Provide extra information that will help our AI better
+                understand how to process your emails.
+              </DialogDescription>
+            </DialogHeader>
+
+            <AboutSection />
+          </DialogContent>
+        </Dialog>
       }
     />
   );
