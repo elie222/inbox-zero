5841e5caa404b089db8c36be51a5dacd6696bf0f
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-08-27 12:09:29 +0300

    add soc2 badge

--- apps/web/app/(landing)/home/Privacy.tsx
+++ apps/web/app/(landing)/home/Privacy.tsx
@@ -1,6 +1,28 @@
+import Image from "next/image";
+
 export function Privacy() {
   return (
     <div className="bg-white py-24" id="features">
+      <div className="mb-8 flex flex-col items-center justify-center gap-4 sm:flex-row">
+        <div className="flex items-center gap-8">
+          <Image
+            src="/images/home/soc2.svg"
+            alt="SOC2 Type II Compliant"
+            className="h-[120px] w-auto"
+            width="200"
+            height="120"
+          />
+
+          <Image
+            src="/images/home/soc2.png"
+            alt="SOC2 Type II Compliant"
+            className="h-[160px] w-auto"
+            width="300"
+            height="160"
+          />
+        </div>
+      </div>
+
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="font-cal text-base leading-7 text-blue-600">
