aa14eb5bc1191859c7418f2e16a4a28bbefb7e35
TiboGrx <gorioux.thibaut@gmail.com>
2025-08-29 18:03:58 +0200

    Use entrypoint.sh to run migration when web container starts

--- apps/web/entrypoint.sh
+++ apps/web/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-pnpm install
-pnpm prisma migrate dev
-pnpm run dev 
+if [ "$NODE_ENV" = "production" ]; then
+  echo "Running in production mode..."
+  pnpm --filter inbox-zero-ai exec -- prisma migrate deploy
+  pnpm --filter inbox-zero-ai start
+else
+  echo "Running in development mode..."
+  pnpm install
+  pnpm prisma migrate dev
+  pnpm run dev
+fi
+
