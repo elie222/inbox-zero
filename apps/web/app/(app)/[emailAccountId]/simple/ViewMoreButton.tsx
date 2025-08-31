7d78992cab81286a2a814a97c7a9d5a1175a5ac9
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-08-31 21:39:08 +0300

    Remove simple mode which hasn't been an active feature for a long time

--- apps/web/app/(app)/[emailAccountId]/simple/ViewMoreButton.tsx
+++
@@ -1,7 +0,0 @@
-export function ViewMoreButton({ onClick }: { onClick: () => void }) {
-  return (
-    <button type="button" className="font-semibold" onClick={onClick}>
-      View more
-    </button>
-  );
-}
