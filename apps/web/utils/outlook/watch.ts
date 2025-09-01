17dbadf7f89dc59e93c381cb364e4e43fba54ad0
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-09-01 12:39:57 +0300

    add client state for msft webhook

--- apps/web/utils/outlook/watch.ts
+++ apps/web/utils/outlook/watch.ts
@@ -3,13 +3,18 @@ import addDays from "date-fns/addDays";
 import { env } from "@/env";
 
 export async function watchOutlook(client: Client) {
-  const subscription = await client.api("/subscriptions").post({
+  const subscriptionPayload = {
     changeType: "created,updated",
     // must be https
     notificationUrl: `${env.NODE_ENV === "development" ? env.WEBHOOK_URL || env.NEXT_PUBLIC_BASE_URL : env.NEXT_PUBLIC_BASE_URL}/api/outlook/webhook`,
     resource: "/me/mailFolders/inbox/messages",
     expirationDateTime: addDays(new Date(), 3).toISOString(), // 3 days (max allowed)
-  });
+    clientState: env.MICROSOFT_WEBHOOK_CLIENT_STATE,
+  };
+
+  const subscription = await client
+    .api("/subscriptions")
+    .post(subscriptionPayload);
 
   return {
     id: subscription.id,
