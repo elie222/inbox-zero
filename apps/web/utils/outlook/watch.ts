12ed5c20e6c43513903b06d10be6a5dd94a51ed5
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-09-01 13:08:18 +0300

    msft notification url

--- apps/web/utils/outlook/watch.ts
+++ apps/web/utils/outlook/watch.ts
@@ -1,18 +1,29 @@
 import type { Client } from "@microsoft/microsoft-graph-client";
+import type { Subscription } from "@microsoft/microsoft-graph-types";
 import addDays from "date-fns/addDays";
 import { env } from "@/env";
 
 export async function watchOutlook(client: Client) {
+  const base =
+    env.NODE_ENV === "development"
+      ? env.WEBHOOK_URL || env.NEXT_PUBLIC_BASE_URL
+      : env.NEXT_PUBLIC_BASE_URL;
+
+  // must be https
+  const notificationUrl = new URL("/api/outlook/webhook", base);
+  if (notificationUrl.protocol === "http:") {
+    notificationUrl.protocol = "https:";
+  }
+
   const subscriptionPayload = {
     changeType: "created,updated",
-    // must be https
-    notificationUrl: `${env.NODE_ENV === "development" ? env.WEBHOOK_URL || env.NEXT_PUBLIC_BASE_URL : env.NEXT_PUBLIC_BASE_URL}/api/outlook/webhook`,
+    notificationUrl: notificationUrl.toString(),
     resource: "/me/mailFolders/inbox/messages",
     expirationDateTime: addDays(new Date(), 3).toISOString(), // 3 days (max allowed)
     clientState: env.MICROSOFT_WEBHOOK_CLIENT_STATE,
   };
 
-  const subscription = await client
+  const subscription: Subscription = await client
     .api("/subscriptions")
     .post(subscriptionPayload);
 
