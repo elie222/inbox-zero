5f19b0f4df34ae210060b8e803605b004189f813
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-09-21 23:11:32 +0300

    end ab test

--- apps/web/app/(landing)/welcome-upgrade/page.tsx
+++ apps/web/app/(landing)/welcome-upgrade/page.tsx
@@ -2,14 +2,14 @@ import { PricingLazy } from "@/app/(app)/premium/PricingLazy";
 import { Footer } from "@/app/(landing)/home/Footer";
 import { WelcomeUpgradeNav } from "@/app/(landing)/welcome-upgrade/WelcomeUpgradeNav";
 import { WelcomeUpgradeHeader } from "@/app/(landing)/welcome-upgrade/WelcomeUpgradeHeader";
-import { WelcomeUpgradeTestimonial } from "@/app/(landing)/welcome-upgrade/Testimonial";
+import { Testimonial } from "@/app/(landing)/welcome-upgrade/Testimonial";
 
 export default function WelcomeUpgradePage() {
   return (
     <>
       <WelcomeUpgradeNav />
       <PricingLazy showSkipUpgrade header={<WelcomeUpgradeHeader />} />
-      <WelcomeUpgradeTestimonial />
+      <Testimonial />
       <Footer />
     </>
   );
