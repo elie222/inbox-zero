f659b467fb7bd1acfb4b9f068168a48da97accf1
jshwrnr <joshwerner001@gmail.com>
2025-11-06 10:40:09 +0200

    Add video modal

--- apps/web/components/new-landing/sections/Hero.tsx
+++ apps/web/components/new-landing/sections/Hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { Gmail } from "@/components/new-landing/icons/Gmail";
 import { Outlook } from "@/components/new-landing/icons/Outlook";
@@ -6,6 +8,12 @@ import { Paragraph } from "@/components/new-landing/common/Typography";
 import { CallToAction } from "@/components/new-landing/CallToAction";
 import { LiquidGlassButton } from "@/components/new-landing/LiquidGlassButton";
 import { Play } from "@/components/new-landing/icons/Play";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 export function Hero() {
   return (
@@ -34,9 +42,25 @@ export function Hero() {
           </div>
         </div>
         <div className="relative">
-          <LiquidGlassButton className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <Play />
-          </LiquidGlassButton>
+          <Dialog>
+            <DialogTrigger asChild>
+              <LiquidGlassButton className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <Play />
+              </LiquidGlassButton>
+            </DialogTrigger>
+            <DialogContent className="max-w-4xl border-0 bg-transparent p-0">
+              <DialogTitle className="sr-only">Video player</DialogTitle>
+              <div className="relative aspect-video w-full">
+                <iframe
+                  src="https://www.youtube.com/embed/hfvKvTHBjG0?autoplay=1&rel=0"
+                  className="size-full rounded-lg"
+                  title="Video content"
+                  allowFullScreen
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                />
+              </div>
+            </DialogContent>
+          </Dialog>
           <Image
             src="/images/new-landing/video-thumbnail.png"
             alt="an organized inbox"
