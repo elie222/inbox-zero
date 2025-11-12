41428dcc3fb3898c4cd1590a0255b17d86bb7ff9
jshwrnr <joshwerner001@gmail.com>
2025-11-12 07:50:01 +0200

    Fix video player border radius on mobile

--- apps/web/components/new-landing/sections/Hero.tsx
+++ apps/web/components/new-landing/sections/Hero.tsx
@@ -82,7 +82,7 @@ export function Hero() {
 export function HeroVideoPlayer() {
   return (
     <div className="relative w-full">
-      <div className="relative border border-[#EFEFEF] rounded-[43px] overflow-hidden block">
+      <div className="relative border border-[#EFEFEF] rounded-3xl md:rounded-[43px] overflow-hidden block">
         <Dialog>
           <DialogTrigger asChild>
             <LiquidGlassButton className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
