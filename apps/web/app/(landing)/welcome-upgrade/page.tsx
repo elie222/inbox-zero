9b5fd0944e76813e1e01248027be17da6a8775eb
jshwrnr <joshwerner001@gmail.com>
2025-11-16 15:58:47 +0200

    Use old pricing on welcome page

--- apps/web/app/(landing)/welcome-upgrade/page.tsx
+++ apps/web/app/(landing)/welcome-upgrade/page.tsx
@@ -1,14 +1,13 @@
-import { PricingLazy } from "@/app/(app)/premium/PricingLazy";
 import { Footer } from "@/app/(landing)/home/Footer";
 import { WelcomeUpgradeNav } from "@/app/(landing)/welcome-upgrade/WelcomeUpgradeNav";
-import { WelcomeUpgradeHeader } from "@/app/(landing)/welcome-upgrade/WelcomeUpgradeHeader";
 import { Testimonial } from "@/app/(landing)/welcome-upgrade/Testimonial";
+import { OldPricingLazy } from "@/app/(app)/premium/OldPricingLazy";
 
 export default function WelcomeUpgradePage() {
   return (
     <>
       <WelcomeUpgradeNav />
-      <PricingLazy showSkipUpgrade header={<WelcomeUpgradeHeader />} />
+      <OldPricingLazy />
       <div className="mt-8">
         <Testimonial />
       </div>
