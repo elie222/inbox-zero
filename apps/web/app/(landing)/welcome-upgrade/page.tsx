f836718a75c27145289a645c6e1a73269e45929d
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2025-11-18 20:38:37 +0200

    Put back pricing in app

--- apps/web/app/(landing)/welcome-upgrade/page.tsx
+++ apps/web/app/(landing)/welcome-upgrade/page.tsx
@@ -1,13 +1,14 @@
 import { Footer } from "@/app/(landing)/home/Footer";
 import { WelcomeUpgradeNav } from "@/app/(landing)/welcome-upgrade/WelcomeUpgradeNav";
+import { WelcomeUpgradeHeader } from "@/app/(landing)/welcome-upgrade/WelcomeUpgradeHeader";
 import { Testimonial } from "@/app/(landing)/welcome-upgrade/Testimonial";
-import { OldPricingLazy } from "@/app/(app)/premium/OldPricingLazy";
+import { AppPricingLazy } from "@/app/(app)/premium/AppPricingLazy";
 
 export default function WelcomeUpgradePage() {
   return (
     <>
       <WelcomeUpgradeNav />
-      <OldPricingLazy />
+      <AppPricingLazy showSkipUpgrade header={<WelcomeUpgradeHeader />} />
       <div className="mt-8">
         <Testimonial />
       </div>
