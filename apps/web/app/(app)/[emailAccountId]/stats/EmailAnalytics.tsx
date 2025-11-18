a5bed512675c145ea84493031377d59c6e2028ba
jshwrnr <joshwerner001@gmail.com>
2025-11-18 17:26:06 +0200

    Combine email analytics cards

--- apps/web/app/(app)/[emailAccountId]/stats/EmailAnalytics.tsx
+++ apps/web/app/(app)/[emailAccountId]/stats/EmailAnalytics.tsx
@@ -2,15 +2,18 @@
 
 import useSWR from "swr";
 import type { DateRange } from "react-day-picker";
+import { BarList as TremorBarList, Flex, Text } from "@tremor/react";
 import { useExpanded } from "@/app/(app)/[emailAccountId]/stats/useExpanded";
 import type { RecipientsResponse } from "@/app/api/user/stats/recipients/route";
 import type { SendersResponse } from "@/app/api/user/stats/senders/route";
 import { LoadingContent } from "@/components/LoadingContent";
 import { Skeleton } from "@/components/ui/skeleton";
 import { BarList } from "@/components/charts/BarList";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getDateRangeParams } from "@/app/(app)/[emailAccountId]/stats/params";
 import { getGmailSearchUrl } from "@/utils/url";
 import { useAccount } from "@/providers/EmailAccountProvider";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 export function EmailAnalytics(props: {
   dateRange?: DateRange | undefined;
@@ -41,47 +44,75 @@ export function EmailAnalytics(props: {
   const { expanded, extra } = useExpanded();
 
   return (
-    <div className="grid gap-2 sm:gap-4 sm:grid-cols-3">
+    <div className="grid gap-2 sm:gap-4 sm:grid-cols-2">
       <LoadingContent
         loading={isLoading}
         error={error}
         loadingComponent={<Skeleton className="h-64 w-full rounded" />}
       >
         {data && (
-          <BarList
-            title="Who emails you most"
-            col1="Sender"
-            col2="Emails"
-            data={data.mostActiveSenderEmails
-              ?.slice(0, expanded ? undefined : 5)
-              .map((d) => ({
-                ...d,
-                href: getGmailSearchUrl(d.name, userEmail),
-                target: "_blank",
-              }))}
-            extra={extra}
-          />
-        )}
-      </LoadingContent>
-      <LoadingContent
-        loading={isLoading}
-        error={error}
-        loadingComponent={<Skeleton className="h-64 w-full rounded" />}
-      >
-        {data && (
-          <BarList
-            title="Domains that email you most"
-            col1="Domain"
-            col2="Emails"
-            data={data.mostActiveSenderDomains
-              ?.slice(0, expanded ? undefined : 5)
-              .map((d) => ({
-                ...d,
-                href: getGmailSearchUrl(d.name, userEmail),
-                target: "_blank",
-              }))}
-            extra={extra}
-          />
+          <Tabs defaultValue="sender">
+            <TabsContent value="sender">
+              <Card className="h-full bg-background">
+                <CardHeader>
+                  <div className="flex items-center justify-between">
+                    <CardTitle className="text-lg">Who emails you</CardTitle>
+                    <TabsList>
+                      <TabsTrigger value="sender">Sender</TabsTrigger>
+                      <TabsTrigger value="domain">Domain</TabsTrigger>
+                    </TabsList>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <Flex>
+                    <Text>Sender</Text>
+                    <Text>Emails</Text>
+                  </Flex>
+                  <TremorBarList
+                    data={data.mostActiveSenderEmails
+                      ?.slice(0, expanded ? undefined : 5)
+                      .map((d) => ({
+                        ...d,
+                        href: getGmailSearchUrl(d.name, userEmail),
+                        target: "_blank",
+                      }))}
+                    className="mt-2"
+                  />
+                  {extra}
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="domain">
+              <Card className="h-full bg-background">
+                <CardHeader>
+                  <div className="flex items-center justify-between">
+                    <CardTitle className="text-lg">Who emails you</CardTitle>
+                    <TabsList>
+                      <TabsTrigger value="sender">Sender</TabsTrigger>
+                      <TabsTrigger value="domain">Domain</TabsTrigger>
+                    </TabsList>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <Flex>
+                    <Text>Domain</Text>
+                    <Text>Emails</Text>
+                  </Flex>
+                  <TremorBarList
+                    data={data.mostActiveSenderDomains
+                      ?.slice(0, expanded ? undefined : 5)
+                      .map((d) => ({
+                        ...d,
+                        href: getGmailSearchUrl(d.name, userEmail),
+                        target: "_blank",
+                      }))}
+                    className="mt-2"
+                  />
+                  {extra}
+                </CardContent>
+              </Card>
+            </TabsContent>
+          </Tabs>
         )}
       </LoadingContent>
       <LoadingContent
