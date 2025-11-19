c33fb8329e5a4f73bd6d3fe5134a1dbff55caa66
jshwrnr <joshwerner001@gmail.com>
2025-11-19 20:46:54 +0200

    Add icons for email items

--- apps/web/components/charts/HorizontalBarChart.tsx
+++ apps/web/components/charts/HorizontalBarChart.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cx } from "class-variance-authority";
+import Image from "next/image";
 
 interface HorizontalBarChartProps {
   data: Array<{
@@ -22,6 +23,16 @@ export function HorizontalBarChart({
     <div className={cx("space-y-2", className)}>
       {data.map((item) => {
         const widthPercentage = (item.value / maxValue) * 100;
+        const domain = item.name.includes("@")
+          ? item.name.split("@")[1]
+          : item.name;
+        const hasSubdomain = domain.split(".").length > 2;
+        const apexDomain = hasSubdomain
+          ? domain.split(".").slice(1).join(".")
+          : domain;
+        const icon = domain
+          ? `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://${apexDomain}&size=64`
+          : "";
 
         return (
           <div
@@ -34,17 +45,26 @@ export function HorizontalBarChart({
                   className="absolute top-0 left-0 bg-gradient-to-r from-blue-100 to-blue-50 h-full rounded-md"
                   style={{ width: `${widthPercentage}%` }}
                 />
-                <a
-                  href={item.href}
-                  target={item.target}
-                  className="text-sm text-gray-900 truncate block z-10 relative hover:underline"
-                >
-                  {item.name}
-                </a>
+                <div className="flex items-center gap-2">
+                  <Image
+                    width={20}
+                    height={20}
+                    src={icon}
+                    alt="favicon"
+                    className="rounded-full z-10"
+                  />
+                  <a
+                    href={item.href}
+                    target={item.target}
+                    className="text-sm text-gray-900 truncate block z-10 relative hover:underline"
+                  >
+                    {item.name}
+                  </a>
+                </div>
               </div>
             </div>
             <div className="flex-shrink-0">
-              <span className="text-sm text-gray-500">
+              <span className="text-sm text-gray-600">
                 {item.value.toLocaleString()}
               </span>
             </div>
