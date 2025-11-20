862491b3ac5b2d505fa963f538458bc3186ce9e5
jshwrnr <joshwerner001@gmail.com>
2025-11-20 11:32:00 +0200

    Remove tremor from progress panel

--- apps/web/components/ui/progress.tsx
+++ apps/web/components/ui/progress.tsx
@@ -4,11 +4,14 @@ import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 
 import { cn } from "@/utils/index";
+import { cx } from "class-variance-authority";
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
+    color?: string;
+  }
+>(({ className, value, color = "bg-blue-500", ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,7 +21,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-blue-500 transition-all"
+      className={cx("h-full w-full flex-1 transition-all", color)}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
