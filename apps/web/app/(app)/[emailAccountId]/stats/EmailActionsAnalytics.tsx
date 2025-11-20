a1641457dc61fb4e0b5d7dad6db4077e06b2cb0b
jshwrnr <joshwerner001@gmail.com>
2025-11-20 08:31:54 +0200

    Add in temp mock data

--- apps/web/app/(app)/[emailAccountId]/stats/EmailActionsAnalytics.tsx
+++ apps/web/app/(app)/[emailAccountId]/stats/EmailActionsAnalytics.tsx
@@ -6,11 +6,13 @@ import { LoadingContent } from "@/components/LoadingContent";
 import { Skeleton } from "@/components/ui/skeleton";
 import { CardBasic } from "@/components/ui/card";
 import type { EmailActionStatsResponse } from "@/app/api/user/stats/email-actions/route";
+import { MOCK_EMAIL_ACTIONS } from "@/app/(app)/[emailAccountId]/stats/mock-data";
 
 export function EmailActionsAnalytics() {
-  const { data, isLoading, error } = useOrgSWR<EmailActionStatsResponse>(
+  const { /*data, */ isLoading, error } = useOrgSWR<EmailActionStatsResponse>(
     "/api/user/stats/email-actions",
   );
+  const data = MOCK_EMAIL_ACTIONS;
 
   return (
     <LoadingContent
