1b5ff68cb1a4d81e4a89ef1e410efb5d1a20e10d
jshwrnr <joshwerner001@gmail.com>
2025-11-23 17:29:36 +0200

    Move badge colors to tailwind config

--- apps/web/components/new-landing/common/Badge.tsx
+++ apps/web/components/new-landing/common/Badge.tsx
@@ -32,26 +32,26 @@ export function Badge({
     {
       variants: {
         variant: {
-          blue: "text-[#006EFF] from-[#D6E8FC] to-[#C3DEFC] shadow-[0px_2px_3.4px_0px_#CFD9F938,0px_1px_1px_0px_#CFD9F994]",
+          blue: "text-badge-blue-main from-badge-blue-outer-from to-badge-blue-outer-to shadow-[0px_2px_3.4px_0px_#CFD9F938,0px_1px_1px_0px_#CFD9F994]",
           purple:
-            "text-[#6410FF] from-[#E1D5FC] to-[#D7C3FC] shadow-[0px_2px_3.4px_0px_#CFD9F938,0px_1px_1px_0px_#CFD9F994]",
+            "text-badge-purple-main from-badge-purple-outer-from to-badge-purple-outer-to shadow-[0px_2px_3.4px_0px_#CFD9F938,0px_1px_1px_0px_#CFD9F994]",
           "dark-blue":
-            "text-[#124DFF] from-[#D5DEFC] to-[#C2D0FC] shadow-[0px_2px_3.4px_0px_#CFD9F938,0px_1px_1px_0px_#CFD9F994]",
+            "text-badge-dark-blue-main from-badge-dark-blue-outer-from to-badge-dark-blue-outer-to shadow-[0px_2px_3.4px_0px_#CFD9F938,0px_1px_1px_0px_#CFD9F994]",
           green:
-            "text-[#17A34A] from-[#DDF4D3] to-[#CFF4C0] shadow-[0px_2px_3.4px_0px_#CFF9DE38,0px_1px_1px_0px_#76D98F1C]",
+            "text-badge-green-main from-badge-green-outer-from to-badge-green-outer-to shadow-[0px_2px_3.4px_0px_#CFF9DE38,0px_1px_1px_0px_#76D98F1C]",
           yellow:
-            "text-[#D8A40C] from-[#E7E0CB] to-[#E7DBB9] shadow-[0px_2px_3.4px_0px_#F9EDCF38,0px_1px_1px_0px_#F9ECCF94]",
+            "text-badge-yellow-main from-badge-yellow-outer-from to-badge-yellow-outer-to shadow-[0px_2px_3.4px_0px_#F9EDCF38,0px_1px_1px_0px_#F9ECCF94]",
           brown:
-            "text-[#CC762F] from-[#EFDFD3] to-[#E9D1BE] shadow-[0px_2px_3.4px_0px_#F0D4BA38,0px_1px_1px_0px_#F9E0CF94]",
-          red: "text-[#C94244] from-[#FDD3D4] to-[#FCC0C0] shadow-[0px_2px_3.4px_0px_#F9CFD326,0px_1px_1px_0px_#F9CFD08A]",
+            "text-badge-brown-main from-badge-brown-outer-from to-badge-brown-outer-to shadow-[0px_2px_3.4px_0px_#F0D4BA38,0px_1px_1px_0px_#F9E0CF94]",
+          red: "text-badge-red-main from-badge-red-outer-from to-badge-red-outer-to shadow-[0px_2px_3.4px_0px_#F9CFD326,0px_1px_1px_0px_#F9CFD08A]",
           "light-blue":
-            "text-[#49D1FA] from-[#E5F9FF] to-[#D0F4FF] shadow-[0px_2px_3.4px_0px_#E6E6E638,0px_1px_1px_0px_#B1B1B11C]",
+            "text-badge-light-blue-main from-badge-light-blue-outer-from to-badge-light-blue-outer-to shadow-[0px_2px_3.4px_0px_#E6E6E638,0px_1px_1px_0px_#B1B1B11C]",
           orange:
-            "text-[#E65707] from-[#FCE2D5] to-[#FCD6C2] shadow-[0px_2px_3.4px_0px_#F9D3CF38,0px_1px_1px_0px_#F9E5CF94]",
-          pink: "text-[#C942B2] from-[#FDD3EB] to-[#FDBFE0] shadow-[0px_2px_3.4px_0px_#F9CFD326,0px_1px_1px_0px_#F9CFD08A]",
-          gray: "text-[#8E8E8E] from-[#EEEEEE] to-[#E6E6E6] shadow-[0px_2px_3.4px_0px_#E6E6E638,0px_1px_1px_0px_#B1B1B11C]",
+            "text-badge-orange-main from-badge-orange-outer-from to-badge-orange-outer-to shadow-[0px_2px_3.4px_0px_#F9D3CF38,0px_1px_1px_0px_#F9E5CF94]",
+          pink: "text-badge-pink-main from-badge-pink-outer-from to-badge-pink-outer-to shadow-[0px_2px_3.4px_0px_#F9CFD326,0px_1px_1px_0px_#F9CFD08A]",
+          gray: "text-badge-gray-main from-badge-gray-outer-from to-badge-gray-outer-to shadow-[0px_2px_3.4px_0px_#E6E6E638,0px_1px_1px_0px_#B1B1B11C]",
           "dark-gray":
-            "text-[#525252] from-[#EEEEEE] to-[#E6E6E6] shadow-[0px_2px_3.4px_0px_#E6E6E638,0px_1px_1px_0px_#B1B1B11C]",
+            "text-badge-dark-gray-main from-badge-dark-gray-outer-from to-badge-dark-gray-outer-to shadow-[0px_2px_3.4px_0px_#E6E6E638,0px_1px_1px_0px_#B1B1B11C]",
         },
       },
     },
@@ -62,18 +62,21 @@ export function Badge({
     {
       variants: {
         variant: {
-          blue: "from-[#EFF6FF] to-[#D8E9FF]",
-          purple: "from-[#F3EAFE] to-[#E7DAFF]",
-          "dark-blue": "from-[#EFF3FF] to-[#D9E2FF]",
-          green: "from-[#F3FFEF] to-[#E1FFD8]",
-          yellow: "from-[#FFFBEF] to-[#FFF3DA]",
-          brown: "from-[#FEEDE0] to-[#F8E0CC]",
-          red: "from-[#FFEEF0] to-[#FFDADB]",
-          "light-blue": "from-[#FEFFFF] to-[#E5F9FF]",
-          orange: "from-[#FFF5EF] to-[#FFE7DA]",
-          pink: "from-[#FFEEF8] to-[#FFDAEC]",
-          gray: "from-[#FFFFFF] to-[#F6F6F6]",
-          "dark-gray": "from-[#FFFFFF] to-[#F6F6F6]",
+          blue: "from-badge-blue-inner-from to-badge-blue-inner-to",
+          purple: "from-badge-purple-inner-from to-badge-purple-inner-to",
+          "dark-blue":
+            "from-badge-dark-blue-inner-from to-badge-dark-blue-inner-to",
+          green: "from-badge-green-inner-from to-badge-green-inner-to",
+          yellow: "from-badge-yellow-inner-from to-badge-yellow-inner-to",
+          brown: "from-badge-brown-inner-from to-badge-brown-inner-to",
+          red: "from-badge-red-inner-from to-badge-red-inner-to",
+          "light-blue":
+            "from-badge-light-blue-inner-from to-badge-light-blue-inner-to",
+          orange: "from-badge-orange-inner-from to-badge-orange-inner-to",
+          pink: "from-badge-pink-inner-from to-badge-pink-inner-to",
+          gray: "from-badge-gray-inner-from to-badge-gray-inner-to",
+          "dark-gray":
+            "from-badge-dark-gray-inner-from to-badge-dark-gray-inner-to",
         },
       },
     },
