02987cf55be39ce0e379c9b551c2c2d6c2d66fc5
jshwrnr <joshwerner001@gmail.com>
2026-01-12 14:40:46 +0200

    Remove migration file

--- apps/web/prisma/migrations/20260112000000_add_thread_tracker_follow_up_index/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "ThreadTracker_emailAccountId_type_resolved_followUpAppliedA_idx" ON "ThreadTracker"("emailAccountId", "type", "resolved", "followUpAppliedAt", "sentAt");
