1d1474e3a9f450bc5a4ea3b157ec93e4a68ff68e
jshwrnr <joshwerner001@gmail.com>
2026-01-12 13:50:46 +0200

    Remove migration file

--- apps/web/prisma/migrations/20260108072620_add_follow_up_reminders/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "EmailAccount" ADD COLUMN "followUpAwaitingReplyDays" INTEGER NOT NULL DEFAULT 3,
-ADD COLUMN "followUpNeedsReplyDays" INTEGER NOT NULL DEFAULT 3,
-ADD COLUMN "followUpRemindersEnabled" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "ThreadTracker" ADD COLUMN "followUpAppliedAt" TIMESTAMP(3);
