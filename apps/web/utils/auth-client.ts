052b0aa15c36a0174d73d5dcb1c20d7712368e32
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2026-01-26 15:37:40 +0200

    Add debug logging to auth-client

--- apps/web/utils/auth-client.ts
+++ apps/web/utils/auth-client.ts
@@ -3,6 +3,12 @@ import { env } from "@/env";
 import { ssoClient } from "@better-auth/sso/client";
 import { organizationClient } from "better-auth/client/plugins";
 
+// DEBUG: Log what baseURL the auth client is using
+console.log(
+  "[auth-client] NEXT_PUBLIC_BASE_URL from env:",
+  env.NEXT_PUBLIC_BASE_URL,
+);
+
 export const { signIn, signOut, signUp, useSession, getSession, sso } =
   createAuthClient({
     baseURL: env.NEXT_PUBLIC_BASE_URL,
