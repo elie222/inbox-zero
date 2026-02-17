f048f2ee90d8f8b16ea64df39a87b73cdbc3d188
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2026-02-17 11:27:43 +0200

    Fix enum migration ordering

--- apps/web/prisma/migrations/20260217103000_add_telegram_provider/migration.sql
+++ apps/web/prisma/migrations/20260217103000_add_telegram_provider/migration.sql
@@ -1,8 +1,2 @@
 -- AlterEnum
 ALTER TYPE "MessagingProvider" ADD VALUE IF NOT EXISTS 'TELEGRAM';
-
--- CreateIndex
-CREATE UNIQUE INDEX "MessagingChannel_telegram_active_bot_unique"
-ON "MessagingChannel"("provider", "teamId")
-WHERE "provider" = 'TELEGRAM'::"MessagingProvider"
-  AND "isConnected" = true;
