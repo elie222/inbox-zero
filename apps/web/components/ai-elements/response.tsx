131caf769253d5ace4c6352dedcc0af957b21766
Eliezer Steinbock <3090527+elie222@users.noreply.github.com>
2026-03-13 01:49:51 +0200

    Refine assistant chat spacing

--- apps/web/components/ai-elements/response.tsx
+++ apps/web/components/ai-elements/response.tsx
@@ -19,6 +19,9 @@ export const Response = memo(
     createElement(Streamdown, {
       className: cn(
         "size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0",
+        "[&_[data-streamdown='heading-1']]:!mt-8",
+        "[&_[data-streamdown='heading-2']]:!mt-8",
+        "[&_[data-streamdown='heading-3']]:!mt-7",
         "[&_a]:!text-inherit [&_a]:underline [&_a]:underline-offset-2 [&_a:hover]:opacity-80",
         className,
       ),
